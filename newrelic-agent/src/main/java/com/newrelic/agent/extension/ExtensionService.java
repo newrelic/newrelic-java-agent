cb0b4e0982386532350ba4ac901be1a9dc1de0e1
jasonjkeller <jasonjkeller@gmail.com>
2021-03-28 23:10:50 -0700

    Remove import

--- newrelic-agent/src/main/java/com/newrelic/agent/extension/ExtensionService.java
+++ newrelic-agent/src/main/java/com/newrelic/agent/extension/ExtensionService.java
@@ -51,7 +51,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 /**
