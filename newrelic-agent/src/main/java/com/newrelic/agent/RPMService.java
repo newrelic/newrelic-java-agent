d8bfe18eb4265143a9790e33cdef0ea83ff50836
jasonjkeller <jasonjkeller@gmail.com>
2021-03-30 07:43:34 -0700

    Remove TODO

--- newrelic-agent/src/main/java/com/newrelic/agent/RPMService.java
+++ newrelic-agent/src/main/java/com/newrelic/agent/RPMService.java
@@ -268,7 +268,7 @@ public class RPMService extends AbstractService implements IRPMService, Environm
         connectionTimestamp = System.nanoTime();
         connected = true;
         hasEverConnected = true;
-        entityGuid = data.get("entity_guid") != null ? data.get("entity_guid").toString() : ""; // TODO
+        entityGuid = data.get("entity_guid") != null ? data.get("entity_guid").toString() : "";
 
         if (connectionListener != null) {
             config = config != null ? config : ServiceFactory.getConfigService().getDefaultAgentConfig();
