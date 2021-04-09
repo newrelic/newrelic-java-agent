d3d22575f1369923a318818b582077bbeec1952d
Andre Onuki <otanuki@gmail.com>
2021-04-09 15:09:36 -0400

    Adding http status code/text to external spans

--- newrelic-api/src/main/java/com/newrelic/api/agent/HttpParameters.java
+++ newrelic-api/src/main/java/com/newrelic/api/agent/HttpParameters.java
@@ -31,6 +31,10 @@ public class HttpParameters implements ExternalParameters {
      */
     private final String procedure;
 
+    private final Integer statusCode;
+
+    private final String statusText;
+
     /**
      * The headers from the response of the external call.
      */
@@ -49,6 +53,8 @@ public class HttpParameters implements ExternalParameters {
         this.library = library;
         this.uri = uri;
         this.procedure = procedure;
+        this.statusCode = null;
+        this.statusText = null;
         this.inboundResponseHeaders = inboundHeaders;
         this.extendedInboundResponseHeaders = extendedInboundHeaders;
     }
@@ -57,8 +63,20 @@ public class HttpParameters implements ExternalParameters {
         this.library = httpParameters.library;
         this.uri = httpParameters.uri;
         this.procedure = httpParameters.procedure;
+        this.statusCode = httpParameters.statusCode;
+        this.statusText = httpParameters.statusText;
         this.inboundResponseHeaders = httpParameters.inboundResponseHeaders;
         this.extendedInboundResponseHeaders = null;
+    }
+
+    protected HttpParameters(Builder builder) {
+        this.library = builder.library;
+        this.uri = builder.uri;
+        this.procedure = builder.procedure;
+        this.statusCode = builder.statusCode;
+        this.statusText = builder.statusText;
+        this.inboundResponseHeaders = builder.inboundHeaders;
+        this.extendedInboundResponseHeaders = builder.extendedInboundHeaders;
     }
 
     /**
@@ -92,6 +110,26 @@ public class HttpParameters implements ExternalParameters {
     }
 
     /**
+     * Returns the HTTP status code for the call.
+     *
+     * @return the status code for the call, null if not available
+     * @since 7.0.0
+     */
+    public Integer getStatusCode() {
+        return statusCode;
+    }
+
+    /**
+     * Returns the HTTP reason message for the call.
+     *
+     * @return the text of the reason message, null if not available
+     * @since 7.0.0
+     */
+    public String getStatusText() {
+        return statusText;
+    }
+
+    /**
      * Returns the headers from the response of the external call.
      *
      * @return the response headers
@@ -115,6 +153,8 @@ public class HttpParameters implements ExternalParameters {
         private String library;
         private URI uri;
         private String procedure;
+        private Integer statusCode;
+        private String statusText;
         private InboundHeaders inboundHeaders;
         private ExtendedInboundHeaders extendedInboundHeaders;
 
@@ -147,8 +187,17 @@ public class HttpParameters implements ExternalParameters {
             return this;
         }
 
+        @Override
+        public Build status(Integer statusCode, String statusText) {
+            this.statusCode = statusCode;
+            if (statusText != null && !statusText.isEmpty()) {
+                this.statusText = statusText;
+            }
+            return this;
+        }
+
         public HttpParameters build() {
-            return new HttpParameters(library, uri, procedure, inboundHeaders, extendedInboundHeaders);
+            return new HttpParameters(this);
         }
     }
 
@@ -201,7 +250,6 @@ public class HttpParameters implements ExternalParameters {
          * @return the completed HttpParameters object
          */
         Build extendedInboundHeaders(ExtendedInboundHeaders extendedInboundHeaders);
-
         /**
          * No inbound headers.
          *
@@ -211,6 +259,12 @@ public class HttpParameters implements ExternalParameters {
     }
 
     public interface Build {
+
+        /**
+         * Set the status code/text of the HTTP response.
+         * @return the builder in a buildable state.
+         */
+        Build status(Integer statusCode, String statusText);
 
         /**
          * Build the final {@link HttpParameters} for the API call.
