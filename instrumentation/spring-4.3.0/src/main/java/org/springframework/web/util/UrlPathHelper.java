183b522be30474352cd8d1fcda7e6c39b723aca3
tbradellis <bradellis10@gmail.com>
2021-04-16 16:12:19 -0700

    removed WebReqDispatcher change

--- instrumentation/spring-4.3.0/src/main/java/org/springframework/web/util/UrlPathHelper.java
+++ instrumentation/spring-4.3.0/src/main/java/org/springframework/web/util/UrlPathHelper.java
@@ -1,13 +1,11 @@
 package org.springframework.web.util;
 
 import com.newrelic.agent.bridge.AgentBridge;
-import com.newrelic.agent.bridge.TransactionNamePriority;
 import com.newrelic.api.agent.Transaction;
+import com.newrelic.api.agent.TransactionNamePriority;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
-import com.nr.agent.instrumentation.SpringControllerUtility;
 import com.nr.agent.instrumentation.SpringPlaceholderConfig;
-
 import javax.servlet.http.HttpServletRequest;
 
 @Weave
