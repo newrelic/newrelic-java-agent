1239e5362700d517f7be67d4a088c56be6274e68
Gerard Downes <gdownes@gmail.com>
2021-06-24 14:06:31 +0100

    Add missing copyright comment

--- instrumentation/sttp-3/src/main/scala/sttp/client/Http4sBackend_Instrumentation.scala
+++ instrumentation/sttp-3/src/main/scala/sttp/client/Http4sBackend_Instrumentation.scala
@@ -1,3 +1,10 @@
+/*
+ *
+ *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
 package sttp.client
 
 import cats.effect.{Blocker, ConcurrentEffect, ContextShift}
