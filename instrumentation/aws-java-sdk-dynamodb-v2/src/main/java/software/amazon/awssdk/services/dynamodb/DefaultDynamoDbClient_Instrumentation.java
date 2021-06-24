1054bfb8224328ab89257211fa76653eeb7071e5
Todd W Crone <twcrone@gmail.com>
2021-06-24 14:23:36 -0400

    Add more sync instrumentation

--- instrumentation/aws-java-sdk-dynamodb-v2/src/main/java/software/amazon/awssdk/services/dynamodb/DefaultDynamoDbClient_Instrumentation.java
+++ instrumentation/aws-java-sdk-dynamodb-v2/src/main/java/software/amazon/awssdk/services/dynamodb/DefaultDynamoDbClient_Instrumentation.java
@@ -8,8 +8,17 @@ import com.newrelic.api.agent.weaver.Weaver;
 import com.nr.instrumentation.dynamodb_v2.DynamoDBMetricUtil;
 import software.amazon.awssdk.core.client.config.SdkClientConfiguration;
 import software.amazon.awssdk.core.client.config.SdkClientOption;
+import software.amazon.awssdk.services.dynamodb.model.CreateTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.CreateTableResponse;
+import software.amazon.awssdk.services.dynamodb.model.DeleteTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.DeleteTableResponse;
+import software.amazon.awssdk.services.dynamodb.model.DescribeTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.DescribeTableResponse;
 import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.GetItemResponse;
+import software.amazon.awssdk.services.dynamodb.model.ListTablesRequest;
+import software.amazon.awssdk.services.dynamodb.model.ListTablesResponse;
+import software.amazon.awssdk.services.dynamodb.model.ScanResponse;
 
 import java.net.URI;
 
@@ -20,9 +29,48 @@ final class DefaultDynamoDbClient_Instrumentation {
     @Trace
     public GetItemResponse getItem(GetItemRequest getItemRequest) {
         URI endpoint = clientConfiguration.option(SdkClientOption.ENDPOINT);
-        System.out.println("logging sync client endpoint: " + endpoint.toString());
-        DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "scan", getItemRequest.tableName(), endpoint);
+        System.out.println("getItem -> sync client endpoint: " + endpoint.toString());
+        DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "getItem", getItemRequest.tableName(), endpoint);
         return Weaver.callOriginal();
     }
 
+    @Trace
+    public ListTablesResponse listTables(ListTablesRequest request) {
+        URI endpoint = clientConfiguration.option(SdkClientOption.ENDPOINT);
+        System.out.println("listTables -> sync client endpoint: " + endpoint.toString());
+        DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "listTables", request.exclusiveStartTableName(), endpoint);
+        return Weaver.callOriginal();
+    }
+
+    @Trace
+    public CreateTableResponse createTable(CreateTableRequest request) {
+        URI endpoint = clientConfiguration.option(SdkClientOption.ENDPOINT);
+        System.out.println("createTable -> sync client endpoint: " + endpoint.toString());
+        DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "createTable", request.tableName(), endpoint);
+        return Weaver.callOriginal();
+    }
+
+    @Trace
+    public DeleteTableResponse deleteTable(DeleteTableRequest request) {
+        URI endpoint = clientConfiguration.option(SdkClientOption.ENDPOINT);
+        System.out.println("deleteTable -> sync client endpoint: " + endpoint.toString());
+        DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "deleteTable", request.tableName(), endpoint);
+        return Weaver.callOriginal();
+    }
+
+    @Trace
+    public DescribeTableResponse describeTable(DescribeTableRequest request) {
+        URI endpoint = clientConfiguration.option(SdkClientOption.ENDPOINT);
+        System.out.println("describeTable -> sync client endpoint: " + endpoint.toString());
+        DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "describeTable", request.tableName(), endpoint);
+        return Weaver.callOriginal();
+    }
+
+    @Trace
+    public ScanResponse scan(DescribeTableRequest request) {
+        URI endpoint = clientConfiguration.option(SdkClientOption.ENDPOINT);
+        System.out.println("scan -> sync client endpoint: " + endpoint.toString());
+        DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "scan", request.tableName(), endpoint);
+        return Weaver.callOriginal();
+    }
 }
