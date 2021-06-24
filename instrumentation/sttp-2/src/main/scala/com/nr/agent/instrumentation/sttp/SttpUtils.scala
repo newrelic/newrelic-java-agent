c49b63e9856ab66d88e8799ae2e328d0b16688d5
Gerard Downes <gdownes@gmail.com>
2021-06-24 13:42:21 +0100

    Add instrumentation for the Scala HTTP client sttp. Instrumentation to capture send requests for three sttp backends, HttpURLConnectionBackend, Http4sBackend & AkkaHttpBackend

---
+++ instrumentation/sttp-2/src/main/scala/com/nr/agent/instrumentation/sttp/SttpUtils.scala
@@ -0,0 +1,34 @@
+/*
+ *
+ *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package com.nr.agent.instrumentation.sttp
+
+import com.newrelic.api.agent.{HttpParameters, NewRelic, Segment, TransactionNamePriority}
+import sttp.client3.{Request, Response}
+
+import java.net.URI
+
+
+object SttpUtils {
+
+  def startSegment[R, T](request: Request[T, R]) = {
+    NewRelic.getAgent.getTransaction.setTransactionName(TransactionNamePriority.FRAMEWORK_LOW, true, "Sttp", "send");
+    val segment = NewRelic.getAgent.getTransaction.startSegment("SttpBackend", "send")
+    segment.addOutboundRequestHeaders(new OutboundHttpHeaders(request))
+    segment
+  }
+
+  def finishSegment[R, T](request: Request[T, R], segment: Segment, response: Response[T]) = {
+    segment.reportAsExternal(HttpParameters
+      .library("Sttp")
+      .uri(new URI(request.uri.toString()))
+      .procedure(request.method.method)
+      .inboundHeaders(new InboundHttpHeaders(response))
+      .build())
+    segment.end()
+  }
+}
---
+++ instrumentation/sttp-2/src/main/scala/sttp/client/AkkaHttpBackend_Instrumentation.scala
@@ -0,0 +1,32 @@
+package sttp.client
+
+import akka.event.LoggingAdapter
+import akka.http.scaladsl.HttpsConnectionContext
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.model.ws.WebSocketRequest
+import akka.http.scaladsl.settings.ConnectionPoolSettings
+import com.newrelic.api.agent.weaver.Weaver
+import com.newrelic.api.agent.weaver.scala.{ScalaMatchType, ScalaWeave}
+import com.nr.agent.instrumentation.sttp.DelegateFutureAkkaStreams
+import sttp.capabilities.WebSockets
+import sttp.capabilities.akka.AkkaStreams
+import sttp.client3.akkahttp.AkkaHttpBackend.EncodingHandler
+import sttp.client3.{SttpBackend, SttpBackendOptions}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@ScalaWeave(`type` = ScalaMatchType.Object, `originalName` = "sttp.client3.akkahttp.AkkaHttpBackend")
+class AkkaHttpBackend_Instrumentation {
+  def apply(
+             options: SttpBackendOptions,
+             customHttpsContext: Option[HttpsConnectionContext],
+             customConnectionPoolSettings: Option[ConnectionPoolSettings],
+             customLog: Option[LoggingAdapter],
+             customizeRequest: HttpRequest => HttpRequest,
+             customizeWebsocketRequest: WebSocketRequest => WebSocketRequest,
+             customizeResponse: (HttpRequest, HttpResponse) => HttpResponse,
+             customEncodingHandler: EncodingHandler
+           )(implicit
+             ec: Option[ExecutionContext]
+           ): SttpBackend[Future, AkkaStreams with WebSockets] = new DelegateFutureAkkaStreams(Weaver.callOriginal())
+}
