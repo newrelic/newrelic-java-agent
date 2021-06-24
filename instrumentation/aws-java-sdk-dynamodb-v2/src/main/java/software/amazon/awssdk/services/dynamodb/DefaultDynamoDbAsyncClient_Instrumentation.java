26fd2f458f01d4558e2690200b2a12335ed1a4f1
Todd W Crone <twcrone@gmail.com>
2021-06-24 10:03:54 -0400

    Add a few more methods to instrument based on runner app

--- instrumentation/aws-java-sdk-dynamodb-v2/src/main/java/software/amazon/awssdk/services/dynamodb/DefaultDynamoDbAsyncClient_Instrumentation.java
+++ instrumentation/aws-java-sdk-dynamodb-v2/src/main/java/software/amazon/awssdk/services/dynamodb/DefaultDynamoDbAsyncClient_Instrumentation.java
@@ -8,10 +8,18 @@ import com.newrelic.api.agent.weaver.Weaver;
 import com.nr.instrumentation.dynamodb_v2.DynamoDBMetricUtil;
 import software.amazon.awssdk.core.client.config.SdkClientConfiguration;
 import software.amazon.awssdk.core.client.config.SdkClientOption;
+import software.amazon.awssdk.services.dynamodb.model.CreateTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.CreateTableResponse;
 import software.amazon.awssdk.services.dynamodb.model.DeleteItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.DeleteItemResponse;
+import software.amazon.awssdk.services.dynamodb.model.DeleteTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.DeleteTableResponse;
+import software.amazon.awssdk.services.dynamodb.model.DescribeTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.DescribeTableResponse;
 import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.GetItemResponse;
+import software.amazon.awssdk.services.dynamodb.model.ListTablesRequest;
+import software.amazon.awssdk.services.dynamodb.model.ListTablesResponse;
 import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.PutItemResponse;
 import software.amazon.awssdk.services.dynamodb.model.ScanRequest;
@@ -24,10 +32,10 @@ import java.util.concurrent.CompletableFuture;
 final class DefaultDynamoDbAsyncClient_Instrumentation {
 
     private final SdkClientConfiguration clientConfiguration = Weaver.callOriginal();
-    private final URI endpoint = clientConfiguration != null ? clientConfiguration.option(SdkClientOption.ENDPOINT) : null;
 
     @Trace
     public CompletableFuture<ScanResponse> scan(ScanRequest scanRequest) {
+        URI endpoint = clientConfiguration != null ? clientConfiguration.option(SdkClientOption.ENDPOINT) : null;
         System.out.println("-> scan async on endpoint: " + endpoint.toString());
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "scan", scanRequest.tableName(), endpoint);
         return Weaver.callOriginal();
@@ -35,6 +43,7 @@ final class DefaultDynamoDbAsyncClient_Instrumentation {
 
     @Trace
     public CompletableFuture<PutItemResponse> putItem(PutItemRequest request) {
+        URI endpoint = clientConfiguration != null ? clientConfiguration.option(SdkClientOption.ENDPOINT) : null;
         System.out.println("-> putItem async on endpoint: " + endpoint.toString());
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "putItem", request.tableName(), endpoint);
         return Weaver.callOriginal();
@@ -42,6 +51,7 @@ final class DefaultDynamoDbAsyncClient_Instrumentation {
 
     @Trace
     public CompletableFuture<GetItemResponse> getItem(GetItemRequest request) {
+        URI endpoint = clientConfiguration != null ? clientConfiguration.option(SdkClientOption.ENDPOINT) : null;
         System.out.println("-> getItem async on endpoint: " + endpoint.toString());
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "getItem", request.tableName(), endpoint);
         return Weaver.callOriginal();
@@ -49,8 +59,41 @@ final class DefaultDynamoDbAsyncClient_Instrumentation {
 
     @Trace
     public CompletableFuture<DeleteItemResponse> deleteItem(DeleteItemRequest request) {
+        URI endpoint = clientConfiguration != null ? clientConfiguration.option(SdkClientOption.ENDPOINT) : null;
         System.out.println("-> deleteItem async on endpoint: " + endpoint.toString());
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "deleteItem", request.tableName(), endpoint);
+        return Weaver.callOriginal();
+    }
+
+    @Trace
+    public CompletableFuture<ListTablesResponse> listTables(ListTablesRequest request) {
+        URI endpoint = clientConfiguration != null ? clientConfiguration.option(SdkClientOption.ENDPOINT) : null;
+        System.out.println("-> listTables async on endpoint: " + endpoint.toString());
+        DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "listTables", request.exclusiveStartTableName(), endpoint);
+        return Weaver.callOriginal();
+    }
+
+    @Trace
+    public CompletableFuture<DescribeTableResponse> describeTable(DescribeTableRequest request) {
+        URI endpoint = clientConfiguration != null ? clientConfiguration.option(SdkClientOption.ENDPOINT) : null;
+        System.out.println("-> describeTable async on endpoint: " + endpoint.toString());
+        DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "describeTable", request.tableName(), endpoint);
+        return Weaver.callOriginal();
+    }
+
+    @Trace
+    public CompletableFuture<CreateTableResponse> createTable(CreateTableRequest request) {
+        URI endpoint = clientConfiguration != null ? clientConfiguration.option(SdkClientOption.ENDPOINT) : null;
+        System.out.println("-> createTable async on endpoint: " + endpoint.toString());
+        DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "createTable", request.tableName(), endpoint);
+        return Weaver.callOriginal();
+    }
+
+    @Trace
+    public CompletableFuture<DeleteTableResponse> deleteTable(DeleteTableRequest request) {
+        URI endpoint = clientConfiguration != null ? clientConfiguration.option(SdkClientOption.ENDPOINT) : null;
+        System.out.println("-> deleteTable async on endpoint: " + endpoint.toString());
+        DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "deleteTable", request.tableName(), endpoint);
         return Weaver.callOriginal();
     }
 }
