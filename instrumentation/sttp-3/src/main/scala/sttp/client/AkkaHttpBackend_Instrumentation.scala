2e45a469db4daee7c13e87b3466b921e435bf6ff
Gerard Downes <gdownes@gmail.com>
2021-06-24 14:06:31 +0100

    Add missing copyright comment

--- instrumentation/sttp-3/src/main/scala/sttp/client/AkkaHttpBackend_Instrumentation.scala
+++ instrumentation/sttp-3/src/main/scala/sttp/client/AkkaHttpBackend_Instrumentation.scala
@@ -1,3 +1,10 @@
+/*
+ *
+ *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
 package sttp.client
 
 import akka.event.LoggingAdapter
