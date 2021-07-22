6e5c9f42de7ca01242bd3457a17fadb0be53f574
richardgibson <rickityg@gmail.com>
2021-07-22 15:16:19 +0100

    HTTP4S blaze client instrumentation

---
+++ instrumentation/http4s-blaze-client-2.13_0.21/src/main/scala/com/nr/instrumentation/http4s/InboundResponseWrapper.scala
@@ -0,0 +1,32 @@
+package com.nr.instrumentation.http4s
+
+import com.newrelic.api.agent.{ExtendedInboundHeaders, HeaderType}
+import org.http4s.Response
+import org.http4s.util.CaseInsensitiveString
+
+import scala.jdk.CollectionConverters._
+import java.util
+
+class InboundResponseWrapper[F[_]](response: Response[F]) extends ExtendedInboundHeaders {
+  /**
+    * Return the type of header key syntax used for this.
+    *
+    * @return An <code>enum</code> specifying the type of headers present.
+    * @since 3.5.0
+    */
+  override def getHeaderType: HeaderType = HeaderType.HTTP
+
+  /**
+    * Returns the value of the specified request header as a <code>String</code>. If the request does not include a
+    * header with the specified input name, then this method returns <code>null</code>.
+    *
+    * @param name The name of the desired request header.
+    * @return A <code>String</code> containing the value of the specified input request header, or <code>null</code> if the request header is not present.
+    * @since 3.5.0
+    */
+  override def getHeader(name: String): String =
+    response.headers.find(_.name == CaseInsensitiveString(name)).map(_.value).orNull
+
+  override def getHeaders(name: String): util.List[String] =
+    response.headers.toList.map(_.name.toString()).asJava
+}
