56792b886d242a6ccd5f02ef2832706d61a7f00c
Todd W Crone <twcrone@gmail.com>
2021-07-22 08:59:54 -0400

    Move module directory

--- instrumentation/graphql-java-16.2/src/main/java/graphql/GraphQL_Instrumentation.java
+++ instrumentation/graphql-java-16.2/src/main/java/graphql/GraphQL_Instrumentation.java
@@ -1,11 +1,12 @@
 package graphql;
 
+import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.TracedMethod;
 import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
-import com.nr.instrumentation.graphql.GraphQLInstrumentationUtil;
-import graphql.execution.ExecutionContext;
+import com.nr.instrumentation.graphql.GraphQLMetricUtil;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -14,13 +15,21 @@ public class GraphQL_Instrumentation {
 
     @Trace
     public CompletableFuture<ExecutionResult> executeAsync(ExecutionInput executionInput) {
-        System.out.println("Weaving execute() query = [" + executionInput.getQuery() + "]");
+//        System.out.println(executionInput.getContext().getClass().getCanonicalName());
+        System.out.println("Weaving execute() " + executionInput.getQuery());
+        String operation = executionInput.getOperationName();
+        String transactionName = "post " + (operation != null ? operation : "anonymous");
+        NewRelic.setTransactionName("GraphQL", transactionName);
+        TracedMethod tracedMethod = NewRelic.getAgent().getTracedMethod();
+        GraphQLMetricUtil.metrics(tracedMethod, operation);
+//        GraphQLMetricUtil.metrics(tracedMethod, executionInput.getOperationName());
 //        util.instrumentExecutionContext((ExecutionContext) executionInput.getContext());
 //        CompletableFuture<ExecutionResult> cfResult = Weaver.callOriginal();
 //        if(cfResult != null) {
 //            cfResult.thenAccept(result -> util.instrumentExecutionResult(result, executionInput));
 //        }
 //        return cfResult;
+        System.out.println("Done.");
         return Weaver.callOriginal();
     }
 
