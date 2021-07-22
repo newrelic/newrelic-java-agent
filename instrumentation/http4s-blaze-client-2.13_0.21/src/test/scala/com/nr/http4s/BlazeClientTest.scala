33a1d01a39648664283f8db55502440c2c57e9cd
richardgibson <rickityg@gmail.com>
2021-07-22 15:16:19 +0100

    HTTP4S blaze client instrumentation

---
+++ instrumentation/http4s-blaze-client-2.13_0.21/src/test/scala/com/nr/http4s/BlazeClientTest.scala
@@ -0,0 +1,42 @@
+package com.nr.http4s
+
+import cats.effect.{ContextShift, IO, Timer}
+import com.newrelic.agent.introspec.internal.HttpServerRule
+import com.newrelic.agent.introspec.{InstrumentationTestConfig, InstrumentationTestRunner, Introspector}
+import com.nr.http4s.Http4sTestUtils.{getSegments, getTraces, makeRequest}
+import org.junit.runner.RunWith
+import org.junit.{Assert, Rule, Test}
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.global
+import scala.concurrent.duration.DurationInt
+
+
+@RunWith(classOf[InstrumentationTestRunner])
+@InstrumentationTestConfig(includePrefixes = Array("org.http4s", "com.newrelic.cats.api"))
+class BlazeClientTest {
+
+  @Rule
+  def server: HttpServerRule = _server
+
+  implicit val ec: ExecutionContext = global
+  implicit val cs: ContextShift[IO] = IO.contextShift(global)
+  implicit val timer: Timer[IO] = IO.timer(global)
+
+  val _server = new HttpServerRule()
+
+  @Test
+  def blazeClientTest: Unit = {
+
+    val introspector: Introspector = InstrumentationTestRunner.getIntrospector
+    val response = makeRequest(s"${server.getEndPoint}?no-transaction=1").unsafeRunTimed(2.seconds)
+
+
+    val traces = getTraces(introspector)
+    val segments = getSegments(traces)
+    val txnCount = introspector.getFinishedTransactionCount()
+    Assert.assertTrue("Web request successful", response.isDefined)
+//    Assert.assertEquals("Transaction count correct", 1, txnCount)
+//    Assert.assertEquals("Trace present", 1, traces.size)
+  }
+}
