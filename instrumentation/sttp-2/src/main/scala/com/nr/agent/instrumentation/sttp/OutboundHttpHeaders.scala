963119c152d14bfdbdea40605d380c3dcf79b579
Gerard Downes <gdownes@gmail.com>
2021-07-22 14:11:38 +0100

    Include comment to outline no-op

--- instrumentation/sttp-2/src/main/scala/com/nr/agent/instrumentation/sttp/OutboundHttpHeaders.scala
+++ instrumentation/sttp-2/src/main/scala/com/nr/agent/instrumentation/sttp/OutboundHttpHeaders.scala
@@ -9,12 +9,13 @@ package com.nr.agent.instrumentation.sttp
 
 import com.newrelic.api.agent.{HeaderType, OutboundHeaders}
 import sttp.client.Request
-import sttp.model.Header
 
 class OutboundHttpHeaders[T, S](val request: Request[T, S]) extends OutboundHeaders {
   override def getHeaderType = HeaderType.HTTP
 
-  override def setHeader(name: String, value: String): Unit = {
-    request.headers :+ Header(name, value)
-  }
+  /**
+    * Sets a response header with the given name and value.
+    * NO-OP Sttp Request Headers are immutable and so can't be set from here
+    */
+  override def setHeader(name: String, value: String): Unit = request.header(name, value)
 }
