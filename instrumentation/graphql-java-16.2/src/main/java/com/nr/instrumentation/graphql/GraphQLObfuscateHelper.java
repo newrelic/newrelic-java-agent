ed002b82d9864fcd904b3f3a741c7d365778313c
xxia <xxia@newrelic.com>
2021-08-02 22:50:16 -0700

    WIP refactor obfuscate code into helper

--- instrumentation/graphql-java-16.2/src/main/java/com/nr/instrumentation/graphql/GraphQLObfuscateHelper.java
+++ instrumentation/graphql-java-16.2/src/main/java/com/nr/instrumentation/graphql/GraphQLObfuscateHelper.java
@@ -1,8 +1,53 @@
 package com.nr.instrumentation.graphql;
 
+import graphql.language.*;
+
+import java.util.List;
+
 public class GraphQLObfuscateHelper {
-    public static String obfuscate(String query) {
-        return query;
+    public static String obfuscate(Document document) {
+        StringBuilder queryBuilder = new StringBuilder();
+
+        //document.getDefinitions().get(0) - the OperationDefinition.
+        //How is it possible to get a list of definitions??? Research this.
+        OperationDefinition operationDefinition = null;
+        if(!document.getDefinitions().isEmpty()){
+            operationDefinition = (OperationDefinition) document.getDefinitions().get(0);
+            queryBuilder.append(operationDefinition.getOperation().name());
+            queryBuilder.append(" ");
+            queryBuilder.append(operationDefinition.getName() == null ? "" : operationDefinition.getName());
+            queryBuilder.append("{");
+
+            //At this point of the first layer, the structure repeats into the layers.
+            //List<Fields -> Field -> SelectionSet -> List<Fields> -> Field -> SelectionSet
+            List<Node> fields = operationDefinition.getSelectionSet().getChildren();
+            return buildGraph(queryBuilder, fields, 1).append("\n").append("}").toString();
+        }
+        return "no document definition found";
+    }
+
+    private static StringBuilder buildGraph(StringBuilder builder, List<Node> fields, int queryLayer) {
+        String indent = new String(new char[queryLayer * 2]).replace("\0", " ");
+        for (Node field : fields) {
+            Field castField = (Field) field;
+            SelectionSet selectionSet = castField.getSelectionSet();
+            if (selectionSet == null) {
+                builder.append("\n");
+                builder.append(indent);
+
+                builder.append(castField.getName());
+            } else {
+                builder.append("\n");
+                builder.append(indent);
+                builder.append(castField.getName());
+                builder.append("{");
+                buildGraph(builder, selectionSet.getChildren(), ++queryLayer);
+                builder.append("\n");
+                builder.append(indent);
+                builder.append("}");
+            }
+        }
+        return builder;
     }
 }
 
--- instrumentation/graphql-java-16.2/src/test/java/com/nr/instrumentation/graphql/GraphQLTransactionNameTest.java
+++ instrumentation/graphql-java-16.2/src/test/java/com/nr/instrumentation/graphql/GraphQLTransactionNameTest.java
@@ -2,6 +2,7 @@ package com.nr.instrumentation.graphql;
 
 import graphql.language.*;
 import graphql.parser.Parser;
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
@@ -9,7 +10,6 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,58 +29,17 @@ public class GraphQLTransactionNameTest {
         }
     }
 
+    @Ignore
     @Test
     public void testBuildQuery() {
         //setup
         String testFileName = "buildQueryString";
-        String expectedTransactionName = "who cares";
         testFileName = TEST_DATA_DIR + "/" + testFileName.trim();
-        expectedTransactionName = expectedTransactionName.trim();
         //given
         Document document = parse(testFileName);
 
-        //StringBuilder
-        StringBuilder builder = new StringBuilder();
-        SelectionSet set = (SelectionSet) document.getChildren().get(0).getChildren().get(0);
-
-        //document.getDefinitions() - returns a list of definitions. In examples, I only see one definition. How cxan a query have multiple???
-
-        //document.getDefinitions().get(0) - the OperationDefinition
-        OperationDefinition opDef = (OperationDefinition) document.getDefinitions().get(0);
-        builder.append(opDef.getOperation().name());
-        builder.append(" ");
-        builder.append(opDef.getName() == null ? "" : opDef.getName());
-        builder.append("{");
-
-        //opDef.getSelectionSet().getChildren() - List<Fields> being queried in first layer
-        //At this point of the first layer, the structure is Field -> SelectionSet -> List<Fields>
-        List<Node> fields = opDef.getSelectionSet().getChildren();
-        String finalGraph = buildGraph(builder, fields, 1).append("\n").append("}").toString();
-        System.out.println("");
-    }
-
-    private StringBuilder buildGraph(StringBuilder builder, List<Node> fields, int queryLayer) {
-        String indent = new String(new char[queryLayer * 2]).replace("\0", " ");
-        for (Node field : fields) {
-            Field castField = (Field) field;
-            SelectionSet selectionSet = castField.getSelectionSet();
-            if (selectionSet == null) {
-                builder.append("\n");
-                builder.append(indent);
-
-                builder.append(castField.getName());
-            } else {
-                builder.append("\n");
-                builder.append(indent);
-                builder.append(castField.getName());
-                builder.append("{");
-                buildGraph(builder, selectionSet.getChildren(), ++queryLayer);
-                builder.append("\n");
-                builder.append(indent);
-                builder.append("}");
-            }
-        }
-        return builder;
+        String obfuscatedQuery = GraphQLObfuscateHelper.obfuscate(document);
+        System.out.println(obfuscatedQuery);
     }
 
     @ParameterizedTest
