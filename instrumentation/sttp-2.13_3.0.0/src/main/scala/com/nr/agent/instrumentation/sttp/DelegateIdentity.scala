2b056e074d61e0573fd8adba7733ab642ee48a21
Gerard Downes <gdownes@gmail.com>
2021-08-03 19:46:35 +0100

    Support 2.12

---
+++ instrumentation/sttp-2.13_3.0.0/src/main/scala/com/nr/agent/instrumentation/sttp/DelegateIdentity.scala
@@ -0,0 +1,24 @@
+/*
+ *
+ *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package com.nr.agent.instrumentation.sttp
+
+import com.nr.agent.instrumentation.sttp.SttpUtils.{finishSegment, startSegment}
+import sttp.capabilities.Effect
+import sttp.client3.{DelegateSttpBackend, Identity, Request, Response, SttpBackend}
+
+class DelegateIdentity(delegate: SttpBackend[Identity, Any]) extends DelegateSttpBackend[Identity, Any](delegate) {
+  override def send[T, R >: Any with Effect[Identity] ](request: Request[T, R]): Response[T] = {
+    val segment = startSegment(request)
+
+    val response = delegate.send(request)
+
+    finishSegment(request, segment, response)
+
+    response
+  }
+}
---
+++ instrumentation/sttp-http4s-2.13_3.3.0/src/main/scala/com/nr/agent/instrumentation/sttp/DelegateAsync.scala
@@ -0,0 +1,33 @@
+/*
+ *
+ *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package com.nr.agent.instrumentation.sttp
+
+import cats.effect.Async
+import com.nr.agent.instrumentation.sttp.SttpUtils.{finishSegment, startSegment}
+import sttp.capabilities.Effect
+import sttp.client3.{DelegateSttpBackend, Request, Response, SttpBackend}
+
+class DelegateAsync[F[_]: Async, WS](delegate: SttpBackend[F, WS]) extends DelegateSttpBackend[F, WS] (delegate) {
+
+  override def send[T, R >: WS with Effect[F]](request: Request[T, R]): F[Response[T]] = {
+    val segment = startSegment(request)
+
+    val handleResponse = delegate.send(request)
+
+    val handleError = Async[F].handleErrorWith(handleResponse)(f => {
+      segment.end()
+      Async[F].raiseError(f)
+    })
+    val response = Async[F].map(handleError)(response => {
+      finishSegment(request, segment, response)
+      response
+    })
+
+    response
+  }
+}
