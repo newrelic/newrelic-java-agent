c3b85ddc75799063cf42918e446ba0674fcb074b
xxia <xxia@newrelic.com>
2021-08-06 10:51:23 -0700

    instrument execute

--- instrumentation/graphql-java-16.2/src/main/java/graphql/ExecutionStrategy_Instrumentation.java
+++ instrumentation/graphql-java-16.2/src/main/java/graphql/ExecutionStrategy_Instrumentation.java
@@ -6,17 +6,39 @@ import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
+import com.nr.instrumentation.graphql.GraphQLTransactionName;
 import graphql.execution.ExecutionContext;
 import graphql.execution.ExecutionStrategyParameters;
 import graphql.execution.FieldValueInfo;
+import graphql.language.Document;
+import graphql.language.OperationDefinition;
 import graphql.schema.GraphQLObjectType;
 
 import java.util.concurrent.CompletableFuture;
 
 import static com.nr.instrumentation.graphql.GraphQLAttributeUtil.addAttributeForArgument;
+import static com.nr.instrumentation.graphql.GraphQLObfuscateUtil.obfuscateQuery;
+import static com.nr.instrumentation.graphql.GraphQLTransactionName.getFirstOperationDefinitionFrom;
+import static com.nr.instrumentation.graphql.GraphQLTransactionName.getOperationTypeFrom;
 
 @Weave(originalName = "graphql.execution.ExecutionStrategy", type = MatchType.BaseClass)
 public class ExecutionStrategy_Instrumentation {
+
+    @Trace
+    public CompletableFuture<ExecutionResult> execute(ExecutionContext executionContext, ExecutionStrategyParameters parameters) {
+        Document document = executionContext.getDocument();
+        String query = executionContext.getExecutionInput().getQuery();
+        String transactionName = GraphQLTransactionName.from(document);
+        NewRelic.setTransactionName("GraphQL", transactionName);
+        NewRelic.getAgent().getTracedMethod().setMetricName("GraphQL/operation" + transactionName);
+        OperationDefinition definition = getFirstOperationDefinitionFrom(document);
+        String operationName = definition.getName();
+        AgentBridge.privateApi.addTracerParameter("graphql.operation.type", definition != null ? getOperationTypeFrom(definition) : "Unavailable");
+        AgentBridge.privateApi.addTracerParameter("graphql.operation.name", operationName != null ? operationName  : "<anonymous>");
+        AgentBridge.privateApi.addTracerParameter("graphql.operation.query", obfuscateQuery(query));
+        return Weaver.callOriginal();
+    }
+
     @Trace
     protected CompletableFuture<FieldValueInfo> resolveFieldWithInfo(ExecutionContext executionContext, ExecutionStrategyParameters parameters) {
         NewRelic.getAgent().getTracedMethod().setMetricName("GraphQL/resolve/" + parameters.getPath().getSegmentName());
