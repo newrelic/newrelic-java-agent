6910ca45c43b6062e234896d98682863a762ed8b
xxia <xxia@newrelic.com>
2021-08-16 12:46:50 -0700

    prevent double reporting of errors at end of graphql request

--- instrumentation/graphql-java-16.2/src/main/java/graphql/GraphQL_Instrumentation.java
+++ instrumentation/graphql-java-16.2/src/main/java/graphql/GraphQL_Instrumentation.java
@@ -8,18 +8,12 @@ import com.newrelic.api.agent.weaver.Weaver;
 
 import java.util.concurrent.CompletableFuture;
 
-import static com.nr.instrumentation.graphql.GraphQLErrorUtil.maybeReportExecutionResultError;
-
 @Weave(originalName = "graphql.GraphQL", type = MatchType.ExactClass)
 public class GraphQL_Instrumentation {
 
     @Trace
     public CompletableFuture<ExecutionResult> executeAsync(ExecutionInput executionInput){
         NewRelic.getAgent().getTracedMethod().setMetricName("GraphQL/executeAsync");
-        CompletableFuture<ExecutionResult> executionResult = Weaver.callOriginal();
-        if(executionResult != null && executionResult.isDone()){
-            maybeReportExecutionResultError(executionResult);
-        }
-        return executionResult;
+        return Weaver.callOriginal();
     }
 }
