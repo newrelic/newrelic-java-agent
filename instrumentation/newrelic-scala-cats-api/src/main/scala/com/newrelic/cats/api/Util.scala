1cf4357b0e61e4cc0a059a670a9bdf088de923af
richardgibson <rickityg@gmail.com>
2021-09-14 15:58:18 +0100

    Cats effect 3 instrumnetation

--- instrumentation/newrelic-scala-cats-api/src/main/scala/com/newrelic/cats/api/Util.scala
+++ instrumentation/newrelic-scala-cats-api/src/main/scala/com/newrelic/cats/api/Util.scala
@@ -5,14 +5,19 @@ import cats.implicits._
 import com.newrelic.agent.bridge.{AgentBridge, ExitTracer}
 import com.newrelic.api.agent.NewRelic
 
+import java.util.logging.Level
+
 object Util {
 
   def wrapTrace[S, F[_]: Sync](body: F[S]): F[S] =
-    Sync[F].delay(AgentBridge.instrumentation.createScalaTxnTracer())
-      .redeemWith(
+    Sync[F].delay{
+      val tracer = AgentBridge.instrumentation.createScalaTxnTracer()
+      logThreadTxnInfo()
+      tracer
+    }.redeemWith(
         _ => body,
         tracer => for {
-          txn <- Sync[F].delay(NewRelic.getAgent.getTransaction)
+          _ <- Sync[F].delay(NewRelic.getAgent.getTransaction)
           res <- attachErrorEvent(body, tracer)
           _ <- Sync[F].delay(tracer.finish(172, null))
         } yield res
@@ -24,4 +29,12 @@ object Util {
       tracer.finish(throwable)
       Sync[F].raiseError(throwable)
     })
+
+
+  private def logThreadTxnInfo(): Unit = {
+    if (AgentBridge.getAgent.getLogger.isLoggable(Level.FINEST)) {
+      val txn = AgentBridge.getAgent.getTransaction(false)
+      AgentBridge.getAgent.getLogger.log(Level.FINEST, s"${Thread.currentThread().getName}: txn info $txn")
+    }
+  }
 }
