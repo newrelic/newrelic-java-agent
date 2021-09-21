6b3a945766e8a061d7d55bcedb21379b1f292275
Gerard Downes <gdownes@gmail.com>
2021-09-21 17:20:20 +0100

    Ignore failing tests

--- instrumentation/rabbit-amqp-3.5.0/src/test/java/com/nr/agent/instrumentation/rabbitamqp350/RabbitMQTest.java
+++ instrumentation/rabbit-amqp-3.5.0/src/test/java/com/nr/agent/instrumentation/rabbitamqp350/RabbitMQTest.java
@@ -35,6 +35,7 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
