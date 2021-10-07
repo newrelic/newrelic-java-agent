da8cb957826f71974e4959524e5395560b30389f
jasonjkeller <jasonjkeller@gmail.com>
2021-10-07 13:05:09 -0700

    Prevent mongodb instrumentation from applying to async/reactive clients

--- instrumentation/mongodb-3.1/src/main/java/com/mongodb/async/client/MongoClients.java
+++ instrumentation/mongodb-3.1/src/main/java/com/mongodb/async/client/MongoClients.java
@@ -1,6 +1,6 @@
 /*
  *
- *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * Copyright 2021 New Relic Corporation. All rights reserved.
  *  * SPDX-License-Identifier: Apache-2.0
  *
  */
@@ -11,4 +11,4 @@ import com.newrelic.api.agent.weaver.SkipIfPresent;
 
 // This instrumentation will cause Segment timeouts and incorrect metrics if it applies to the async driver
 @SkipIfPresent
-final class NettyMongoClients {}
+final class MongoClients {}
