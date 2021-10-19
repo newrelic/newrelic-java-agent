7a8cba5af86caaac71174f40dcae389fd90f1a81
xxia <xxia@newrelic.com>
2021-10-19 16:38:05 -0700

    rename method

--- newrelic-agent/src/main/java/com/newrelic/agent/config/AgentConfigImpl.java
+++ newrelic-agent/src/main/java/com/newrelic/agent/config/AgentConfigImpl.java
@@ -352,7 +352,7 @@ public class AgentConfigImpl extends BaseConfig implements AgentConfig {
         propsWithSystemProps.putAll(SystemPropertyFactory.getSystemPropertyProvider().getNewRelicEnvVarsWithoutPrefix());
         flatten("", propsWithSystemProps, flattenedProps);
         checkHighSecurityPropsInFlattened(flattenedProps);
-        maybeSetServerSpanHarvestLimit();
+        setServerSpanHarvestLimit();
         this.flattenedProperties = Collections.unmodifiableMap(flattenedProps);
         this.waitForTransactionsInMillis = getProperty(WAIT_FOR_TRANSACTIONS, DEFAULT_WAIT_FOR_TRANSACTIONS);
         this.customInstrumentationEditorAllowed = getProperty(LaspPolicies.LASP_CUSTOM_INSTRUMENTATION_EDITOR, !highSecurity);
@@ -527,7 +527,7 @@ public class AgentConfigImpl extends BaseConfig implements AgentConfig {
         }
     }
 
-    private void maybeSetServerSpanHarvestLimit() {
+    private void setServerSpanHarvestLimit() {
         Map<String, Object> spanEventHarvestLimits = getProperty(SERVER_SPAN_HARVEST_CONFIG);
         if (spanEventHarvestLimits != null) {
             Long harvestLimit = (Long) spanEventHarvestLimits.get(SERVER_SPAN_HARVEST_LIMIT);
