8f655972af2cf3abe58c14ad7caed2c0f0fd36de
richardgibson <rickityg@gmail.com>
2021-10-20 09:35:05 +0100

    Akka http bindFlow bug fix #449

--- instrumentation/akka-http-core-2.13_10.2.0/src/main/scala/com/nr/instrumentation/akkahttpcore/ResponseFuture.scala
+++ instrumentation/akka-http-core-2.13_10.2.0/src/main/scala/com/nr/instrumentation/akkahttpcore/ResponseFuture.scala
@@ -7,48 +7,15 @@
 
 package com.nr.instrumentation.akkahttpcore
 
-import java.util.logging.Level
 
 import akka.http.scaladsl.model.HttpResponse
 import com.newrelic.agent.bridge.{AgentBridge, Token}
-import com.newrelic.api.agent.{NewRelic, Transaction}
+import com.newrelic.api.agent.{ Transaction}
 import com.newrelic.api.agent.weaver.Weaver
 
 import scala.concurrent.{ExecutionContext, Future}
 
 object ResponseFuture {
 
-  def wrapResponse(token: Token)(implicit ec: ExecutionContext): (HttpResponse) => Future[HttpResponse] = { response:HttpResponse => {
-    Future {
-      var updatedResponse: HttpResponse = response
-      var localToken = token
 
-      try {
-        val txn: Transaction = localToken.getTransaction
-        if (txn != null) {
-          val wrappedResponse = new ResponseWrapper(response)
-          txn.setWebResponse(wrappedResponse)
-          txn.addOutboundResponseHeaders()
-          txn.markResponseSent()
-          updatedResponse = wrappedResponse.response
-
-          localToken.expire()
-          localToken = null
-        }
-      } catch {
-        case t: Throwable => AgentBridge.instrumentation.noticeInstrumentationError(t, Weaver.getImplementationTitle)
-          try {
-            localToken.expire()
-            localToken = null;
-          }
-          catch {
-            case t: Throwable => AgentBridge.instrumentation.noticeInstrumentationError(t, Weaver.getImplementationTitle)
-          }
-
-      }
-
-      updatedResponse
-    }
-  }
-  }
 }
