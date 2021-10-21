5b2b2a63542ca74c41330b97d668881c5c62daa8
tbradellis <bradellis10@gmail.com>
2021-10-21 10:21:12 -0700

    really drop exception on RespStatsImp

--- newrelic-agent/src/main/java/com/newrelic/agent/stats/ResponseTimeStatsImpl.java
+++ newrelic-agent/src/main/java/com/newrelic/agent/stats/ResponseTimeStatsImpl.java
@@ -7,11 +7,13 @@
 
 package com.newrelic.agent.stats;
 
+import com.newrelic.agent.Agent;
 import com.newrelic.agent.config.AgentConfigImpl;
 import com.newrelic.agent.util.TimeConversion;
 import com.newrelic.api.agent.NewRelic;
 
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 /**
  * This class is not thread-safe.
@@ -78,8 +80,8 @@ public class ResponseTimeStatsImpl extends AbstractStats implements ResponseTime
         if (NewRelic.getAgent().getConfig().getValue(AgentConfigImpl.METRIC_DEBUG, AgentConfigImpl.DEFAULT_METRIC_DEBUG)) {
             if (count < 0 || total < 0 || totalExclusive < 0 || sumOfSquares < 0) {
                 NewRelic.incrementCounter("Supportability/ResponseTimeStatsImpl/NegativeValue");
-                throw new IllegalArgumentException(
-                        " [count=" + count + ", total=" + total + ", totalExclusive=" + totalExclusive + ", sum of squares=" + sumOfSquares + "]");
+                Agent.LOG.log(Level.INFO, "Invalid count {0}, total {1}, totalExclusive {2}, or sum of squares {3}",
+                        count, total, totalExclusive, sumOfSquares);
             }
         }
 
