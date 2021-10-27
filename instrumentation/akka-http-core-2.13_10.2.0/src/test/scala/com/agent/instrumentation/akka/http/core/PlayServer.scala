63c95c53e287fb73dbeaa3d3c7aeb16ff0679b4e
xxia <xxia@newrelic.com>
2021-10-27 11:51:33 -0700

    Revert "Akka http bindFlow bug fix #449"
    This reverts commit 8f655972af2cf3abe58c14ad7caed2c0f0fd36de.

--- instrumentation/akka-http-core-2.13_10.2.0/src/test/scala/com/agent/instrumentation/akka/http/core/PlayServer.scala
+++ instrumentation/akka-http-core-2.13_10.2.0/src/test/scala/com/agent/instrumentation/akka/http/core/PlayServer.scala
@@ -12,8 +12,6 @@ import akka.event.Logging
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.HttpMethods._
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives.{complete, get, onSuccess, path}
-import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Source, _}
 import akka.util.Timeout
@@ -30,31 +28,36 @@ class PlayServer() {
   implicit val materializer = ActorMaterializer()
   implicit val timeout: Timeout = 3 seconds
 
+  val config = ConfigFactory.load()
+  val logger = Logging(system, getClass)
+
   var bindingFuture: Future[Http.ServerBinding] = _
 
   def start(port: Int, async: Boolean) = {
-    val requestHandler: HttpRequest => HttpResponse = {
-      case HttpRequest(GET, Uri.Path("/ping"), _, _, _) => HttpResponse(entity = "Boops!")
-    }
-    val asyncRequestHandler: HttpRequest => Future[HttpResponse] = {
-      case HttpRequest(GET, Uri.Path("/asyncPing"), _, _, _) => Future(HttpResponse(entity = "Hoops!"))
-    }
-    bindingFuture = if (async)
-      Http().bindAndHandleAsync(asyncRequestHandler, interface = "localhost", port)
-    else
-      Http().bindAndHandleSync(requestHandler, interface = "localhost", port)
-    Await.ready(bindingFuture, timeout.duration)
-  }
 
-  def startFromFlow(port: Int) = {
-    val routeFlow: Flow[HttpRequest, HttpResponse, _] = Route.handlerFlow(
-      path("ping") {
-        get(onSuccess(Future("Hoops"))(complete(_)))
+    if (async) {
+
+      val asyncRequestHandler: HttpRequest => Future[HttpResponse] = {
+        case HttpRequest(GET, Uri.Path("/asyncPing"), _, _, _) =>
+          Future[HttpResponse](HttpResponse(entity = "Hoops!"))
       }
-    )
-    bindingFuture = Http()
-      .bindAndHandle(routeFlow, "localhost", port)
-    Await.ready(bindingFuture, timeout.duration)
+
+      bindingFuture = Http().bindAndHandleAsync(asyncRequestHandler, interface = "localhost", port)
+
+    }
+    else {
+
+      val requestHandler: HttpRequest => HttpResponse = {
+        case HttpRequest(GET, Uri.Path("/ping"), _, _, _) =>
+          HttpResponse(entity = "Boops!")
+      }
+
+      bindingFuture = Http().bindAndHandleSync(requestHandler, interface = "localhost", port)
+    }
+
+    Await.ready({
+      bindingFuture
+    }, timeout.duration)
   }
 
   def stop() = {
