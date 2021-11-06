dea937bfae8128e673e6a1cd76cd93c907b7ae16
jasonjkeller <jasonjkeller@gmail.com>
2021-11-05 17:39:01 -0700

    Update netty-reactor modules to fix token linking across contexts

--- instrumentation/netty-reactor-0.9.0/src/main/java/com/nr/instrumentation/reactor/netty/TokenLinkingSubscriber.java
+++ instrumentation/netty-reactor-0.9.0/src/main/java/com/nr/instrumentation/reactor/netty/TokenLinkingSubscriber.java
@@ -17,8 +17,14 @@ import reactor.util.context.Context;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
-// Based on OpenTelemetry code
-// https://github.com/open-telemetry/opentelemetry-java-instrumentation/blob/main/instrumentation/reactor-3.1/library/src/main/java/io/opentelemetry/instrumentation/reactor/TracingSubscriber.java
+/**
+ * Implementation of a reactor.core.CoreSubscriber (a Context aware subscriber) that can be added as
+ * a lifecycle hook on Flux/Mono operators to propagate, retrieve, and link Tokens across async contexts.
+ *
+ * Based on OpenTelemetry code:
+ * https://github.com/open-telemetry/opentelemetry-java-instrumentation/blob/main/instrumentation/reactor-3.1/library/src/main/java/io/opentelemetry/instrumentation/reactor/TracingSubscriber.java
+ * @param <T>
+ */
 public class TokenLinkingSubscriber<T> implements CoreSubscriber<T> {
     private final Token token;
     private final Subscriber<? super T> subscriber;
--- instrumentation/netty-reactor-0.9.0/src/main/java/reactor/core/scheduler/SchedulerTask_Instrumentation.java
+++ instrumentation/netty-reactor-0.9.0/src/main/java/reactor/core/scheduler/SchedulerTask_Instrumentation.java
@@ -10,24 +10,14 @@ package reactor.core.scheduler;
 import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
-import com.nr.instrumentation.reactor.netty.TokenLinkingSubscriber;
-import reactor.core.publisher.Hooks;
-import reactor.core.publisher.Hooks_Instrumentation;
-
-import static com.nr.instrumentation.reactor.netty.TokenLinkingSubscriber.tokenLift;
 
 @Weave(originalName = "reactor.core.scheduler.SchedulerTask")
 final class SchedulerTask_Instrumentation {
 
     // We need to be able to link the Token here when executing on a supplied Scheduler via Mono::publishOn
+    // A Token should be available on the thread that this task executes on if tokenLift() was added to Hooks.onEachOperator
     @Trace(async = true, excludeFromTransactionTrace = true)
     public Void call() {
-        // Add tokenLift hook if it hasn't already been added. This allows for tokens to be retrieved from
-        // the current context and linked across threads at various points of the Flux/Mono lifecycle.
-        if (!Hooks_Instrumentation.instrumented.getAndSet(true)) {
-            Hooks.onEachOperator(TokenLinkingSubscriber.class.getName(), tokenLift());
-        }
-
         return Weaver.callOriginal();
     }
 }
