3ee43db8540f732437319ed7bc241e61f6c401d4
xi xia <xxia@newrelic.com>
2021-11-30 19:30:52 -0800

    add constant for error message size

--- newrelic-agent/src/main/java/com/newrelic/agent/config/ConfigConstant.java
+++ newrelic-agent/src/main/java/com/newrelic/agent/config/ConfigConstant.java
@@ -10,4 +10,5 @@ package com.newrelic.agent.config;
 public class ConfigConstant {
     public static final int MAX_USER_ATTRIBUTES = 64;
     public static final int MAX_USER_ATTRIBUTE_SIZE = 255;
+    public static final int MAX_ERROR_MESSAGE_SIZE = 1024;
 }
