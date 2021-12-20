dc8b989036563ae87b0171be61182500eecbfe4a
Josep Subirats Castell <jsubirats@newrelic.com>
2021-12-20 14:02:50 +0100

    Fixes instrumentation ranges, simplifies instrumentation for better performance

--- instrumentation/logback-classic-1.2/src/main/java/com/nr/agent/instrumentation/logbackclassic12/Logger_Instrumentation.java
+++ instrumentation/logback-classic-1.2/src/main/java/com/nr/agent/instrumentation/logbackclassic12/Logger_Instrumentation.java
@@ -9,13 +9,11 @@ import org.slf4j.Marker;
 
 @Weave(originalName = "ch.qos.logback.classic.Logger", type = MatchType.Interface)
 public abstract class Logger_Instrumentation {
-    private static final String LOG_LINES_METRIC = "Logging/lines";
-    private static final String LOG_LINES_BY_LEVEL_METRIC_TEMPLATE = "Logging/lines/%s";
 
     private void buildLoggingEventAndAppend(final String localFQCN, final Marker marker, final Level level, final String msg, final Object[] params,
             final Throwable t) {
         Weaver.callOriginal();
-        NewRelic.incrementCounter(LOG_LINES_METRIC);
-        NewRelic.incrementCounter(String.format(LOG_LINES_BY_LEVEL_METRIC_TEMPLATE, level));
+        NewRelic.incrementCounter("Logging/lines");
+        NewRelic.incrementCounter(String.format("Logging/lines/%s", level));
     }
 }
