f82918b65351ba0f442652d8e3039a3cfaf01e45
richardgibson <rickityg@gmail.com>
2022-01-04 16:52:34 +0000

    issue 349 completable future tokens not expired correctly

---
+++ instrumentation/java.completable-future-jdk8/src/main/java/util/TokenAwareRunnable.java
@@ -0,0 +1,33 @@
+package util;
+
+import com.newrelic.agent.bridge.AgentBridge;
+
+import static util.TokenAndRefUtils.*;
+
+
+public final class TokenAwareRunnable implements Runnable {
+  private final Runnable delegate;
+
+  private AgentBridge.TokenAndRefCount tokenAndRefCount;
+
+  public TokenAwareRunnable(Runnable delegate) {
+    this.delegate = delegate;
+    //get token state from calling Thread
+    this.tokenAndRefCount = getThreadTokenAndRefCount();
+    logTokenInfo(tokenAndRefCount, "TokenAwareRunnable token info set");
+  }
+
+  @Override
+  public void run() {
+    try {
+      if (delegate != null) {
+        logTokenInfo(tokenAndRefCount, "Token info set in thread");
+        setThreadTokenAndRefCount(tokenAndRefCount);
+        delegate.run();
+      }
+    } finally {
+      logTokenInfo(tokenAndRefCount, "Clearing token info from thread ");
+      clearThreadTokenAndRefCountAndTxn(tokenAndRefCount);
+    }
+  }
+}
