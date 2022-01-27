6a0d0c5404efde95aa28232a8a1c985b028952e4
jasonjkeller <jasonjkeller@gmail.com>
2022-01-26 17:41:06 -0800

    Cleanup instrumentation

--- instrumentation/apache-log4j-2/src/main/java/com/nr/agent/instrumentation/log4j2/AgentUtil.java
+++ instrumentation/apache-log4j-2/src/main/java/com/nr/agent/instrumentation/log4j2/AgentUtil.java
@@ -37,25 +37,4 @@ public class AgentUtil {
             AgentBridge.getAgent().getLogSender().recordLogEvent(logEventMap);
         }
     }
-
-//    public static Map<String, String> createLogEventAttributesMap(LogEvent event) {
-//        Map<String, String> linkingMetadata = NewRelic.getAgent().getLinkingMetadata();
-//        // TODO omit attributes that don't have values
-//        //  {trace.id=, hostname=192.168.1.8, entity.type=SERVICE, entity.guid=MjIxMjg2NHxBUE18QVBQTElDQVRJT058MTY2NjIxNDQ3NQ, entity.name=SpringBoot PetClinic, span.id=}
-//        return linkingMetadata;
-//    }
-//
-//    public static Map<String, String> getLinkingMetadataAsMap() {
-//        Map<String, String> linkingMetadata = NewRelic.getAgent().getLinkingMetadata();
-//        // TODO omit attributes that don't have values
-//        //  {trace.id=, hostname=192.168.1.8, entity.type=SERVICE, entity.guid=MjIxMjg2NHxBUE18QVBQTElDQVRJT058MTY2NjIxNDQ3NQ, entity.name=SpringBoot PetClinic, span.id=}
-//        return linkingMetadata;
-//    }
-//
-//    public static String getLinkingMetadataAsString() {
-//        Map<String, String> linkingMetadata = NewRelic.getAgent().getLinkingMetadata();
-//        // TODO omit attributes that don't have values
-//        //  {trace.id=, hostname=192.168.1.8, entity.type=SERVICE, entity.guid=MjIxMjg2NHxBUE18QVBQTElDQVRJT058MTY2NjIxNDQ3NQ, entity.name=SpringBoot PetClinic, span.id=}
-//        return linkingMetadata.toString();
-//    }
 }
