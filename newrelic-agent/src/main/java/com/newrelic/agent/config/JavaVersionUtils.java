d2b25d70bb5acfedafff9ac510846325b0fc2cb4
Andre Onuki <otanuki@gmail.com>
2022-02-01 15:00:52 -0500

    JavaVersionUtil refactor

--- newrelic-agent/src/main/java/com/newrelic/agent/config/JavaVersionUtils.java
+++ newrelic-agent/src/main/java/com/newrelic/agent/config/JavaVersionUtils.java
@@ -58,7 +58,9 @@ public class JavaVersionUtils {
                     .append("Please use a 6.5.3 New Relic agent or a later version of Java.");
         } else if (EXCLUSIVE_MAX_JAVA_VERSION_PATTERN.matcher(javaSpecificationVersion).matches()) {
             message.append("Java version is: ").append(javaSpecificationVersion).append(". ");
-            message.append("This version of the New Relic Agent does not support versions of Java greater than 17.");
+            message.append("This version of the New Relic Agent does not support versions of Java greater than ");
+            message.append(MAX_SUPPORTED_VERSION);
+            message.append(".");
         }
         return message.toString();
     }
