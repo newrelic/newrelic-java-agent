3d7c764e556ac42705e9570772226aca6716c752
jasonjkeller <jasonjkeller@gmail.com>
2022-02-18 10:21:38 -0800

    Add instrumentation enabled supportability metrics

--- instrumentation/apache-log4j-2/src/main/java/org/apache/logging/log4j/core/config/LoggerConfig_Instrumentation.java
+++ instrumentation/apache-log4j-2/src/main/java/org/apache/logging/log4j/core/config/LoggerConfig_Instrumentation.java
@@ -2,9 +2,13 @@ package org.apache.logging.log4j.core.config;
 
 import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.NewField;
 import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.WeaveAllConstructors;
 import com.newrelic.api.agent.weaver.Weaver;
 import org.apache.logging.log4j.core.LogEvent;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static com.nr.agent.instrumentation.log4j2.AgentUtil.isApplicationLoggingEnabled;
 import static com.nr.agent.instrumentation.log4j2.AgentUtil.isApplicationLoggingForwardingEnabled;
@@ -13,6 +17,16 @@ import static com.nr.agent.instrumentation.log4j2.AgentUtil.recordNewRelicLogEve
 
 @Weave(originalName = "org.apache.logging.log4j.core.config.LoggerConfig", type = MatchType.ExactClass)
 public class LoggerConfig_Instrumentation {
+    @NewField
+    public static AtomicBoolean instrumented = new AtomicBoolean(false);
+
+    @WeaveAllConstructors
+    public LoggerConfig_Instrumentation() {
+        // Generate the instrumentation module supportability metric only once
+        if (!instrumented.getAndSet(true)) {
+            NewRelic.incrementCounter("Supportability/Logging/enabled/Java/Log4j2");
+        }
+    }
 
     protected void callAppenders(LogEvent event) {
         // Do nothing if application_logging.enabled: false
--- instrumentation/logback-classic-1.2/src/main/java/ch/qos/logback/classic/Logger_Instrumentation.java
+++ instrumentation/logback-classic-1.2/src/main/java/ch/qos/logback/classic/Logger_Instrumentation.java
@@ -2,13 +2,27 @@ package ch.qos.logback.classic;
 
 import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.NewField;
 import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.WeaveAllConstructors;
 import com.newrelic.api.agent.weaver.Weaver;
 import com.nr.agent.instrumentation.logbackclassic12.AgentUtil;
 import org.slf4j.Marker;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 @Weave(originalName = "ch.qos.logback.classic.Logger", type = MatchType.ExactClass)
 public abstract class Logger_Instrumentation {
+    @NewField
+    public static AtomicBoolean instrumented = new AtomicBoolean(false);
+
+    @WeaveAllConstructors
+    Logger_Instrumentation() {
+        // Generate the instrumentation module supportability metric only once
+        if (!instrumented.getAndSet(true)) {
+            NewRelic.incrementCounter("Supportability/Logging/enabled/Java/LogbackClassic1.2");
+        }
+    }
 
     private void buildLoggingEventAndAppend(final String localFQCN, final Marker marker, final Level level, final String msg, final Object[] params,
             final Throwable t) {
