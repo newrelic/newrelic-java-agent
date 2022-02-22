408f25e180843b8a3d59445f954c201856c2ce3e
Andrii Verkhovtsev <averkhovtsev@trading-point.com>
2022-02-22 23:15:02 +0200

    support kafka-clients version 3+

---
+++ instrumentation/kafka-clients-metrics-3.0.0/src/main/java/org/apache/kafka/clients/consumer/ConsumerRebalanceListener_Instrumentation.java
@@ -0,0 +1,37 @@
+/*
+ *
+ *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.apache.kafka.clients.consumer;
+
+import static com.nr.instrumentation.kafka.Metrics.REBALANCE_ASSIGNED_BASE;
+import static com.nr.instrumentation.kafka.Metrics.REBALANCE_REVOKED_BASE;
+
+import com.newrelic.api.agent.NewRelic;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import java.util.Collection;
+import org.apache.kafka.common.TopicPartition;
+
+@Weave(type = MatchType.Interface, originalName = "org.apache.kafka.clients.consumer.ConsumerRebalanceListener")
+public class ConsumerRebalanceListener_Instrumentation {
+
+    public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+        for (TopicPartition topicPartition : partitions) {
+            NewRelic.incrementCounter(REBALANCE_REVOKED_BASE + topicPartition.topic() + "/" + topicPartition.partition());
+        }
+        Weaver.callOriginal();
+    }
+
+    public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+        for (TopicPartition topicPartition : partitions) {
+            NewRelic.incrementCounter(REBALANCE_ASSIGNED_BASE + topicPartition.topic() + "/" + topicPartition.partition());
+        }
+        Weaver.callOriginal();
+    }
+
+}
