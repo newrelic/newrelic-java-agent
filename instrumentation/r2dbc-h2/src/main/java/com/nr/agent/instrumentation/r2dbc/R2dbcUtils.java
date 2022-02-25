312cec01d27c73ca61fcdfb99b245d14d70ea1e7
Gerard Downes <gdownes@gmail.com>
2022-02-25 12:39:26 +0000

    Only compute on valid segments

--- instrumentation/r2dbc-h2/src/main/java/com/nr/agent/instrumentation/r2dbc/R2dbcUtils.java
+++ instrumentation/r2dbc-h2/src/main/java/com/nr/agent/instrumentation/r2dbc/R2dbcUtils.java
@@ -1,5 +1,6 @@
 package com.nr.agent.instrumentation.r2dbc;
 
+import com.newrelic.agent.bridge.NoOpSegment;
 import com.newrelic.agent.bridge.datastore.DatastoreVendor;
 import com.newrelic.agent.bridge.datastore.JdbcHelper;
 import com.newrelic.agent.bridge.datastore.R2dbcObfuscator;
@@ -15,16 +16,18 @@ public class R2dbcUtils {
         Segment segment = NewRelic.getAgent().getTransaction().startSegment("execute");
         return request != null ? request
                 .doOnSubscribe((subscription) -> {
-                    String[] sqlOperationCollection = R2dbcOperation.extractFrom(sql);
-                    if(sqlOperationCollection != null) {
-                        segment.reportAsExternal(DatastoreParameters
-                                .product(DatastoreVendor.H2.name())
-                                .collection(sqlOperationCollection[1])
-                                .operation(sqlOperationCollection[0])
-                                .instance("localhost", JdbcHelper.parseInMemoryIdentifier(url))
-                                .databaseName(databaseName)
-                                .slowQuery(sql, R2dbcObfuscator.R2DBC_QUERY_CONVERTER)
-                                .build());
+                    if(segment != null && !(segment instanceof NoOpSegment)) {
+                        String[] sqlOperationCollection = R2dbcOperation.extractFrom(sql);
+                        if(sqlOperationCollection != null) {
+                            segment.reportAsExternal(DatastoreParameters
+                                    .product(DatastoreVendor.H2.name())
+                                    .collection(sqlOperationCollection[1])
+                                    .operation(sqlOperationCollection[0])
+                                    .instance("localhost", JdbcHelper.parseInMemoryIdentifier(url))
+                                    .databaseName(databaseName)
+                                    .slowQuery(sql, R2dbcObfuscator.R2DBC_QUERY_CONVERTER)
+                                    .build());
+                        }
                     }
                 })
                 .doFinally((type) -> segment.end()) : null;
