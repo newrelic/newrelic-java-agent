d7e2c271228a627b3a5416b64e6d7e896d173210
Brad Ellis <bradellis10@gmail.com>
2022-03-02 17:11:46 -0800

    Jedis 4.0.0 (#698)
    * open jedis version to verifyInstrumentation
    
    * jedis 4.0.0+ instrumentation
    
    * remove unnecessary trace
    
    * implement suggested changes
    
    * remove NewField usage from pubsub

---
+++ instrumentation/jedis-4.0.0/src/main/java/redis/clients/jedis/JedisPubSub_Instrumentation.java
@@ -0,0 +1,83 @@
+/*
+ *
+ *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package redis.clients.jedis;
+
+import com.newrelic.agent.bridge.datastore.DatastoreVendor;
+import com.newrelic.api.agent.DatastoreParameters;
+import com.newrelic.api.agent.NewRelic;
+import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+
+@SuppressWarnings({ "ResultOfMethodCallIgnored", "unused" })
+@Weave(type = MatchType.ExactClass, originalName = "redis.clients.jedis.JedisPubSub")
+public class JedisPubSub_Instrumentation {
+
+    private volatile Connection client;
+
+    public void proceed(Connection client, String... channels) {
+        Weaver.callOriginal();
+    }
+
+    public void proceedWithPatterns(Connection client, String... channels) {
+        Weaver.callOriginal();
+    }
+
+    @Trace
+    public void onMessage(String channel, String message) {
+        Weaver.callOriginal();
+
+        reportMethodAsExternal("message");
+    }
+
+    @Trace
+    public void onPMessage(String pattern, String channel, String message) {
+        Weaver.callOriginal();
+
+        reportMethodAsExternal("message");
+    }
+
+    @Trace
+    public void onSubscribe(String channel, int subscribedChannels) {
+        Weaver.callOriginal();
+
+        reportMethodAsExternal("subscribe");
+    }
+
+    @Trace
+    public void onUnsubscribe(String channel, int subscribedChannels) {
+        Weaver.callOriginal();
+
+        reportMethodAsExternal("unsubscribe");
+    }
+
+    @Trace
+    public void onPUnsubscribe(String pattern, int subscribedChannels) {
+        Weaver.callOriginal();
+
+        reportMethodAsExternal("unsubscribe");
+    }
+
+    @Trace
+    public void onPSubscribe(String pattern, int subscribedChannels) {
+        Weaver.callOriginal();
+
+        reportMethodAsExternal("subscribe");
+    }
+
+    private void reportMethodAsExternal(String commandName) {
+        NewRelic.getAgent().getTracedMethod().reportAsExternal(DatastoreParameters
+                .product(DatastoreVendor.Redis.name())
+                .collection(null)
+                .operation(commandName)
+                .instance(client.getHostAndPort().getHost(), client.getHostAndPort().getPort())
+                .build());
+
+    }
+}
