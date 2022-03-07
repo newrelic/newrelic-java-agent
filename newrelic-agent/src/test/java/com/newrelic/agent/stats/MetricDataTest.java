db6f65b0f34d591249399e9af04097817e1593dc
Todd W Crone <twcrone@gmail.com>
2022-03-07 13:10:38 -0500

    Not sure why they needed to set via reflection

--- newrelic-agent/src/test/java/com/newrelic/agent/stats/MetricDataTest.java
+++ newrelic-agent/src/test/java/com/newrelic/agent/stats/MetricDataTest.java
@@ -7,20 +7,18 @@
 
 package com.newrelic.agent.stats;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.lang.reflect.Field;
-import java.util.Arrays;
-
-import org.json.simple.JSONArray;
-import org.junit.Assert;
-import org.junit.Test;
-
 import com.newrelic.agent.MetricData;
 import com.newrelic.agent.MetricNames;
 import com.newrelic.agent.metric.MetricName;
 import com.newrelic.agent.transport.DataSenderWriter;
+import org.json.simple.JSONArray;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
 
 public class MetricDataTest {
 
@@ -49,12 +47,9 @@ public class MetricDataTest {
     }
 
     @Test
-    public void invalidStat() throws IllegalArgumentException, SecurityException, IllegalAccessException,
-            NoSuchFieldException {
+    public void invalidStat() throws IllegalArgumentException, SecurityException {
         StatsImpl stats = new StatsImpl();
-        Field countField = AbstractStats.class.getDeclaredField("count");
-        countField.setAccessible(true);
-        countField.set(stats, -1);
+        stats.setCallCount(-1);
         MetricData data = MetricData.create(MetricName.create("dude"), stats);
         String json = DataSenderWriter.toJSONString(data);
         Assert.assertEquals("[{\"name\":\"dude\"},[0,0,0,0,0,0]]", json);
