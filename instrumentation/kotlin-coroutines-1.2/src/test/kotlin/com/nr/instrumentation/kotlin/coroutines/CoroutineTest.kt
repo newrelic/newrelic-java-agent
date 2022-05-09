89cc938094084af7ac58b5e8fb4e40f064f45380
Todd W Crone <twcrone@gmail.com>
2022-05-09 11:41:32 -0400

    Incorporating more extensive tests from Richard...fails when segments are executed via different dispatchers

--- instrumentation/kotlin-coroutines-1.2/src/test/kotlin/com/nr/instrumentation/kotlin/coroutines/CoroutineTest.kt
+++ instrumentation/kotlin-coroutines-1.2/src/test/kotlin/com/nr/instrumentation/kotlin/coroutines/CoroutineTest.kt
@@ -1,7 +1,7 @@
 package com.nr.instrumentation.kotlin.coroutines
 
-import com.newrelic.agent.introspec.InstrumentationTestConfig
-import com.newrelic.agent.introspec.InstrumentationTestRunner
+import com.newrelic.agent.introspec.*
+import com.newrelic.api.agent.NewRelic
 import com.newrelic.api.agent.Trace
 import kotlinx.coroutines.*
 import org.junit.Assert
@@ -50,6 +50,79 @@ class CoroutineTest {
         val name = introspector.transactionNames.iterator().next()
         val expectedName = "OtherTransaction/Custom/com.nr.instrumentation.kotlin.coroutines.CoroutineTest/cancel"
         assertEquals("Expected transaction name", expectedName, name)
+    }
+
+    @Test
+    fun simpleCoroutineTest() {
+        val introspector = InstrumentationTestRunner.getIntrospector()
+        val result = runBlocking {
+            txn {
+                val i = trace("segment1") { 1 }
+                val j = trace("segment2") { 2 }
+                i + j
+            }
+        }
+        val txnCount = introspector.getFinishedTransactionCount()
+        val traces = getTraces(introspector)
+        val segments = getSegments(traces)
+
+        assertEquals("result correct", 3, result)
+        assertEquals("transaction finished", 1, txnCount)
+        assertSegmentPresent("segment1", segments)
+        assertSegmentPresent("segment2", segments)
+    }
+
+    @Test
+    fun threadSwitchCoroutineTest() {
+        val introspector = InstrumentationTestRunner.getIntrospector()
+        val result = runBlocking {
+            txn {
+                val i = withContext(Dispatchers.IO){trace("segment1") { 1 }}
+                val j = trace("segment2") { 2 }
+                i + j
+            }
+        }
+        val txnCount = introspector.getFinishedTransactionCount()
+        val traces = getTraces(introspector)
+        val segments = getSegments(traces)
+
+        assertEquals("result correct", 3, result)
+        assertEquals("transaction finished", 1, txnCount)
+        assertSegmentPresent("segment1", segments)
+        assertSegmentPresent("segment2", segments)
+    }
+
+    @Trace(dispatcher = true)
+    suspend fun <S> txn(block: suspend () -> S): S {
+        val txn = NewRelic.getAgent().transaction
+        return block()
+    }
+
+    suspend fun <S> trace(segmentName: String, block: suspend () -> S): S {
+        val txn = NewRelic.getAgent().getTransaction()
+        println("Thread ${Thread.currentThread().name}")
+        val segment = txn.startSegment(segmentName)
+        val evaluatedBlock = block()
+        segment.end()
+        return evaluatedBlock
+    }
+
+    fun getTraces(introspector: Introspector): Iterable<TransactionTrace> =
+            introspector.transactionNames.flatMap {
+                introspector.getTransactionTracesForTransaction(it)
+            }
+
+    fun getSegments(traces: Iterable<TransactionTrace>): List<TraceSegment> =
+            traces.flatMap { txnTrace -> getSegments(txnTrace.initialTraceSegment) }
+
+    fun getSegments(segment: TraceSegment): List<TraceSegment> {
+        val childSegments = segment.children.flatMap { getSegments(it) }
+        return childSegments + segment
+    }
+
+    fun assertSegmentPresent(segmentName: String, segments: List<TraceSegment>) {
+        val segment = segments.find { it.name == "Custom/$segmentName" }
+        Assert.assertNotNull("segment $segmentName not found", segment)
     }
 
     @Trace(dispatcher = true)
