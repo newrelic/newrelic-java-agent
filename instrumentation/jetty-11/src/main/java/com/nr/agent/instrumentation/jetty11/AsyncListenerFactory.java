8e47f84c38414624825b63a8035c4ba4bfdd7ef6
Brad Ellis <bradellis10@gmail.com>
2022-07-25 14:54:39 -0700

    jetty 11 winning (#946)

---
+++ instrumentation/jetty-11/src/main/java/com/nr/agent/instrumentation/jetty11/AsyncListenerFactory.java
@@ -0,0 +1,55 @@
+/*
+ *
+ *  * Copyright 2022 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package com.nr.agent.instrumentation.jetty11;
+
+import com.newrelic.agent.bridge.AgentBridge;
+import jakarta.servlet.AsyncEvent;
+import jakarta.servlet.AsyncListener;
+import org.eclipse.jetty.server.HttpChannelState;
+
+import java.io.IOException;
+
+public final class AsyncListenerFactory {
+
+    /*
+     * The public enum Action was added in 9.1 and does not exist in 9.0.*. So declaring a variable of this type is
+     * sufficient to prevent this instrumentation module from loading in 9.0.*.
+     */
+    public static HttpChannelState.Action isPostJetty90 = null;
+
+    private AsyncListenerFactory() {
+    }
+
+    private static final AsyncListener ASYNC_LISTENER = new AsyncListener() {
+
+        @Override
+        public void onComplete(AsyncEvent asyncEvent) throws IOException {
+            AgentBridge.asyncApi.completeAsync(asyncEvent.getAsyncContext());
+        }
+
+        @Override
+        public void onTimeout(AsyncEvent asyncEvent) throws IOException {
+            // do nothing
+        }
+
+        @Override
+        public void onError(AsyncEvent asyncEvent) throws IOException {
+            AgentBridge.asyncApi.errorAsync(asyncEvent.getAsyncContext(), asyncEvent.getThrowable());
+        }
+
+        @Override
+        public void onStartAsync(AsyncEvent asyncEvent) throws IOException {
+            // do nothing
+        }
+
+    };
+
+    public static AsyncListener getAsyncListener() {
+        return ASYNC_LISTENER;
+    }
+}
