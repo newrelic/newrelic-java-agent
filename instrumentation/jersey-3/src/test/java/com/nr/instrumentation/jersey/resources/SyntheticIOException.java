b6115976add65d77e82f66522fd786fac35a7849
tbradellis <bradellis10@gmail.com>
2022-07-26 15:03:48 -0700

    jersey 3 module

---
+++ instrumentation/jersey-3/src/test/java/com/nr/instrumentation/jersey/resources/SyntheticIOException.java
@@ -0,0 +1,17 @@
+/*
+ *
+ *  * Copyright 2022 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package com.nr.instrumentation.jersey.resources;
+
+import jakarta.ws.rs.NameBinding;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@NameBinding
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SyntheticIOException {
+}
---
+++ instrumentation/jersey-3/src/test/java/com/nr/instrumentation/jersey/resources/SyntheticIOExceptionWriterInterceptor.java
@@ -0,0 +1,29 @@
+/*
+ *
+ *  * Copyright 2022 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package com.nr.instrumentation.jersey.resources;
+
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.ext.Provider;
+import jakarta.ws.rs.ext.WriterInterceptor;
+import jakarta.ws.rs.ext.WriterInterceptorContext;
+import java.io.IOException;
+
+/**
+ * This WriterInterceptor simulates an IOException occurring when writing out a response to a client. This reproduces a
+ * bug in our instrumentation where a Segment is created but never finished, preventing the Transaction from finishing.
+ */
+@Provider
+@SyntheticIOException
+public class SyntheticIOExceptionWriterInterceptor implements WriterInterceptor {
+
+    @Override
+    public void aroundWriteTo(WriterInterceptorContext context) throws IOException, WebApplicationException {
+        throw new IOException("Simulated IOException when writing Response to client");
+    }
+
+}
