957f67cb17419f77e55244633a852357e569b298
Brad Ellis <bradellis10@gmail.com>
2022-07-25 20:54:19 -0700

    jaxrsws to jakartarsws (#952)

---
+++ instrumentation/jakarta-ws-rs-3.0.0/src/main/java/com/nr/instrumentation/jakarta/ws/rs/api/JakartaWsRsApi_Instrumentation.java
@@ -0,0 +1,105 @@
+/*
+ *
+ *  * Copyright 2022 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package com.nr.instrumentation.jakarta.ws.rs.api;
+
+import com.newrelic.agent.bridge.AgentBridge;
+import com.newrelic.agent.bridge.Transaction;
+import com.newrelic.agent.bridge.TransactionNamePriority;
+import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.WeaveIntoAllMethods;
+import com.newrelic.api.agent.weaver.WeaveWithAnnotation;
+import com.newrelic.api.agent.weaver.Weaver;
+
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.HEAD;
+import jakarta.ws.rs.OPTIONS;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import java.util.Queue;
+import java.util.logging.Level;
+
+@WeaveWithAnnotation(annotationClasses = { "jakarta.ws.rs.Path" }, type = MatchType.Interface)
+public class JakartaWsRsApi_Instrumentation {
+
+    @WeaveWithAnnotation(annotationClasses = { "jakarta.ws.rs.PUT", "jakarta.ws.rs.POST", "jakarta.ws.rs.GET",
+            "jakarta.ws.rs.DELETE", "jakarta.ws.rs.HEAD", "jakarta.ws.rs.OPTIONS", "jakarta.ws.rs.PATCH" })
+    @WeaveIntoAllMethods
+    @Trace(dispatcher = true)
+    private static void instrumentation() {
+        Transaction transaction = AgentBridge.getAgent().getWeakRefTransaction(false);
+        if (transaction != null) {
+            JakartaWsRsApiHelper.ResourcePath resourcePath = JakartaWsRsApiHelper.subresourcePath.get();
+            if (!transaction.equals(resourcePath.transaction)) {
+                resourcePath.pathQueue.clear();
+                resourcePath.transaction = transaction;
+            }
+
+            String rootPath = null;
+            Path rootPathAnnotation = Weaver.getClassAnnotation(Path.class);
+            if (rootPathAnnotation != null) {
+                rootPath = rootPathAnnotation.value();
+            }
+            String methodPath = null;
+            Path methodPathAnnotation = Weaver.getMethodAnnotation(Path.class);
+            if (methodPathAnnotation != null) {
+                methodPath = methodPathAnnotation.value();
+            }
+
+            String httpMethod = null;
+             /* Unfortunately we have to do this in a big if/else block because we can't currently support variables passed
+             into the Weaver.getMethodAnnotation method. We might be able to make an improvement to handle this in the future */
+            if (Weaver.getMethodAnnotation(PUT.class) != null) {
+                httpMethod = PUT.class.getSimpleName();
+            } else if (Weaver.getMethodAnnotation(POST.class) != null) {
+                httpMethod = POST.class.getSimpleName();
+            } else if (Weaver.getMethodAnnotation(GET.class) != null) {
+                httpMethod = GET.class.getSimpleName();
+            } else if (Weaver.getMethodAnnotation(DELETE.class) != null) {
+                httpMethod = DELETE.class.getSimpleName();
+            } else if (Weaver.getMethodAnnotation(HEAD.class) != null) {
+                httpMethod = HEAD.class.getSimpleName();
+            } else if (Weaver.getMethodAnnotation(OPTIONS.class) != null) {
+                httpMethod = OPTIONS.class.getSimpleName();
+            } else {
+                // PATCH annotation was added later so may not be available
+                try {
+                    if (Weaver.getMethodAnnotation(PATCH.class) != null) {
+                        httpMethod = PATCH.class.getSimpleName();
+                    }
+                } catch (NoClassDefFoundError e) {
+                }
+            }
+
+            if (httpMethod != null) {
+                Queue<String> subresourcePath = resourcePath.pathQueue;
+
+                String fullPath = JakartaWsRsApiHelper.getPath(rootPath, methodPath, httpMethod);
+                subresourcePath.offer(fullPath);
+
+                StringBuilder txNameBuilder = new StringBuilder();
+                String pathPart;
+                while ((pathPart = subresourcePath.poll()) != null) {
+                    txNameBuilder.append(pathPart);
+                }
+
+                transaction.setTransactionName(TransactionNamePriority.FRAMEWORK_HIGH, false, "RestWebService",
+                        txNameBuilder.toString());
+            } else {
+                String fullPath = JakartaWsRsApiHelper.getPath(rootPath, methodPath, null);
+                if (!resourcePath.pathQueue.offer(fullPath)) {
+                    AgentBridge.getAgent().getLogger().log(Level.FINE, "Jakarta-RS Subresource naming queue is full.");
+                    resourcePath.pathQueue.clear();
+                }
+            }
+        }
+    }
+}
---
+++ instrumentation/jakarta-ws-rs-3.0.0/src/test/java/com/nr/instrumentation/jakarta/ws/rs/api/SubResourceTest.java
@@ -0,0 +1,128 @@
+/*
+ *
+ *  * Copyright 2022 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package com.nr.instrumentation.jakarta.ws.rs.api;
+
+import com.newrelic.agent.introspec.InstrumentationTestConfig;
+import com.newrelic.agent.introspec.InstrumentationTestRunner;
+import com.newrelic.agent.introspec.Introspector;
+import com.newrelic.test.marker.Java7IncompatibleTest;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import jakarta.ws.rs.core.Application;
+
+import static org.junit.Assert.assertEquals;
+
+@Category({ Java7IncompatibleTest.class })
+@RunWith(InstrumentationTestRunner.class)
+@InstrumentationTestConfig(includePrefixes = { "org.glassfish", "com.nr.instrumentation.jakarta.ws" })
+public class SubResourceTest extends JerseyTest {
+
+    @Test
+    public void testGetById() {
+        final String id = target("customers/type-id/1").request().get(String.class);
+        assertEquals("one", id);
+
+        Introspector introspector = InstrumentationTestRunner.getIntrospector();
+        int finishedTransactionCount = introspector.getFinishedTransactionCount(30000);
+        assertEquals(1, finishedTransactionCount);
+        String transactionName = introspector.getTransactionNames().toArray()[0].toString();
+        assertEquals("WebTransaction/RestWebService/customers/type-{type}/{id} (GET)", transactionName);
+    }
+
+    @Test
+    public void testGetAllById() {
+        final String id = target("customers/type-id").request().get(String.class);
+        assertEquals("{ \"items\": [ \"one\", \"two\", \"three\" ] }", id);
+
+        Introspector introspector = InstrumentationTestRunner.getIntrospector();
+        int finishedTransactionCount = introspector.getFinishedTransactionCount(30000);
+        assertEquals(1, finishedTransactionCount);
+        String transactionName = introspector.getTransactionNames().toArray()[0].toString();
+        assertEquals("WebTransaction/RestWebService/customers/type-{type} (GET)", transactionName);
+    }
+
+    @Test
+    public void testGetByFirstLast() {
+        final String id = target("customers/type-firstLast/first-last").request().get(String.class);
+        assertEquals("FirstName, LastName", id);
+
+        Introspector introspector = InstrumentationTestRunner.getIntrospector();
+        int finishedTransactionCount = introspector.getFinishedTransactionCount(30000);
+        assertEquals(1, finishedTransactionCount);
+        String transactionName = introspector.getTransactionNames().toArray()[0].toString();
+        assertEquals("WebTransaction/RestWebService/customers/type-{type}/{first}-{last} (GET)", transactionName);
+    }
+
+    @Test
+    public void testGetAllByFirstLast() {
+        final String id = target("customers/type-firstLast").request().get(String.class);
+        assertEquals("{ \"names\": [ \"FirstName, LastName\", \"SecondName, LastName\", \"ThirdName, LastName\" ] }", id);
+
+        Introspector introspector = InstrumentationTestRunner.getIntrospector();
+        int finishedTransactionCount = introspector.getFinishedTransactionCount(30000);
+        assertEquals(1, finishedTransactionCount);
+        String transactionName = introspector.getTransactionNames().toArray()[0].toString();
+        assertEquals("WebTransaction/RestWebService/customers/type-{type} (GET)", transactionName);
+    }
+
+    @Test
+    public void testDoubleNestedGetById() {
+        final String id = target("customers/orders/getStuff/1").request().get(String.class);
+        assertEquals("one", id);
+
+        Introspector introspector = InstrumentationTestRunner.getIntrospector();
+        int finishedTransactionCount = introspector.getFinishedTransactionCount(30000);
+        assertEquals(1, finishedTransactionCount);
+        String transactionName = introspector.getTransactionNames().toArray()[0].toString();
+        assertEquals("WebTransaction/RestWebService/customers/orders/getStuff/{id} (GET)", transactionName);
+    }
+
+    @Test
+    public void testDoubleNestedGetAllById() {
+        final String id = target("customers/orders/getStuff").request().get(String.class);
+        assertEquals("{ \"items\": [ \"one\", \"two\", \"three\" ] }", id);
+
+        Introspector introspector = InstrumentationTestRunner.getIntrospector();
+        int finishedTransactionCount = introspector.getFinishedTransactionCount(30000);
+        assertEquals(1, finishedTransactionCount);
+        String transactionName = introspector.getTransactionNames().toArray()[0].toString();
+        assertEquals("WebTransaction/RestWebService/customers/orders/getStuff (GET)", transactionName);
+    }
+
+    @Test
+    public void testDefaultGet() {
+        final String result = target("customers/default-get-test").request().get(String.class);
+        assertEquals("TEST", result);
+    }
+
+    @Test
+    public void getGetInterfaceWithImpl() {
+        for (int i = 0; i < 100; i++) {
+            // We need to run this many times in order to get the path to be re-used incorrectly
+            final String result = target("v1/people/getPeople").request().get(String.class);
+            assertEquals("Returning from getPeople", result);
+        }
+
+        Introspector introspector = InstrumentationTestRunner.getIntrospector();
+        int finishedTransactionCount = introspector.getFinishedTransactionCount(30000);
+        assertEquals(100, finishedTransactionCount);
+        assertEquals(1, introspector.getTransactionNames().size());
+        String transactionName = introspector.getTransactionNames().toArray()[0].toString();
+        assertEquals("WebTransaction/RestWebService/v1/people/getPeople (GET)", transactionName);
+    }
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(CustomerLocatorResource.class, FirstLastSubResource.class, IdSubResource.class, PeopleResource.class);
+    }
+
+}
