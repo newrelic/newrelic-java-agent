19245c67d65029073dc41f039086b1382f5d453b
tbradellis <bradellis10@gmail.com>
2022-10-15 14:51:31 -0700

    add embedded tomcat jmx instrumentation
    update copyright
    
    different name for datasource mbean
    
    reorganize tomcat jmx
    
    fix tomcat utils
    
    check for embedded signal
    
    clarify comment for non embedded tc
    
    log different paths by name
    
    also check datasource b4 adding jmx
    
    clarify comment

--- newrelic-agent/src/main/java/com/newrelic/agent/jmx/values/EmbeddedTomcatJmxValues.java
+++ newrelic-agent/src/main/java/com/newrelic/agent/jmx/values/EmbeddedTomcatJmxValues.java
@@ -1,6 +1,6 @@
 /*
  *
- *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * Copyright 2022 New Relic Corporation. All rights reserved.
  *  * SPDX-License-Identifier: Apache-2.0
  *
  */
@@ -10,7 +10,6 @@ package com.newrelic.agent.jmx.values;
 import com.newrelic.agent.MetricNames;
 import com.newrelic.agent.jmx.JmxType;
 import com.newrelic.agent.jmx.metrics.BaseJmxValue;
-import com.newrelic.agent.jmx.metrics.DataSourceJmxMetricGenerator;
 import com.newrelic.agent.jmx.metrics.JmxAction;
 import com.newrelic.agent.jmx.metrics.JmxFrameworkValues;
 import com.newrelic.agent.jmx.metrics.JmxMetric;
@@ -19,27 +18,25 @@ import com.newrelic.agent.jmx.metrics.ServerJmxMetricGenerator;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TomcatJmxValues extends JmxFrameworkValues {
+public class EmbeddedTomcatJmxValues extends JmxFrameworkValues {
 
     /**
-     * This is the main prefix for JMX metrics. However, the prefix can be changed by setting the name of the engine to
-     * something else.
+     * The Mbean group namespaces are different between standalone Tomcat and Embedded Tomcat
+     * Standalone uses the prefix (aka type) Catalina for queries whereas embedded Tomcat uses the prefix (aka type) Tomcat.
+     * Our TomcatJmxValues instrumentation only queries MBeans with the Catalina prefix thus queries initiated by embedded Tomcat
+     * do not provide metrics. Additionally, datasource metrics
+     * were broken out into type: org.apache.tomcat.pool.jmx for embedded tomcat. See (@EmbeddedTomcatDataSourceJmxValues)
      */
-    public static final String PREFIX = "Catalina";
 
-    private static final int METRIC_COUNT = 5;
+    public static final String PREFIX = "Tomcat";
 
-    // SESSION METRICS
+    private static final int METRIC_COUNT = 2;
+
+    // SESSION METRICS (Manager)
     private static final JmxMetric ACTIVE_SESSIONS = ServerJmxMetricGenerator.SESSION_ACTIVE_COUNT.createMetric("activeSessions");
     private static final JmxMetric EXPIRED_SESSIONS = ServerJmxMetricGenerator.SESSION_EXPIRED_COUNT.createMetric("expiredSessions");
     private static final JmxMetric REJECTED_SESSIONS = ServerJmxMetricGenerator.SESSION_REJECTED_COUNT.createMetric("rejectedSessions");
     private static final JmxMetric SESSION_ALIVE_TIME = ServerJmxMetricGenerator.SESSION_AVG_ALIVE_TIME.createMetric("sessionAverageAliveTime");
-
-    private static final JmxMetric CONNECTIONS_ACTIVE = DataSourceJmxMetricGenerator.CONNECTIONS_ACTIVE.createMetric("numActive");
-    private static final JmxMetric CONNECTIONS_IDLE = DataSourceJmxMetricGenerator.CONNECTIONS_IDLE.createMetric("numIdle");
-    private static final JmxMetric CONNECTIONS_MAX = DataSourceJmxMetricGenerator.CONNECTIONS_MAX.createMetric("maxActive");
-    private static final JmxMetric CONNECTIONS_MAX_TOMCAT_8 = DataSourceJmxMetricGenerator.CONNECTIONS_MAX.createMetric("maxTotal");
-
 
     // THREAD POOL METRICS
     private static final JmxMetric CURRENT_MAX_COUNT = ServerJmxMetricGenerator.MAX_THREAD_POOL_COUNT.createMetric("maxThreads");
@@ -49,18 +46,18 @@ public class TomcatJmxValues extends JmxFrameworkValues {
 
     private final List<BaseJmxValue> metrics = new ArrayList<>(METRIC_COUNT);
 
-    public TomcatJmxValues() {
+    public EmbeddedTomcatJmxValues() {
         createMetrics("*");
     }
 
-    public TomcatJmxValues(String name) {
+    public EmbeddedTomcatJmxValues(String name) {
         createMetrics(name);
 
     }
 
     private void createMetrics(String name) {
         /*
-         * Only used by 7.0. The manager bean provides information about sessions. sessionCounter is the total number of
+         * Only used by 7.0+. The manager bean provides information about sessions. sessionCounter is the total number of
          * sessions created by this manager. ActiveSessions is the number of active sessions at this moment.
          * expiredSesions is the number of sessions that have expired. RejectedSessions is the number of sessions
          * rejected due to maxActive being reached. SessionAverageAliveTime is the average time an expired session had
@@ -68,32 +65,10 @@ public class TomcatJmxValues extends JmxFrameworkValues {
          */
         metrics.add(new BaseJmxValue(name + ":type=Manager,context=*,host=*,*", MetricNames.JMX_SESSION + "{context}/",
                 new JmxMetric[] { ACTIVE_SESSIONS, EXPIRED_SESSIONS, REJECTED_SESSIONS, SESSION_ALIVE_TIME }));
-        /* This is for 6.0 and 5.5. */
-        metrics.add(new BaseJmxValue(name + ":type=Manager,path=*,host=*", MetricNames.JMX_SESSION + "{path}/",
-                new JmxMetric[] { ACTIVE_SESSIONS, EXPIRED_SESSIONS, REJECTED_SESSIONS, SESSION_ALIVE_TIME }));
-        /*
-         * Provides information about the thread pool. The current thread count and the current number of threads which
-         * are busy.
-         */
+
         metrics.add(new BaseJmxValue(name + ":type=ThreadPool,name=*", MetricNames.JMX_THREAD_POOL + "{name}/",
                 new JmxMetric[] { CURRENT_ACTIVE_COUNT, CURRENT_IDLE_COUNT, CURRENT_MAX_COUNT }));
-        
-        /*
-         * Provides information about the data source by finding the number of active and idle connections and the max connections. 
-         * In Tomcat 7, the number of max connections is represented by the maxActive attribute. In tomcat 8
-         * this was changed to maxTotal, hence the two CONNECTIONS_MAX metrics.
-         */
-        metrics.add(new BaseJmxValue(name + ":type=DataSource,context=*,host=*," +
-                "class=javax.sql.DataSource,name=*", MetricNames.JMX_DATASOURCES + "{name}/",
-                new JmxMetric[] { CONNECTIONS_ACTIVE, CONNECTIONS_IDLE, CONNECTIONS_MAX, CONNECTIONS_MAX_TOMCAT_8 }));
-        
-        /*
-         * Provides information about the data source when the customer is using JNDI GlobalNamingResources, which do 
-         * not have a context or a host.
-         */
-        metrics.add(new BaseJmxValue(name + ":type=DataSource," +
-                "class=javax.sql.DataSource,name=*", MetricNames.JMX_DATASOURCES + "{name}/",
-                new JmxMetric[] { CONNECTIONS_ACTIVE, CONNECTIONS_IDLE, CONNECTIONS_MAX, CONNECTIONS_MAX_TOMCAT_8 }));
+
     }
 
     @Override
