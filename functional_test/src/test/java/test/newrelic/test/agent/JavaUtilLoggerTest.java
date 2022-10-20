446259e614f19c3cbbcdbc7c642a08d7384c8f5e
jasonjkeller <jasonjkeller@gmail.com>
2022-10-20 13:16:02 -0700

    Add copyright headers

--- functional_test/src/test/java/test/newrelic/test/agent/JavaUtilLoggerTest.java
+++ functional_test/src/test/java/test/newrelic/test/agent/JavaUtilLoggerTest.java
@@ -1,3 +1,10 @@
+/*
+ *
+ *  * Copyright 2022 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
 package test.newrelic.test.agent;
 
 import com.newrelic.agent.Transaction;
--- instrumentation/java.logging-jdk8/src/main/java/java/util/logging/LogRecord_Instrumentation.java
+++ instrumentation/java.logging-jdk8/src/main/java/java/util/logging/LogRecord_Instrumentation.java
@@ -1,3 +1,10 @@
+/*
+ *
+ *  * Copyright 2022 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
 package java.util.logging;
 
 import com.newrelic.api.agent.weaver.Weave;
