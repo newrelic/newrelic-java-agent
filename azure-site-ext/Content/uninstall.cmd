318d7fe7e4b0c89d2eeb615c7d849d042422a83c
Oren Ben-Meir <orenbm11@gmail.com>
2022-10-27 15:11:14 -0400

    modified install script for a simple java agent install

--- azure-site-ext/Content/uninstall.cmd
+++ azure-site-ext/Content/uninstall.cmd
@@ -1,18 +1,7 @@
 :: Copyright 2022 New Relic Corporation. All rights reserved.
 :: SPDX-License-Identifier: Apache-2.0
 
-SET NEW_RELIC_FOLDER="%HOME%\NewRelicAgent"
-IF EXIST %NEW_RELIC_FOLDER% (
-  rd /S /q %NEW_RELIC_FOLDER%
-)
-
-:: Clean up legacy install locations (unfortunately the same location that the nuget agent is deployed)
-SET NEW_RELIC_FOLDER="%WEBROOT_PATH%\newrelic"
-IF EXIST %NEW_RELIC_FOLDER% (
-  rd /S /q %NEW_RELIC_FOLDER%
-)
-
-SET NEW_RELIC_FOLDER="%WEBROOT_PATH%\newrelic_core"
+SET NEW_RELIC_FOLDER="%HOME%\NewRelicJavaAgent"
 IF EXIST %NEW_RELIC_FOLDER% (
   rd /S /q %NEW_RELIC_FOLDER%
 )
