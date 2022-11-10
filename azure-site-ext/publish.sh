1f077d411f417944cb26f8f9ef03207bfdf489db
Oren Ben-Meir <orenbm11@gmail.com>
2022-11-10 13:45:30 -0500

    Add shell script as a GHA template for Azure Site Extensions

---
+++ azure-site-ext/publish.sh
@@ -0,0 +1,10 @@
+# This is just a simple shell script that will later be
+# used as a template for a GHA job for
+# azure site extension uploads.
+
+# Dependencies: .Net 5 and up or .Net Core, Mono, and the Nuget CLI.
+VERSION=1.0.0
+NUSPEC_GENERATED="NewRelic.Java.Azure.WebSites.Extension.${VERSION}.nuspec"
+sed "s/{VERSION}/${VERSION}/g" NewRelic.Java.Azure.WebSites.Extension.nuspec > "${NUSPEC_GENERATED}"
+nuget pack "${NUSPEC_GENERATED}"
+dotnet nuget push "NewRelic.Java.Azure.WebSites.Extension.${VERSION}.nupkg" --api-key ${NUGET_API_KEY} --source ${NUGET_SOURCE}
