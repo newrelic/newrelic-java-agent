28f2f428c0562681fcd22badaeace0f2667682ec
tbradellis <bradellis10@gmail.com>
2022-12-14 09:56:55 -0800

    log UserPrincipal object null if null

--- instrumentation/servlet-user/src/main/java/javax/servlet/Servlet.java
+++ instrumentation/servlet-user/src/main/java/javax/servlet/Servlet.java
@@ -8,10 +8,12 @@
 package javax.servlet;
 
 import java.security.Principal;
+import java.util.logging.Level;
 
 import javax.servlet.http.HttpServletRequest;
 
 import com.newrelic.agent.bridge.AgentBridge;
+import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
@@ -22,12 +24,14 @@ public abstract class Servlet {
 
     @Trace(dispatcher = true)
     public void service(ServletRequest request, ServletResponse response) {
-
+        Principal principal;
         if (request instanceof HttpServletRequest) {
-            Principal principal = ((HttpServletRequest) request).getUserPrincipal();
+            principal = ((HttpServletRequest) request).getUserPrincipal();
             if (principal != null) {
                 AgentBridge.getAgent().getTransaction().getAgentAttributes().put("user", principal.getName());
             }
+            NewRelic.getAgent().getLogger().log(Level.FINEST, "HttpServletRequest UserPrincipal is " + principal);
+
         }
 
         Weaver.callOriginal();
