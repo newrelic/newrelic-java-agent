19845d224fe3c4818e5815271391ebfff927f8d2
duartesaraiva98 <duartemasaraiva@gmail.com>
2022-12-29 09:56:10 +0100

    Add zio 2 instrumentation

---
+++ instrumentation/zio-2/src/main/java/zio/internal/ZIOScheduler_Instrumentation.java
@@ -0,0 +1,18 @@
+package zio.internal;
+
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import scala.Boolean;
+import scala.Function0;
+import java.time.Duration;
+import zio.Unsafe;
+
+@Weave(originalName = "zio.Scheduler", type = MatchType.BaseClass)
+public class ZIOScheduler_Instrumentation {
+
+  public Function0<Boolean> schedule(Runnable task, Duration duration, Unsafe unsafe) {
+    task = new TokenAwareRunnable(task);
+    return Weaver.callOriginal();
+  }
+}
