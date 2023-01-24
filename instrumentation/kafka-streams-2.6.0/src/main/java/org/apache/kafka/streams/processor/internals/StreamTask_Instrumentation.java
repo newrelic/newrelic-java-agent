633a63a33cabe8f6476e78b6dd93a6604b1767f2
Oren Ben-Meir <orenbm11@gmail.com>
2023-01-24 16:52:48 -0500

    add missing copyright comments

--- instrumentation/kafka-streams-2.6.0/src/main/java/org/apache/kafka/streams/processor/internals/StreamTask_Instrumentation.java
+++ instrumentation/kafka-streams-2.6.0/src/main/java/org/apache/kafka/streams/processor/internals/StreamTask_Instrumentation.java
@@ -1,3 +1,10 @@
+/*
+ *
+ *  * Copyright 2023 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
 package org.apache.kafka.streams.processor.internals;
 
 import com.newrelic.api.agent.NewRelic;
