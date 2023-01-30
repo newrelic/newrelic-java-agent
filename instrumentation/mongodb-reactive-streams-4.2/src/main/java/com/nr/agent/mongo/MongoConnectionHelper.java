d3ab7e8d842354c6443b72624a9c7f96d8b886a7
tbradellis <bradellis10@gmail.com>
2023-01-30 12:26:07 -0800

    fix up settings.g

---
+++ instrumentation/mongodb-reactive-streams-4.2/src/main/java/com/nr/agent/mongo/MongoConnectionHelper.java
@@ -0,0 +1,17 @@
+package com.nr.agent.mongo;
+
+public class MongoConnectionHelper {
+
+
+
+    private static String host = null;
+    private static int port = 0;
+
+
+    public static void setHost(String host){
+        host = host;
+    }
+    public static void setPort(int port){
+        port = port;
+    }
+}
