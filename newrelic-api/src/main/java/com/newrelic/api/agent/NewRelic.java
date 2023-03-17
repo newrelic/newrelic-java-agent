bf9abcbd3a21a4521478c7cd4e1cc28077e75c32
Oren Ben-Meir <orenbm11@gmail.com>
2023-03-17 16:10:48 -0400

    Add missing setUserIdParam method to newrelic-api

--- newrelic-api/src/main/java/com/newrelic/api/agent/NewRelic.java
+++ newrelic-api/src/main/java/com/newrelic/api/agent/NewRelic.java
@@ -265,6 +265,14 @@ public final class NewRelic {
     }
 
     /**
+     * Add the userId parameter to the current transaction. It is reported in errors and transaction traces.
+     *
+     * @param userId The user ID to report.
+     */
+    public static void setUserIdParam(String userId) {
+    }
+
+    /**
      * Set the name of the current transaction.
      *
      * @param category Metric category. If the input is null, then the default will be used.
