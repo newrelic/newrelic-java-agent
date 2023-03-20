10af971dcc5fd01ef58968a040c887d44791b7b1
Oren Ben-Meir <orenbm11@gmail.com>
2023-03-20 15:30:18 -0400

    Set userId in servlet user instrumentation

--- instrumentation/servlet-user-5.0/src/main/java/jakarta/servlet/Servlet.java
+++ instrumentation/servlet-user-5.0/src/main/java/jakarta/servlet/Servlet.java
@@ -8,8 +8,11 @@
 package jakarta.servlet;
 
 import java.security.Principal;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Level;
 
 import com.newrelic.api.agent.NewRelic;
+import com.newrelic.api.agent.weaver.NewField;
 import jakarta.servlet.http.HttpServletRequest;
 
 import com.newrelic.agent.bridge.AgentBridge;
@@ -21,6 +24,9 @@ import com.newrelic.api.agent.weaver.Weaver;
 @Weave(type = MatchType.Interface)
 public abstract class Servlet {
 
+    @NewField
+    private final AtomicBoolean logNrUserDeprecation = new AtomicBoolean(true);
+
     @Trace(dispatcher = true)
     public void service(ServletRequest request, ServletResponse response) {
 
@@ -29,6 +35,10 @@ public abstract class Servlet {
             if (principal != null) {
                 // The user attribute is deprecated, the userId is going to be used instead
                 AgentBridge.getAgent().getTransaction().getAgentAttributes().put("user", principal.getName());
+                if (logNrUserDeprecation.get()) {
+                    NewRelic.getAgent().getLogger().log(Level.INFO, "The 'user' attribute for transactions is deprecated, use the 'userId' attribute instead.");
+                    logNrUserDeprecation.set(false);
+                }
                 NewRelic.setUserIdParam(principal.getName());
             }
         }
--- instrumentation/servlet-user/src/main/java/javax/servlet/Servlet.java
+++ instrumentation/servlet-user/src/main/java/javax/servlet/Servlet.java
@@ -8,6 +8,8 @@
 package javax.servlet;
 
 import java.security.Principal;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Level;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,11 +17,15 @@ import com.newrelic.agent.bridge.AgentBridge;
 import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.NewField;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 
 @Weave(type = MatchType.Interface)
 public abstract class Servlet {
+
+    @NewField
+    private final AtomicBoolean logNrUserDeprecation = new AtomicBoolean(true);
 
     @Trace(dispatcher = true)
     public void service(ServletRequest request, ServletResponse response) {
@@ -29,6 +35,10 @@ public abstract class Servlet {
             if (principal != null) {
                 // The user attribute is deprecated, the userId is going to be used instead
                 AgentBridge.getAgent().getTransaction().getAgentAttributes().put("user", principal.getName());
+                if (logNrUserDeprecation.get()) {
+                    NewRelic.getAgent().getLogger().log(Level.INFO, "The 'user' attribute for transactions is deprecated, use the 'userId' attribute instead.");
+                    logNrUserDeprecation.set(false);
+                }
                 NewRelic.setUserIdParam(principal.getName());
             }
         }
