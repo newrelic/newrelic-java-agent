d6ebbc7de0d2b2b305c7139925bd8c9941274b66
Oren Ben-Meir <orenbm11@gmail.com>
2023-03-20 17:02:18 -0400

    Add copyright notice to new kafka instrumentation classes

--- instrumentation/kafka-clients-metrics-3.0.0/src/main/java/com/nr/instrumentation/kafka/MetricsScheduler.java
+++ instrumentation/kafka-clients-metrics-3.0.0/src/main/java/com/nr/instrumentation/kafka/MetricsScheduler.java
@@ -1,3 +1,9 @@
+/*
+ *
+ *  * Copyright 2023 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
 package com.nr.instrumentation.kafka;
 
 import com.newrelic.agent.bridge.AgentBridge;
--- instrumentation/kafka-streams-metrics-1.0.0/src/main/java/com/nr/instrumentation/kafka/streams/DefaultThreadFactory.java
+++ instrumentation/kafka-streams-metrics-1.0.0/src/main/java/com/nr/instrumentation/kafka/streams/DefaultThreadFactory.java
@@ -1,3 +1,9 @@
+/*
+ *
+ *  * Copyright 2023 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
 package com.nr.instrumentation.kafka.streams;
 
 import java.util.concurrent.ThreadFactory;
