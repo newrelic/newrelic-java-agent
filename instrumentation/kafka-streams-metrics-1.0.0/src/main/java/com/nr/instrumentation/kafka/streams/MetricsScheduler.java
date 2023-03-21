dde3da3d1119e5950b67dd4aa137cc41fa656e4e
Oren Ben-Meir <orenbm11@gmail.com>
2023-03-21 14:32:06 -0400

    optimize imports

--- instrumentation/kafka-streams-metrics-1.0.0/src/main/java/com/nr/instrumentation/kafka/streams/MetricsScheduler.java
+++ instrumentation/kafka-streams-metrics-1.0.0/src/main/java/com/nr/instrumentation/kafka/streams/MetricsScheduler.java
@@ -16,7 +16,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
