e9b04238bd95ae142c555be2811973f202213051
Oren Ben-Meir <orenbm11@gmail.com>
2023-03-21 11:57:50 -0400

    Remove "user" deprecation and keeps the attribute

--- instrumentation/servlet-user-5.0/src/main/java/jakarta/servlet/Servlet.java
+++ instrumentation/servlet-user-5.0/src/main/java/jakarta/servlet/Servlet.java
@@ -24,21 +24,13 @@ import com.newrelic.api.agent.weaver.Weaver;
 @Weave(type = MatchType.Interface)
 public abstract class Servlet {
 
-    @NewField
-    private final AtomicBoolean logNrUserDeprecation = new AtomicBoolean(true);
-
     @Trace(dispatcher = true)
     public void service(ServletRequest request, ServletResponse response) {
 
         if (request instanceof HttpServletRequest) {
             Principal principal = ((HttpServletRequest) request).getUserPrincipal();
             if (principal != null) {
-                // The user attribute is deprecated, the userId is going to be used instead
                 AgentBridge.getAgent().getTransaction().getAgentAttributes().put("user", principal.getName());
-                if (logNrUserDeprecation.get()) {
-                    NewRelic.getAgent().getLogger().log(Level.INFO, "The 'user' attribute for transactions is deprecated, use the 'enduser.id' attribute instead.");
-                    logNrUserDeprecation.set(false);
-                }
                 NewRelic.setUserId(principal.getName());
             }
         }
