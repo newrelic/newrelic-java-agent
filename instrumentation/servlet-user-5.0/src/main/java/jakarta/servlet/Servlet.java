25afdb5386d13927f8c6d8079b8c178d8600b1f4
Oren Ben-Meir <orenbm11@gmail.com>
2023-03-27 17:09:01 -0400

    Optimized imports

--- instrumentation/servlet-user-5.0/src/main/java/jakarta/servlet/Servlet.java
+++ instrumentation/servlet-user-5.0/src/main/java/jakarta/servlet/Servlet.java
@@ -7,19 +7,15 @@
 
 package jakarta.servlet;
 
-import java.security.Principal;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.logging.Level;
-
-import com.newrelic.api.agent.NewRelic;
-import com.newrelic.api.agent.weaver.NewField;
-import jakarta.servlet.http.HttpServletRequest;
-
 import com.newrelic.agent.bridge.AgentBridge;
+import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.security.Principal;
 
 @Weave(type = MatchType.Interface)
 public abstract class Servlet {
