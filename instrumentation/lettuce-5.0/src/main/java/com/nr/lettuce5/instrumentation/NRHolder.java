f559a1570efe224e73a4a49eb214ab0451d624b9
Brad Ellis <bradellis10@gmail.com>
2023-04-14 07:46:17 -0700

    guard lettuce npe (#1204)
    merging with tomee.py test fail. False positive. Test is faulty and is being disabled until faultiness resolved. All other tests pass here.

--- instrumentation/lettuce-5.0/src/main/java/com/nr/lettuce5/instrumentation/NRHolder.java
+++ instrumentation/lettuce-5.0/src/main/java/com/nr/lettuce5/instrumentation/NRHolder.java
@@ -11,6 +11,8 @@ import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Segment;
 import com.newrelic.api.agent.Token;
 
+import java.util.logging.Level;
+
 public class NRHolder {
     private String segmentName = null;
     private Segment segment = null;
@@ -45,7 +47,7 @@ public class NRHolder {
             token.linkAndExpire();
             token = null;
         }
-        if (segment != null) {
+        if (segment != null && params != null) {
             String operation = params.getOperation();
             if (operation.equalsIgnoreCase("expire")) {
                 segment.ignore();
@@ -56,9 +58,13 @@ public class NRHolder {
             params = null;
             segment = null;
         } else {
-            String operation = params.getOperation();
-            if (!operation.equalsIgnoreCase("expire")) {
-                NewRelic.getAgent().getTracedMethod().reportAsExternal(params);
+            if (params != null) {
+                String operation = params.getOperation();
+                if (!operation.equalsIgnoreCase("expire")) {
+                    NewRelic.getAgent().getTracedMethod().reportAsExternal(params);
+                }
+            } else {
+                NewRelic.getAgent().getLogger().log(Level.WARNING, "DatastoreParameters was null for Lettuce instrumentation. Unable to report as external.");
             }
         }
     }
