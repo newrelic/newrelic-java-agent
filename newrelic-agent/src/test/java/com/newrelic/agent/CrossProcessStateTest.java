65c3e21c45527527ec62c3dc8283a838910f526f
Andre Onuki <aonuki@newrelic.com>
2023-05-31 15:50:22 -0400

    Upgrading mockito version

--- newrelic-agent/src/test/java/com/newrelic/agent/CrossProcessStateTest.java
+++ newrelic-agent/src/test/java/com/newrelic/agent/CrossProcessStateTest.java
@@ -32,10 +32,10 @@ import org.mockito.Mockito;
 import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyLong;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
