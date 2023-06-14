d851a1f8782eaf9bd1869464411cd2956527a62e
Kate Anderson <kateanderson@newrelic.com>
2023-06-14 15:35:29 -0700

    Extract assertions to individual tests

--- agent-bridge-datastore/src/test/java/com/newrelic/agent/bridge/datastore/R2dbcObfuscatorTest.java
+++ agent-bridge-datastore/src/test/java/com/newrelic/agent/bridge/datastore/R2dbcObfuscatorTest.java
@@ -6,26 +6,40 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class R2dbcObfuscatorTest {
+
     @Test
-    public void testQueryConverter(){
+    public void queryConverter_shouldObscure_allStringLiterals() {
         String testQuotes = "Found one word: 'one''word' and two words: 'two' 'words'. Another word: \"another_word\" and even an escaped word: \'escaped word'";
         assertEquals("Found one word: ? and two words: ? ?. Another word: ? and even an escaped word: ?", R2dbcObfuscator.QUERY_CONVERTER.toObfuscatedQueryString(testQuotes));
 
         String testDollarQuotes = "Found a string literal: $tag$string_literal$tag$";
         assertEquals("Found a string literal: ?", R2dbcObfuscator.QUERY_CONVERTER.toObfuscatedQueryString(testDollarQuotes));
+    }
 
+    @Test
+    public void queryConverter_shouldObscure_Booleans() {
         String testBooleans = "Found true and TRUE and FalSe and false and NULL but not FakeBoolean";
         assertEquals("Found ? and ? and ? and ? and ? but not FakeBoolean", R2dbcObfuscator.QUERY_CONVERTER.toObfuscatedQueryString(testBooleans));
+    }
 
+    @Test
+    public void queryConverter_shouldObscure_Comments() {
         String testSingleLineComment = "Found a line with a comment #here\n Second line with a comment --here\r and a terminating comment #here";
         assertEquals("Found a line with a comment ?\n Second line with a comment ?\r and a terminating comment ?", R2dbcObfuscator.QUERY_CONVERTER.toObfuscatedQueryString(testSingleLineComment));
+
+        String testMultilineComment = "Here is a multiline comment: \n/* The \n lazy brown \r cow */\n And here is text after.";
+        assertEquals("Here is a multiline comment: \n?\n And here is text after.", R2dbcObfuscator.QUERY_CONVERTER.toObfuscatedQueryString(testMultilineComment));
+
+    }
+    @Test
+    public void queryConverter_shouldObscure_HexAndUUID(){
 
         String testHexAndUUID = "Found a hex string: 0x98C72 and a UUID string: {1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed}";
         assertEquals("Found a hex string: ? and a UUID string: ?", R2dbcObfuscator.QUERY_CONVERTER.toObfuscatedQueryString(testHexAndUUID));
     }
 
     @Test
-    public void testPostgresConverter(){
+    public void postgresConverter_isDialectSpecific(){
         String testFindUUIDNotHex = "Found {1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed} but not 0x98C72"; //There will be numeric fall-through
         assertEquals("Found ? but not ?x98C72", R2dbcObfuscator.POSTGRES_QUERY_CONVERTER.toObfuscatedQueryString(testFindUUIDNotHex));
 
@@ -34,7 +48,7 @@ public class R2dbcObfuscatorTest {
     }
 
     @Test
-    public void testMysqlConverter() {
+    public void mysqlConverter_isDialectSpecific() {
         String testFindHexNotUUID = "Found 0x98C72 but not {1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed}"; //There will be numeric fall-through
         assertEquals("Found ? but not {?b9d6bcd-bbfd?b2d?b5d-ab8dfbbd4bed}", R2dbcObfuscator.MYSQL_QUERY_CONVERTER.toObfuscatedQueryString((testFindHexNotUUID)));
 
