7c7918c78af1ecdb4ff6df9cce85f21bd4ac7e73
John Bedell <jbedell@newrelic.com>
2023-06-20 16:57:50 -0400

    a few more tests

--- newrelic-agent/src/test/java/com/newrelic/agent/util/asm/UtilsTest.java
+++ newrelic-agent/src/test/java/com/newrelic/agent/util/asm/UtilsTest.java
@@ -7,7 +7,10 @@
 
 package com.newrelic.agent.util.asm;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 import java.sql.ResultSet;
@@ -23,6 +26,7 @@ import javax.sql.PooledConnection;
 import javax.swing.ListCellRenderer;
 
 import com.newrelic.agent.bridge.AgentBridge;
+import com.newrelic.weave.utils.WeaveUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.objectweb.asm.ClassReader;
@@ -75,6 +79,30 @@ public class UtilsTest {
         Assert.assertEquals("java/util/List.class", Utils.getClassResourceName("java/util/List"));
     }
 
+    @Test
+    public void isJdkProxy_looksLikeButNot() throws IOException {
+        ClassReader classReader = Utils.readClass(MyProxy.class);
+        Assert.assertFalse(Utils.isJdkProxy(classReader));
+    }
+
+    @Test
+    public void isJdkProxy_false() throws IOException {
+        ClassReader classReader = Utils.readClass(BadMethodMatch.class);
+        Assert.assertFalse(Utils.isJdkProxy(classReader));
+    }
+
+    @Test
+    public void print() throws IOException {
+        OutputStream os = new ByteArrayOutputStream();
+        PrintWriter pw = new PrintWriter(os);
+        byte[] classBytes = WeaveUtils.getClassBytesFromClassLoaderResource(MyProxy.class.getName(), MyProxy.class.getClassLoader());
+        Utils.print(classBytes, pw);
+        String result = os.toString();
+        Assert.assertTrue(result.contains("MyProxy"));
+        Assert.assertTrue(result.contains("INNERCLASS com/newrelic/agent/util/asm/UtilsTest$MyProxy"));
+        Assert.assertTrue(result.contains("MyProxy"));
+    }
+
     private void assertClass(ClassReader classReader) {
         assertTrue(Arrays.asList(classReader.getInterfaces()).contains(Type.getInternalName(Map.class)));
         assertTrue(Arrays.asList(classReader.getInterfaces()).contains(
@@ -91,6 +119,13 @@ public class UtilsTest {
 
         public ResultSet executeQuery() throws SQLException {
             return null;
+        }
+    }
+
+    final class MyProxy extends Proxy {
+
+        protected MyProxy(InvocationHandler h) {
+            super(h);
         }
     }
 }
