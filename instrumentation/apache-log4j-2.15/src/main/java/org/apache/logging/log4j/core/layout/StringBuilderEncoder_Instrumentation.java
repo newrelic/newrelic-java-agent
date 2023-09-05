c1571369f55dc19f2eb3e2158bf0ba05b29014b4
jasonjkeller <jasonjkeller@gmail.com>
2023-09-05 12:07:02 -0700

    Prevent log4j2 instrumentation from adding NR-LINKING metadata if it was already added by other logger instrumentation (e.g. JUL).

--- instrumentation/apache-log4j-2.15/src/main/java/org/apache/logging/log4j/core/layout/StringBuilderEncoder_Instrumentation.java
+++ instrumentation/apache-log4j-2.15/src/main/java/org/apache/logging/log4j/core/layout/StringBuilderEncoder_Instrumentation.java
@@ -11,6 +11,7 @@ import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 
+import static com.newrelic.agent.bridge.logging.AppLoggingUtils.BLOB_PREFIX;
 import static com.newrelic.agent.bridge.logging.AppLoggingUtils.getLinkingMetadataBlob;
 import static com.newrelic.agent.bridge.logging.AppLoggingUtils.isApplicationLoggingEnabled;
 import static com.newrelic.agent.bridge.logging.AppLoggingUtils.isApplicationLoggingLocalDecoratingEnabled;
@@ -30,11 +31,15 @@ public class StringBuilderEncoder_Instrumentation {
     }
 
     private void appendAgentMetadata(StringBuilder source) {
-        int breakLine = source.toString().lastIndexOf("\n");
-        if (breakLine != -1) {
-            source.replace(breakLine, breakLine + 1, "");
+        String sourceString = source.toString();
+        // It is possible that the log might already have NR-LINKING metadata from JUL instrumentation
+        if (!sourceString.contains(BLOB_PREFIX)) {
+            int breakLine = sourceString.lastIndexOf("\n");
+            if (breakLine != -1) {
+                source.replace(breakLine, breakLine + 1, "");
+            }
+            source.append(getLinkingMetadataBlob()).append("\n");
         }
-        source.append(getLinkingMetadataBlob()).append("\n");
     }
 
 }
--- instrumentation/apache-log4j-2/src/main/java/org/apache/logging/log4j/core/layout/AbstractStringLayout_Instrumentation.java
+++ instrumentation/apache-log4j-2/src/main/java/org/apache/logging/log4j/core/layout/AbstractStringLayout_Instrumentation.java
@@ -15,6 +15,7 @@ import org.apache.logging.log4j.core.util.StringEncoder;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 
+import static com.newrelic.agent.bridge.logging.AppLoggingUtils.BLOB_PREFIX;
 import static com.newrelic.agent.bridge.logging.AppLoggingUtils.getLinkingMetadataBlob;
 
 @Weave(originalName = "org.apache.logging.log4j.core.layout.AbstractStringLayout", type = MatchType.ExactClass)
@@ -27,7 +28,7 @@ public abstract class AbstractStringLayout_Instrumentation {
     protected byte[] getBytes(final String s) {
         String modified = s;
         // It is possible that the log being formatted into JSON might already have NR-LINKING metadata from JUL instrumentation
-        if (!s.contains("NR-LINKING")) {
+        if (!s.contains(BLOB_PREFIX)) {
             int indexToInsertNrLinkingMetadata = s.indexOf("\n", s.indexOf("message")) - 1;
             // Replace the JSON string with modified version that includes NR-LINKING metadata
             modified = new StringBuilder(s).insert(indexToInsertNrLinkingMetadata, getLinkingMetadataBlob()).toString();
