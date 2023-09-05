9dde58e964c273aad9fdc0a9b94a576d57923038
jasonjkeller <jasonjkeller@gmail.com>
2023-09-05 11:19:23 -0700

    Remove AbstractJacksonLayout instrumentation. Split instrumentation into two modules to support log4j 2.6-2.14 and 2.15+

---
+++ instrumentation/apache-log4j-2.15/src/main/java/org/apache/logging/log4j/core/layout/AbstractStringLayout_Instrumentation.java
@@ -0,0 +1,33 @@
+/*
+ *
+ *  * Copyright 2023 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.apache.logging.log4j.core.layout;
+
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+
+import java.nio.charset.Charset;
+
+import static com.newrelic.agent.bridge.logging.AppLoggingUtils.getLinkingMetadataBlob;
+
+@Weave(originalName = "org.apache.logging.log4j.core.layout.AbstractStringLayout", type = MatchType.ExactClass)
+public abstract class AbstractStringLayout_Instrumentation {
+
+    private final Charset charset = Weaver.callOriginal();
+
+    protected byte[] getBytes(final String s) {
+        String modified = s;
+        if (!s.contains("NR-LINKING")) {
+            int indexToInsertNrLinkingMetadata = s.indexOf("\n", s.indexOf("message")) - 1;
+            // Replace the JSON string with modified version
+            modified = new StringBuilder(s).insert(indexToInsertNrLinkingMetadata, getLinkingMetadataBlob()).toString();
+        }
+        return modified.getBytes(charset);
+    }
+
+}
