2f06bceb4d705ef1575355c59f8bd29c75067241
jasonjkeller <jasonjkeller@gmail.com>
2023-09-05 11:24:32 -0700

    Update copyright headers

--- instrumentation/apache-log4j-2.15/src/test/java/com/nr/agent/instrumentation/log4j2/ExceptionUtilTest.java
+++ instrumentation/apache-log4j-2.15/src/test/java/com/nr/agent/instrumentation/log4j2/ExceptionUtilTest.java
@@ -1,3 +1,10 @@
+/*
+ *
+ *  * Copyright 2023 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
 package com.nr.agent.instrumentation.log4j2;
 
 import org.junit.Test;
