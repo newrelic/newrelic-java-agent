6722b40fe7933abd1cadadc278d38a5ccd3ca575
Jack Berg <jberg@newrelic.com>
2023-09-21 18:07:20 -0500

    Report slowest transaction each minute, align with transaction conventions

--- newrelic-agent/src/main/java/com/newrelic/agent/slowtransactions/SlowTransactionService.java
+++ newrelic-agent/src/main/java/com/newrelic/agent/slowtransactions/SlowTransactionService.java
@@ -1,13 +1,13 @@
 package com.newrelic.agent.slowtransactions;
 
 import com.newrelic.agent.ExtendedTransactionListener;
+import com.newrelic.agent.HarvestListener;
 import com.newrelic.agent.Transaction;
 import com.newrelic.agent.TransactionData;
 import com.newrelic.agent.service.AbstractService;
 import com.newrelic.agent.service.ServiceFactory;
+import com.newrelic.agent.stats.StatsEngine;
 import com.newrelic.agent.stats.TransactionStats;
-import com.newrelic.agent.util.DefaultThreadFactory;
-import com.newrelic.agent.util.StackTraces;
 import com.newrelic.api.agent.NewRelic;
 
 import java.lang.management.ManagementFactory;
@@ -18,50 +18,54 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
-public class SlowTransactionService extends AbstractService implements ExtendedTransactionListener {
+public class SlowTransactionService extends AbstractService implements ExtendedTransactionListener, HarvestListener {
 
-    private final ScheduledExecutorService scheduler;
     private final ConcurrentHashMap<String, Transaction> openTransactions = new ConcurrentHashMap<>();
     private final Set<String> previouslyReportedTransactions = new HashSet<>();
     private final ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
 
     public SlowTransactionService() {
         super(SlowTransactionService.class.getSimpleName());
-        this.scheduler = Executors.newSingleThreadScheduledExecutor(new DefaultThreadFactory("Slow Transactions Thread", true));
     }
 
     @Override
     public void dispatcherTransactionStarted(Transaction transaction) {
-        getLogger().info("Transaction started with id " + transaction.getGuid());
+        if (getLogger().isLoggable(Level.FINEST)) {
+            getLogger().finest("Transaction started with id " + transaction.getGuid());
+        }
         openTransactions.put(transaction.getGuid(), transaction);
     }
 
     @Override
     public void dispatcherTransactionCancelled(Transaction transaction) {
-        getLogger().info("Transaction cancelled with guid " + transaction.getGuid());
+        if (getLogger().isLoggable(Level.FINEST)) {
+            getLogger().finest("Transaction cancelled with guid " + transaction.getGuid());
+        }
         openTransactions.remove(transaction.getGuid());
+        previouslyReportedTransactions.remove(transaction.getGuid());
     }
 
     @Override
     public void dispatcherTransactionFinished(TransactionData transactionData, TransactionStats transactionStats) {
-        getLogger().info("Transaction finished with guid " + transactionData.getGuid());
+        if (getLogger().isLoggable(Level.FINEST)) {
+            getLogger().finest("Transaction finished with guid " + transactionData.getGuid());
+        }
         openTransactions.remove(transactionData.getGuid());
+        previouslyReportedTransactions.remove(transactionData.getGuid());
     }
 
     @Override
     protected void doStart() throws Exception {
-        // NewRelic.getAgent().getInsights().recordCustomEvent();
         ServiceFactory.getTransactionService().addTransactionListener(this);
-        scheduler.scheduleAtFixedRate(this::run, 10, 10, TimeUnit.SECONDS);
+        ServiceFactory.getHarvestService().addHarvestListener(this);
     }
 
     @Override
     protected void doStop() throws Exception {
         ServiceFactory.getTransactionService().removeTransactionListener(this);
+        ServiceFactory.getHarvestService().removeHarvestListener(this);
     }
 
     @Override
@@ -69,9 +73,19 @@ public class SlowTransactionService extends AbstractService implements ExtendedT
         return true;
     }
 
+    @Override
+    public void beforeHarvest(String appName, StatsEngine statsEngine) {
+        run();
+    }
+
+    @Override
+    public void afterHarvest(String appName) {
+    }
+
     private void run() {
-        getLogger().info("Identifying slow threads. Open transactions: " + openTransactions.size());
-        long slowThresholdMs = 1000; // Only consider transcations open longer than this
+        if (getLogger().isLoggable(Level.FINE)) {
+            getLogger().fine("Identifying slow threads. Open transactions: " + openTransactions.size());
+        }
         Transaction slowestOpen = null;
         long slowestOpenMs = 0;
 
@@ -83,7 +97,7 @@ public class SlowTransactionService extends AbstractService implements ExtendedT
             }
 
             long openMs = System.currentTimeMillis() - transaction.getWallClockStartTimeMs();
-            if (openMs > slowThresholdMs && openMs > slowestOpenMs) {
+            if (openMs > slowestOpenMs) {
                 slowestOpen = transaction;
                 slowestOpenMs = openMs;
             }
@@ -98,28 +112,48 @@ public class SlowTransactionService extends AbstractService implements ExtendedT
         // Extract some data about its state and emit as event.
         String guid = slowestOpen.getGuid();
         Map<String, Object> attributes = new HashMap<>();
-        // General
-        attributes.put("transaction.guid", guid);
-        attributes.put("transaction.open_ms", slowestOpenMs);
 
-        // Transaction attributes
-        slowestOpen.getIntrinsicAttributes().forEach((key, value) -> attributes.put("intrinsic." + key, value));
-        slowestOpen.getAgentAttributes().forEach((key, value) -> attributes.put("agent." + key, value));
-        slowestOpen.getUserAttributes().forEach((key, value) -> attributes.put("user." + key, value));
-        slowestOpen.getErrorAttributes().forEach((key, value) -> attributes.put("error." + key, value));
+        // Attributes
+        // Write attributes first so hardcoded attributes are prioritized
+        attributes.putAll(slowestOpen.getUserAttributes());
+        attributes.putAll(slowestOpen.getErrorAttributes());
+        attributes.putAll(slowestOpen.getAgentAttributes());
+        attributes.putAll(slowestOpen.getIntrinsicAttributes());
+
+        // General
+        attributes.put("guid", guid);
+        attributes.put("name", slowestOpen.getPriorityTransactionName().getName());
+        attributes.put("transactionType", slowestOpen.getPriorityTransactionName().getCategory());
+        attributes.put("timestamp", slowestOpen.getWallClockStartTimeMs());
+        attributes.put("elapsed_ms", slowestOpenMs);
 
         // Initiating thread info
         long initiatingThreadId = slowestOpen.getInitiatingThreadId();
-        attributes.put("initiating_thread.id", initiatingThreadId);
+        attributes.put("thread.id", initiatingThreadId);
         ThreadInfo threadInfo = threadMXBean.getThreadInfo(initiatingThreadId, 20);
         if (threadInfo != null) {
-            attributes.put("initiating_thread.name", threadInfo.getThreadName());
-            attributes.put("initiating_thread.stacktrace", StackTraces.stackTracesToStrings(threadInfo.getStackTrace()));
+            attributes.put("thread.name", threadInfo.getThreadName());
+            attributes.put("thread.state", threadInfo.getThreadState().name());
+            attributes.put("code.stacktrace", stackTraceString(threadInfo.getStackTrace()));
         }
 
-        getLogger().info("Transaction with guid " + guid + " has exceeded slow transaction threshold of " + slowThresholdMs + ", attributes: " + attributes);
+        if (getLogger().isLoggable(Level.FINE)) {
+            getLogger().fine("Slowest open transaction has guid " + guid + " has been open for " + slowestOpenMs + "ms, attributes: " + attributes);
+        }
+        // TODO: emit event such that stack trace isn't truncated so early
         NewRelic.getAgent().getInsights().recordCustomEvent("SlowTransaction", attributes);
-        // TODO: remove old entries after a while to avoid unbounded memory usage
         previouslyReportedTransactions.add(guid);
+    }
+
+    private static String stackTraceString(StackTraceElement[] stackTrace) {
+        if (stackTrace.length == 0) {
+            return "";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(stackTrace[0]);
+        for (int i = 1; i < stackTrace.length; i++) {
+            stringBuilder.append("\tat ").append(stackTrace[i]);
+        }
+        return stringBuilder.toString();
     }
 }
