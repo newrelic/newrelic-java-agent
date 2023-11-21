07cacb41343b6b18b1bb2f3b7f2770662b805b88
Oren Ben-Meir <orenbm11@gmail.com>
2023-11-20 22:19:10 -0500

    Renamed jetty12 modules and fix ee10 instrumentation verification

---
+++ instrumentation/jetty-ee10-servlet-12/src/main/java/com/nr/agent/instrumentation/jetty/ee10/servlet/ServerHelper.java
@@ -0,0 +1,39 @@
+package com.nr.agent.instrumentation.jetty.ee10.servlet;
+
+import com.newrelic.agent.bridge.AgentBridge;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.ee10.servlet.AsyncContextEvent;
+import org.eclipse.jetty.ee10.servlet.ServletContextRequest;
+import org.eclipse.jetty.ee10.servlet.ServletRequestState;
+
+import java.util.logging.Level;
+
+public class ServerHelper {
+
+    public static void preHandleDispatch(ServletContextRequest servletContextRequest) {
+        HttpServletRequest request = servletContextRequest.getServletApiRequest();
+        HttpServletResponse response = servletContextRequest.getHttpServletResponse();
+        AgentBridge.getAgent().getTransaction(true).requestInitialized(new JettyRequest(request),
+                new JettyResponse(response));
+    }
+
+    public static void preHandleDispatchAsync(ServletContextRequest servletContextRequest, ServletRequestState state) {
+        HttpServletRequest request = servletContextRequest.getServletApiRequest();
+        AsyncContextEvent asyncContextEvent = state.getAsyncContextEvent();
+        if (asyncContextEvent == null) {
+            AgentBridge.getAgent().getLogger().log(Level.FINE, "AsyncContextEvent is null for request: {0}.", request);
+            return;
+        }
+        AgentBridge.asyncApi.resumeAsync(asyncContextEvent.getAsyncContext());
+    }
+
+    public static void postHandleDispatch(ServletContextRequest servletContextRequest) {
+        HttpServletRequest request = servletContextRequest.getServletApiRequest();
+        if (request.isAsyncStarted()) {
+            AgentBridge.asyncApi.suspendAsync(request.getAsyncContext());
+        }
+        AgentBridge.getAgent().getTransaction().requestDestroyed();
+    }
+
+}
---
+++ instrumentation/jetty-ee10-servlet-12/src/main/java/org/eclipse/jetty/ee10/servlet/ServletChannel_Instrumentation.java
@@ -0,0 +1,63 @@
+package org.eclipse.jetty.ee10.servlet;
+
+import com.newrelic.agent.bridge.AgentBridge;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import com.nr.agent.instrumentation.jetty.ee10.servlet.ServerHelper;
+
+@Weave(originalName = "org.eclipse.jetty.ee10.servlet.ServletChannel", type = MatchType.ExactClass)
+public class ServletChannel_Instrumentation {
+    private final ServletRequestState _state = Weaver.callOriginal();
+
+    private class RequestDispatchable implements ServletChannel.Dispatchable {
+
+        @Override
+        public void dispatch() throws Exception {
+            ServletContextRequest servletContextReq = getServletContextRequest();
+            boolean isStarted = AgentBridge.getAgent().getTransaction().isStarted();
+            boolean startTransaction = servletContextReq != null && servletContextReq.getServletApiRequest() != null && !isStarted;
+            if (startTransaction) {
+                ServerHelper.preHandleDispatch(servletContextReq);
+            }
+
+            try {
+                Weaver.callOriginal();
+            } finally {
+                if (startTransaction) {
+                    ServerHelper.postHandleDispatch(servletContextReq);
+                }
+            }
+        }
+    }
+
+    private class AsyncDispatchable implements ServletChannel.Dispatchable {
+
+        @Override
+        public void dispatch() throws Exception {
+            ServletContextRequest servletContextReq = getServletContextRequest();
+            boolean isStarted = AgentBridge.getAgent().getTransaction().isStarted();
+            boolean startTransaction = servletContextReq != null && servletContextReq.getServletApiRequest() != null
+                    && !isStarted && _state != null;
+            if (startTransaction) {
+                ServerHelper.preHandleDispatchAsync(servletContextReq, _state);
+            }
+            try {
+                Weaver.callOriginal();
+            } finally {
+                if (startTransaction) {
+                    ServerHelper.postHandleDispatch(servletContextReq);
+                }
+            }
+        }
+    }
+
+    public ServletContextHandler getServletContextHandler() {
+        return Weaver.callOriginal();
+    }
+
+    public ServletContextRequest getServletContextRequest()
+    {
+        return Weaver.callOriginal();
+    }
+}
