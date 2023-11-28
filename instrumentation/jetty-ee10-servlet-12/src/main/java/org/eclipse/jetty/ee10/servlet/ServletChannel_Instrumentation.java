5fdf7147fc09d3b9bb864deedbd971a881f26928
Oren Ben-Meir <orenbm11@gmail.com>
2023-11-28 18:17:27 -0500

    Weave inner classes in jetty 12 ee10

--- instrumentation/jetty-ee10-servlet-12/src/main/java/org/eclipse/jetty/ee10/servlet/ServletChannel_Instrumentation.java
+++ instrumentation/jetty-ee10-servlet-12/src/main/java/org/eclipse/jetty/ee10/servlet/ServletChannel_Instrumentation.java
@@ -3,6 +3,7 @@ package org.eclipse.jetty.ee10.servlet;
 import com.newrelic.agent.bridge.AgentBridge;
 import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.NewField;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 import com.nr.agent.instrumentation.jetty.ee10.servlet.ServerHelper;
@@ -10,6 +11,23 @@ import com.nr.agent.instrumentation.jetty.ee10.servlet.ServerHelper;
 @Weave(originalName = "org.eclipse.jetty.ee10.servlet.ServletChannel", type = MatchType.ExactClass)
 public class ServletChannel_Instrumentation {
     private final ServletRequestState _state = Weaver.callOriginal();
+    private final ServletChannel.Dispatchable _requestDispatchable = Weaver.callOriginal();
+
+    private final ServletChannel.Dispatchable _asyncDispatchable = Weaver.callOriginal();
+
+    private void dispatch(ServletChannel.Dispatchable dispatchable) {
+        // Initialize dispatcher to be called
+        if (dispatchable == _requestDispatchable) {
+            ((RequestDispatchable_Instrumentation)_requestDispatchable).servletContextReq = getServletContextRequest();
+        }
+        if (dispatchable == _asyncDispatchable) {
+            ((AsyncDispatchable_Instrumentation)_requestDispatchable)._state = _state;
+            ((AsyncDispatchable_Instrumentation)_requestDispatchable).servletContextReq = getServletContextRequest();
+        }
+
+        // dispatchable.dispatch() is called next after some setup
+        Weaver.callOriginal();
+    }
 
     public void sendResponseAndComplete() {
         ServletContextRequest servletContextRequest = getServletContextRequest();
@@ -23,11 +41,13 @@ public class ServletChannel_Instrumentation {
         }
     }
 
-    private class RequestDispatchable implements ServletChannel.Dispatchable {
+    @Weave(type = MatchType.ExactClass, originalName = "org.eclipse.jetty.ee10.servlet.ServletChannel$RequestDispatchable")
+    private static class RequestDispatchable_Instrumentation implements ServletChannel.Dispatchable {
 
-        @Override
-        public void dispatch() throws Exception {
-            ServletContextRequest servletContextReq = getServletContextRequest();
+        @NewField
+        ServletContextRequest servletContextReq;
+
+        public void dispatch() {
             boolean isStarted = AgentBridge.getAgent().getTransaction().isStarted();
             boolean startTransaction = servletContextReq != null && servletContextReq.getServletApiRequest() != null && !isStarted;
             if (startTransaction) {
@@ -44,11 +64,15 @@ public class ServletChannel_Instrumentation {
         }
     }
 
-    private class AsyncDispatchable implements ServletChannel.Dispatchable {
+    @Weave(type = MatchType.ExactClass, originalName = "org.eclipse.jetty.ee10.servlet.ServletChannel$AsyncDispatchable")
+    private static class AsyncDispatchable_Instrumentation implements ServletChannel.Dispatchable {
 
-        @Override
-        public void dispatch() throws Exception {
-            ServletContextRequest servletContextReq = getServletContextRequest();
+        @NewField
+        ServletContextRequest servletContextReq;
+        @NewField
+        ServletRequestState _state;
+
+        public void dispatch() {
             boolean isStarted = AgentBridge.getAgent().getTransaction().isStarted();
             boolean startTransaction = servletContextReq != null && servletContextReq.getServletApiRequest() != null
                     && !isStarted && _state != null;
