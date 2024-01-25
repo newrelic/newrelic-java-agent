2e82d9c9fbfddf97add515d9c7edaa19f42bccc5
Andre Onuki <aonuki@newrelic.com>
2024-01-25 16:45:40 -0500

    Vertx 4.5.x instrumentation

---
+++ instrumentation/vertx-core-4.5.0/src/main/java/com/nr/vertx/instrumentation/AsyncHandlerWrapper.java
@@ -0,0 +1,35 @@
+/*
+ *
+ *  * Copyright 2024 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+package com.nr.vertx.instrumentation;
+
+import com.newrelic.api.agent.Token;
+import com.newrelic.api.agent.Trace;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+
+public class AsyncHandlerWrapper<T> implements Handler<AsyncResult<T>> {
+    private Handler<AsyncResult<T>> original;
+
+    private Token token;
+
+    public AsyncHandlerWrapper(Handler<AsyncResult<T>> original, Token token) {
+        this.original = original;
+        this.token = token;
+    }
+
+    @Override
+    @Trace(async = true, excludeFromTransactionTrace = true)
+    public void handle(AsyncResult<T> event) {
+        if (token != null) {
+            token.linkAndExpire();
+            token = null;
+        }
+
+        this.original.handle(event);
+        this.original = null;
+    }
+}
---
+++ instrumentation/vertx-core-4.5.1/src/main/java/io/vertx/core/impl/future/FutureImpl_Instrumentation.java
@@ -0,0 +1,43 @@
+/*
+ *
+ *  * Copyright 2024 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package io.vertx.core.impl.future;
+
+import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import com.nr.vertx.instrumentation.VertxCoreUtil;
+
+@Weave(originalName = "io.vertx.core.impl.future.FutureImpl")
+public abstract class FutureImpl_Instrumentation {
+
+    private Listener listener;
+
+    public abstract boolean isComplete();
+
+    @Trace(async = true, excludeFromTransactionTrace = true)
+    public void addListener(Listener listener) {
+        if (isComplete()) {
+            VertxCoreUtil.linkAndExpireToken(listener);
+        } else {
+            VertxCoreUtil.storeToken(listener);
+        }
+        Weaver.callOriginal();
+    }
+
+    @Trace(async = true, excludeFromTransactionTrace = true)
+    public boolean tryComplete(Object result) {
+        VertxCoreUtil.linkAndExpireToken(this.listener);
+        return Weaver.callOriginal();
+    }
+
+    @Trace(async = true, excludeFromTransactionTrace = true)
+    public boolean tryFail(Throwable cause) {
+        VertxCoreUtil.linkAndExpireToken(this.listener);
+        return Weaver.callOriginal();
+    }
+}
