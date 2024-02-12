deeed1f07e94bd8d51b5ca4bfe3c081edf4e89f5
edeleon <edeleon@newrelic.com>
2024-02-12 13:45:23 -0800

    updated extractDateString method and exception logging

--- newrelic-agent/src/main/java/com/newrelic/agent/logging/DeleteLogFilesRunnable.java
+++ newrelic-agent/src/main/java/com/newrelic/agent/logging/DeleteLogFilesRunnable.java
@@ -1,6 +1,5 @@
 package com.newrelic.agent.logging;
 
-import com.newrelic.api.agent.NewRelic;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,7 +11,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.logging.Level;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
@@ -26,13 +24,13 @@ public class DeleteLogFilesRunnable implements Runnable {
     private final int daysToKeepFiles;
     private final String fileNamePrefix;
 
-
-
     public DeleteLogFilesRunnable(Path logDirectoryPath, int fileCount, String fileNamePrefix) {
         this.logDirectoryPath = logDirectoryPath;
         this.daysToKeepFiles = fileCount;
         this.fileNamePrefix = fileNamePrefix;
-        //TODO init the pattern object here
+
+        pattern = Pattern.compile(fileNamePrefix.replace(".", "\\.")
+                + "(\\d{4}\\-(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01]))(\\.\\d)?$");
     }
 
     @Override
@@ -40,55 +38,45 @@ public class DeleteLogFilesRunnable implements Runnable {
         Thread.currentThread().setName("New Relic Expiring Log File Cleanup");
         Path logDirectory = Paths.get(logDirectoryPath.toString());
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        // TODO: create date instance here then pass
 
         try (Stream<Path> paths = Files.list(logDirectory)) {
             paths.forEach(path -> {
                 String fileName = path.getFileName().toString();
                 if (isValidLogFileName(fileName)) {
-                    //TODO not needed
                     try {
                         deleteIfOlderThanThreshold(path, fileName, dateFormat);
-                    } catch (IOException | ParseException e) {
-                        if (NewRelic.getAgent().getLogger().isLoggable(Level.FINEST)) {
+                    } catch (ParseException e) {
                             logger.error("Error deleting log file: " + logDirectoryPath + fileName, e);
-                        }
                     }
                 }
             });
         } catch (IOException e) {
-            if (NewRelic.getAgent().getLogger().isLoggable(Level.FINEST)) {
-                logger.error("Error listing log files in directory: " + logDirectoryPath, e);
-            }
+            logger.error("Error listing log files in directory: " + logDirectoryPath, e);
         }
     }
 
-    private void deleteIfOlderThanThreshold(Path filePath, String fileName, SimpleDateFormat dateFormat) {
+    private void deleteIfOlderThanThreshold(Path filePath, String fileName, SimpleDateFormat dateFormat) throws ParseException {
 
         String dateString = extractDateString(fileName);
-        //TODO wrap in try/catch... catch ParseException
+        Calendar threshold = Calendar.getInstance();
         Date fileDate;
         if (dateString != null) {
-            fileDate = dateFormat.parse(dateString);
+            try {
+                fileDate = dateFormat.parse(dateString);
+                threshold.setTime(fileDate);
+                threshold.add(Calendar.DAY_OF_YEAR, daysToKeepFiles);
+            } catch (ParseException e) {
+                logger.error("Error parsing log file date: ", e);
+            }
         } else {
             return;
         }
 
-        Calendar threshold = Calendar.getInstance();
-        threshold.setTime(fileDate);
-        threshold.add(Calendar.DAY_OF_YEAR, daysToKeepFiles);
-
         if (threshold.getTime().before(new Date())) {
             try {
                 Files.delete(filePath);
-//                if (NewRelic.getAgent().getLogger().isLoggable(Level.FINEST)) {
-//                    logger.log(Level.FINEST, "Daily log file deleted: " + filePath); //TODO: Not necessary to log file deletion
-//                }
             } catch (IOException e) {
-                //TODO always log it
-                if (NewRelic.getAgent().getLogger().isLoggable(Level.FINEST)) {
-                    logger.error("Error deleting expired log file: " + filePath, e);
-                }
+                logger.error("Error deleting expired log file: " + filePath, e);
             }
         }
     }
@@ -98,7 +86,6 @@ public class DeleteLogFilesRunnable implements Runnable {
     }
 
     private String extractDateString(String fileName) {
-        pattern = Pattern.compile(fileNamePrefix.replace(".", "\\.") + "(\\d{4}\\-(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01]))(\\.\\d)?$");
         Matcher matcher = pattern.matcher(fileName);
         return matcher.find() ? matcher.group(1) : null;
     }
