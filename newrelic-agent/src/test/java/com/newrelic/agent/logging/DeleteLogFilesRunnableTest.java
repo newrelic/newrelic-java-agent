3d82a6ac6efbee86bfee7117a0571f71ae80bfc9
edeleon <edeleon@newrelic.com>
2024-02-20 14:02:22 -0800

    added some edge case tests

--- newrelic-agent/src/test/java/com/newrelic/agent/logging/DeleteLogFilesRunnableTest.java
+++ newrelic-agent/src/test/java/com/newrelic/agent/logging/DeleteLogFilesRunnableTest.java
@@ -20,7 +20,7 @@ public class DeleteLogFilesRunnableTest {
 
     @Before
     public void setUp() throws IOException {
-        String fileNamePrefix = "testLogDir.";
+        String fileNamePrefix = "testLogDir";
         tempDir = Files.createTempDirectory(fileNamePrefix);
     }
 
@@ -41,15 +41,44 @@ public class DeleteLogFilesRunnableTest {
     }
 
     @Test
-    public void testDeleteOldFiles() throws Exception {
-        addOldLogFiles(5, "testLogDir.");
+    public void testRunnableAgainstEmptyLogDirectory() throws IOException {
+        tempDir = Files.createTempDirectory("emptyLogDir");
+
+        deleteLogFilesRunnable = new DeleteLogFilesRunnable(tempDir, 3, "testLogDir");
+        deleteLogFilesRunnable.run();
+
+        long actualLogFileCount = Files.list(tempDir).count();
+        assertEquals(0, actualLogFileCount);
+    }
+
+    @Test
+    public void testDeleteOldFilesWithLogExtensions() throws Exception {
+        addOldLogFilesWithExtensions(10, "testLogDir");
 
         long expectedLogFileCount = 5;
         long actualLogFileCount = Files.list(tempDir).count();
 
         assertEquals(expectedLogFileCount, actualLogFileCount);
 
-        deleteLogFilesRunnable = new DeleteLogFilesRunnable(tempDir, 3, "testLogDir.");
+        deleteLogFilesRunnable = new DeleteLogFilesRunnable(tempDir, 3, "testLogDir");
+        deleteLogFilesRunnable.run();
+
+        expectedLogFileCount = 0;
+        actualLogFileCount = Files.list(tempDir).count();
+
+        assertEquals(expectedLogFileCount, actualLogFileCount);
+    }
+
+    @Test
+    public void testDeleteOldFilesWithoutLogExtensions() throws IOException {
+        addOldLogFilesWithoutExtensions(10, "testLogDir");
+
+        long expectedLogFileCount = 5;
+        long actualLogFileCount = Files.list(tempDir).count();
+
+        assertEquals(expectedLogFileCount, actualLogFileCount);
+
+        deleteLogFilesRunnable = new DeleteLogFilesRunnable(tempDir, 3, "testLogDir");
         deleteLogFilesRunnable.run();
 
         expectedLogFileCount = 0;
@@ -63,13 +92,13 @@ public class DeleteLogFilesRunnableTest {
         Files.createFile(tempDir.resolve("invalid_log_file.txt"));
         Files.createFile(tempDir.resolve("invalid.txt"));
         Files.createFile(tempDir.resolve("2024-02-04"));
-        addOldLogFiles(10, "testLogDir.");
+        addOldLogFilesWithExtensions(10, "testLogDir");
 
         long expectedLogFileCount = 8;
         long actualLogFileCount = Files.list(tempDir).count();
         assertEquals(expectedLogFileCount, actualLogFileCount);
 
-        deleteLogFilesRunnable = new DeleteLogFilesRunnable(tempDir, 1, "testLogDir.");
+        deleteLogFilesRunnable = new DeleteLogFilesRunnable(tempDir, 1, "testLogDir");
         deleteLogFilesRunnable.run();
 
         expectedLogFileCount = 3;
@@ -86,22 +115,36 @@ public class DeleteLogFilesRunnableTest {
         Path readOnlyFile = Files.createFile(tempDir.resolve("readonly.log"));
         readOnlyFile.toFile().setReadOnly();
 
-        deleteLogFilesRunnable = new DeleteLogFilesRunnable(tempDir, 3, "testLogDir.");
+        deleteLogFilesRunnable = new DeleteLogFilesRunnable(tempDir, 3, "testLogDir");
         deleteLogFilesRunnable.run();
 
         assertTrue("Read-only file should still exist", Files.exists(readOnlyFile));
     }
 
-    private void addOldLogFiles(int daysAgo, String fileNamePrefix) throws IOException {
+    private void addOldLogFilesWithExtensions(int daysAgo, String fileNamePrefix) throws IOException {
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DAY_OF_YEAR, -daysAgo);
         Date date = calendar.getTime();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
         for (int i = 0; i < 5; i++) {
-            String fileName = fileNamePrefix + dateFormat.format(date) + "." + (i + 1);
+            String fileName = fileNamePrefix + "." + dateFormat.format(date) + "." + (i + 1);
             Files.createFile(tempDir.resolve(fileName));
             calendar.add(Calendar.MINUTE, 1);
+            date = calendar.getTime();
+        }
+    }
+
+    private void addOldLogFilesWithoutExtensions(int daysAgo, String fileNamePrefix) throws IOException {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, -daysAgo);
+        Date date = calendar.getTime();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        for (int i = 0; i < 5; i++) {
+            String fileName = fileNamePrefix + "." + dateFormat.format(date);
+            Files.createFile(tempDir.resolve(fileName));
+            calendar.add(Calendar.DAY_OF_YEAR, 1);
             date = calendar.getTime();
         }
     }
