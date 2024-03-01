97330dc7870b6a6e8c921e79168d2531dbb887d2
Enriqueta De Leon <edeleon@newrelic.com>
2024-02-29 16:29:09 -0800

    Add Executor Service To Delete Logs When log_daily Is Enabled (#1754)
    Resolves #1688

--- newrelic-agent/src/main/java/com/newrelic/agent/logging/FileAppenderFactory.java
+++ newrelic-agent/src/main/java/com/newrelic/agent/logging/FileAppenderFactory.java
@@ -12,13 +12,18 @@ import org.apache.logging.log4j.core.appender.FileAppender;
 import org.apache.logging.log4j.core.appender.FileManager;
 import org.apache.logging.log4j.core.appender.RollingFileAppender;
 import org.apache.logging.log4j.core.appender.rolling.CompositeTriggeringPolicy;
-import org.apache.logging.log4j.core.appender.rolling.CronTriggeringPolicy;
 import org.apache.logging.log4j.core.appender.rolling.DefaultRolloverStrategy;
 import org.apache.logging.log4j.core.appender.rolling.NoOpTriggeringPolicy;
 import org.apache.logging.log4j.core.appender.rolling.SizeBasedTriggeringPolicy;
+import org.apache.logging.log4j.core.appender.rolling.TimeBasedTriggeringPolicy;
 import org.apache.logging.log4j.core.appender.rolling.TriggeringPolicy;
-import org.apache.logging.log4j.core.config.DefaultConfiguration;
 import org.apache.logging.log4j.core.layout.PatternLayout;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import static com.newrelic.agent.logging.Log4jLogger.CONVERSION_PATTERN;
 
@@ -39,22 +44,36 @@ public class FileAppenderFactory {
      */
     static final String FILE_APPENDER_NAME = "File";
 
+    /**
+     * Initial delay for scheduling log files cleanup task (in seconds).
+     */
+    private static final long INITIAL_DELAY_SECONDS = 60;
+
+    /**
+     * Repeat interval for scheduling log files cleanup task (in seconds).
+     */
+    private static final int REPEAT_INTERVAL_SECONDS = 24 * 60 * 60;
+
+
     private final int fileCount;
     private final long logLimitBytes;
     private final String fileName;
     private final boolean isDaily;
+    private final String path;
 
     /**
      * @param fileCount maximum number of log files
      * @param logLimitBytes maximum size of a given log file
      * @param fileName prefix for log file names
      * @param isDaily if the logs are to be rolled over daily
+     * @param path directory path for log files
      */
-    public FileAppenderFactory(int fileCount, long logLimitBytes, String fileName, boolean isDaily) {
+    public FileAppenderFactory(int fileCount, long logLimitBytes, String fileName, boolean isDaily, String path) {
         this.fileCount = fileCount;
         this.logLimitBytes = logLimitBytes;
         this.fileName = fileName;
         this.isDaily = isDaily;
+        this.path = path;
     }
 
     /**
@@ -99,13 +118,29 @@ public class FileAppenderFactory {
     private RollingFileAppender buildDailyRollingAppender() {
 
         TriggeringPolicy policy = buildRollingAppenderTriggeringPolicy();
-        DefaultRolloverStrategy rolloverStrategy = DefaultRolloverStrategy.newBuilder().withMax(String.valueOf(fileCount)).build();
+        DefaultRolloverStrategy rolloverStrategy = DefaultRolloverStrategy.newBuilder()
+                .withMax(String.valueOf(fileCount))
+                .build();
 
         String filePattern = fileName + ".%d{yyyy-MM-dd}";
         if (logLimitBytes > 0) {
             // If we might roll within a day, use a number ordering suffix
             filePattern = fileName + ".%d{yyyy-MM-dd}.%i";
         }
+
+        Path directory = new File(this.path).toPath();
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor(runnable -> {
+            Thread thread = new Thread(runnable);
+            thread.setName("New Relic Expiring Log File Cleanup");
+            thread.setDaemon(true);
+            return thread;
+        });
+        executorService.scheduleWithFixedDelay(
+                new ClearExpiredLogsRunnable(directory, fileCount, fileName),
+                INITIAL_DELAY_SECONDS,
+                REPEAT_INTERVAL_SECONDS,
+                TimeUnit.SECONDS
+        );
 
         return initializeRollingFileAppender()
                 .withPolicy(policy)
@@ -115,7 +150,7 @@ public class FileAppenderFactory {
     }
 
     private TriggeringPolicy buildRollingAppenderTriggeringPolicy() {
-        TriggeringPolicy timeBasedTriggeringPolicy = CronTriggeringPolicy.createPolicy(new DefaultConfiguration(), "true", DAILY_CRON);
+        TimeBasedTriggeringPolicy timeBasedTriggeringPolicy = TimeBasedTriggeringPolicy.newBuilder().withInterval(1).withModulate(true).build();
         TriggeringPolicy sizeBasedTriggeringPolicy = sizeBasedPolicy();
         return CompositeTriggeringPolicy.createPolicy(timeBasedTriggeringPolicy, sizeBasedTriggeringPolicy);
     }
