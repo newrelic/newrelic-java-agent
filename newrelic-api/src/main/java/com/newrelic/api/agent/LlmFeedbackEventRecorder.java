b22730eff54c6bf40a400adb5c97c010752032b6
edeleon <edeleon@newrelic.com>
2024-03-14 17:14:42 -0700

    added ai monitoring implementation

---
+++ newrelic-api/src/main/java/com/newrelic/api/agent/LlmFeedbackEventRecorder.java
@@ -0,0 +1,38 @@
+package com.newrelic.api.agent;
+
+import java.util.Map;
+
+/**
+ * A utility class for recording LlmFeedbackMessage events using the AI Monitoring API.
+ * <p>
+ * This class implements the {@link AiMonitoring} interface and provides a method to record LlmFeedbackMessage events
+ * by delegating to the Insights API for custom event recording.
+ */
+
+public class LlmFeedbackEventRecorder implements AiMonitoring {
+    /**
+     * Records an LlmFeedbackMessage event.
+     *
+     * @param llmFeedbackEventAttributes A map containing the attributes of an LlmFeedbackMessage event. To construct
+     *                                   the llmFeedbackEventAttributes map, use
+     *                                   {@link LlmFeedbackEventAttributes.Builder}
+     *                                   <p>The map must include:</p>
+     *                                   <ul>
+     *                                   <li>"traceId" (String): Trace ID where the chat completion related to the
+     *                                   feedback event occurred</li>
+     *                                   <li>"rating" (Integer/String): Rating provided by an end user</li>
+     *                                   </ul>
+     *                                   Optional attributes:
+     *                                   <ul>
+     *                                   <li>"category" (String): Category of the feedback as provided by the end user</li>
+     *                                   <li>"message" (String): Freeform text feedback from an end user.</li>
+     *                                   <li>"metadata" (Map&lt;String, String&gt;): Set of key-value pairs to store
+     *                                   additional data to submit with the feedback event</li>
+     *                                   </ul>
+     */
+    @Override
+    public void recordLlmFeedbackEvent(Map<String, Object> llmFeedbackEventAttributes) {
+        // Delegate to Insights API for event recording
+        NewRelic.getAgent().getInsights().recordCustomEvent("LlmFeedbackMessage", llmFeedbackEventAttributes);
+    }
+}
