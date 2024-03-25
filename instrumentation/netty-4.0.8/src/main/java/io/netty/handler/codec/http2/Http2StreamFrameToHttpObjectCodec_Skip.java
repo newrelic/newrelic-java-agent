392384781848fd78c7b4a834b3b0673455b30afa
Jason Keller <jasonjkeller@gmail.com>
2024-03-25 10:15:48 -0700

    Add copyright header

--- instrumentation/netty-4.0.8/src/main/java/io/netty/handler/codec/http2/Http2StreamFrameToHttpObjectCodec_Skip.java
+++ instrumentation/netty-4.0.8/src/main/java/io/netty/handler/codec/http2/Http2StreamFrameToHttpObjectCodec_Skip.java
@@ -1,3 +1,10 @@
+/*
+ *
+ *  * Copyright 2024 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
 package io.netty.handler.codec.http2;
 
 import com.newrelic.api.agent.weaver.SkipIfPresent;
