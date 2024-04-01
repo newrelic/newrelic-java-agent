51e1285801362f6de09ac68d28e412fe5baeaaa7
edeleon <edeleon@newrelic.com>
2024-04-01 03:41:06 -0700

    refactored class

--- agent-bridge/src/main/java/com/newrelic/agent/bridge/aimonitoring/LlmTokenCountCallbackHolder.java
+++ agent-bridge/src/main/java/com/newrelic/agent/bridge/aimonitoring/LlmTokenCountCallbackHolder.java
@@ -3,16 +3,27 @@ package com.newrelic.agent.bridge.aimonitoring;
 import com.newrelic.api.agent.LlmTokenCountCallback;
 
 /**
- * storage for an instance of {@link LlmTokenCountCallback}.
+ * A thread-safe holder for an instance of {@link LlmTokenCountCallback}.
+ * This class provides methods for setting and retrieving the callback instance.
  */
 public class LlmTokenCountCallbackHolder {
 
     private static volatile LlmTokenCountCallback llmTokenCountCallback = null;
 
+    /**
+     * Sets the {@link LlmTokenCountCallback} instance to be stored.
+     *
+     * @param newLlmTokenCountCallback the callback instance
+     */
     public static void setLlmTokenCountCallback(LlmTokenCountCallback newLlmTokenCountCallback) {
         llmTokenCountCallback = newLlmTokenCountCallback;
     }
 
+    /**
+     * Retrieves the stored {@link LlmTokenCountCallback} instance.
+     *
+     * @return stored callback instance
+     */
     public static LlmTokenCountCallback getLlmTokenCountCallback() {
         return llmTokenCountCallback;
     }
