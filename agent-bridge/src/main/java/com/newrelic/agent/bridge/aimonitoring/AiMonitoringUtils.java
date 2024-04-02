434c3552b97d57f9daf880d7291e03f3a4f0241c
Jason Keller <jasonjkeller@gmail.com>
2024-04-02 16:24:48 -0700

    Disable AIM if HSM is enabled

--- agent-bridge/src/main/java/com/newrelic/agent/bridge/aimonitoring/AiMonitoringUtils.java
+++ agent-bridge/src/main/java/com/newrelic/agent/bridge/aimonitoring/AiMonitoringUtils.java
@@ -7,13 +7,17 @@
 
 package com.newrelic.agent.bridge.aimonitoring;
 
+import com.newrelic.api.agent.Config;
 import com.newrelic.api.agent.NewRelic;
+
+import java.util.logging.Level;
 
 public class AiMonitoringUtils {
     // Enabled defaults
     private static final boolean AI_MONITORING_ENABLED_DEFAULT = false;
     private static final boolean AI_MONITORING_STREAMING_ENABLED_DEFAULT = true;
     private static final boolean AI_MONITORING_RECORD_CONTENT_ENABLED_DEFAULT = true;
+    private static final boolean HIGH_SECURITY_ENABLED_DEFAULT = false;
 
     /**
      * Check if ai_monitoring features are enabled.
@@ -22,15 +26,20 @@ public class AiMonitoringUtils {
      * @return true if AI monitoring is enabled, else false
      */
     public static boolean isAiMonitoringEnabled() {
-        Boolean enabled = NewRelic.getAgent().getConfig().getValue("ai_monitoring.enabled", AI_MONITORING_ENABLED_DEFAULT);
+        Config config = NewRelic.getAgent().getConfig();
+        Boolean aimEnabled = config.getValue("ai_monitoring.enabled", AI_MONITORING_ENABLED_DEFAULT);
+        Boolean highSecurity = config.getValue("high_security", HIGH_SECURITY_ENABLED_DEFAULT);
 
-        if (enabled) {
-            NewRelic.incrementCounter("Supportability/Java/ML/Enabled");
-        } else {
+        if (highSecurity || !aimEnabled) {
+            aimEnabled = false;
+            String disabledReason = highSecurity ? "High Security Mode." : "agent config.";
+            NewRelic.getAgent().getLogger().log(Level.FINE, "AIM: AI Monitoring is disabled due to " + disabledReason);
             NewRelic.incrementCounter("Supportability/Java/ML/Disabled");
+        } else {
+            NewRelic.incrementCounter("Supportability/Java/ML/Enabled");
         }
 
-        return enabled;
+        return aimEnabled;
     }
 
     /**
