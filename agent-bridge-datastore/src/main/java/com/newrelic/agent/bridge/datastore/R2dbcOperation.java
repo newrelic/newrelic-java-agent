cd16de1fcc32dfc246287137786149d5d3aa456a
Kate Anderson <kateanderson@newrelic.com>
2024-04-26 14:36:50 -0700

    Add match.find() to timing

--- agent-bridge-datastore/src/main/java/com/newrelic/agent/bridge/datastore/R2dbcOperation.java
+++ agent-bridge-datastore/src/main/java/com/newrelic/agent/bridge/datastore/R2dbcOperation.java
@@ -42,9 +42,10 @@ public class R2dbcOperation {
                     for (Pattern pattern : operation.getValue()) {
                         long start = System.currentTimeMillis();
                         Matcher matcher = pattern.matcher(strippedSql);
+                        boolean foundMatch = matcher.find();
                         long matchTime = System.currentTimeMillis() - start;
-                        AgentBridge.getAgent().getLogger().log(Level.FINEST, "NR-262136: regex pattern matching for " + opName + " took " + matchTime);
-                        if (matcher.find()) {
+                        AgentBridge.getAgent().getLogger().log(Level.FINEST, "NR-262136: finding match for " + opName + " took " + matchTime);
+                        if (foundMatch) {
                             String model = matcher.groupCount() > 0 ? removeBrackets(unquoteDatabaseName(matcher.group(1).trim())) : "unknown";
                             return new OperationAndTableName(operation.getKey(), VALID_METRIC_NAME_MATCHER.matcher(model).matches() ? model : "ParseError");
                         }
