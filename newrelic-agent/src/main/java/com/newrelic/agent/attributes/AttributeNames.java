ff9d03573cdf2c1a26ea32874bbd5895c8380410
Oren Ben-Meir <orenbm11@gmail.com>
2024-05-16 15:57:05 -0400

    Change message broker host attribute to be message.broker.endpoint

--- newrelic-agent/src/main/java/com/newrelic/agent/attributes/AttributeNames.java
+++ newrelic-agent/src/main/java/com/newrelic/agent/attributes/AttributeNames.java
@@ -57,7 +57,7 @@ public final class AttributeNames {
     public static final String CLOUD_RESOURCE_ID = "cloud.resource_id";
 
     // Message broker endpoint information
-    public static final String MESSAGE_BROKER_HOST = "message.broker.host";
+    public static final String MESSAGE_BROKER_HOST = "message.broker.endpoint";
     public static final String MESSAGE_BROKER_PORT = "message.broker.port";
 
     public static final String RESPONSE_CONTENT_TYPE_PARAMETER_NAME = "response.headers.contentType";
