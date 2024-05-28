403c35fa8a413e98410f87f96523d4321e5217de
Oren Ben-Meir <orenbm11@gmail.com>
2024-05-28 15:01:26 -0400

    Get endpoint information outside flux subscription

--- instrumentation/r2dbc-postgresql-0.9.2/src/main/java/io/r2dbc/postgresql/R2dbcUtils.java
+++ instrumentation/r2dbc-postgresql-0.9.2/src/main/java/io/r2dbc/postgresql/R2dbcUtils.java
@@ -21,29 +21,31 @@ import io.r2dbc.postgresql.client.Client;
 import io.r2dbc.postgresql.client.ReactorNettyClient_Instrumentation;
 import org.reactivestreams.Subscription;
 import reactor.core.publisher.Flux;
+import reactor.netty.Connection;
 import reactor.util.annotation.Nullable;
 
+import java.net.InetSocketAddress;
 import java.util.function.Consumer;
 
 public class R2dbcUtils {
     public static Flux<PostgresqlResult> wrapRequest(Flux<PostgresqlResult> request, String sql, Client client, PostgresqlConnectionConfiguration connectionConfiguration) {
         if(request != null) {
             Transaction transaction = NewRelic.getAgent().getTransaction();
-            if(transaction != null && !(transaction instanceof NoOpTransaction)) {
+            EndpointData endpointData = extractEndpointData(client);
+            if(transaction != null && !(transaction instanceof NoOpTransaction) && endpointData != null) {
                 Segment segment = transaction.startSegment("execute");
                 return request
-                        .doOnSubscribe(reportExecution(sql, client, connectionConfiguration, segment))
+                        .doOnSubscribe(reportExecution(sql, endpointData, connectionConfiguration, segment))
                         .doFinally((type) -> segment.end());
             }
         }
         return request;
     }
 
-    private static Consumer<Subscription> reportExecution(String sql, Client client, PostgresqlConnectionConfiguration connectionConfiguration, Segment segment) {
+    private static Consumer<Subscription> reportExecution(String sql, EndpointData endpointData, PostgresqlConnectionConfiguration connectionConfiguration, Segment segment) {
         return (subscription) -> {
             OperationAndTableName sqlOperation = R2dbcOperation.extractFrom(sql);
-            EndpointData endpointData = extractEndpointData(client);
-            if (sqlOperation != null && endpointData != null) {
+            if (sqlOperation != null) {
                 segment.reportAsExternal(DatastoreParameters
                         .product(DatastoreVendor.Postgres.name())
                         .collection(sqlOperation.getTableName())
