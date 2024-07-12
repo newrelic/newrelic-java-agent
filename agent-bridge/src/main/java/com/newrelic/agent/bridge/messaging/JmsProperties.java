3bcc104eacc0fde726b1e1f151018d46cd84efb6
Oren Ben-Meir <orenbm11@gmail.com>
2024-07-12 12:44:57 -0400

    Add copyright headers to new agent bridge classes

--- agent-bridge/src/main/java/com/newrelic/agent/bridge/messaging/JmsProperties.java
+++ agent-bridge/src/main/java/com/newrelic/agent/bridge/messaging/JmsProperties.java
@@ -1,3 +1,9 @@
+/*
+ *
+ *  * Copyright 2024 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
 package com.newrelic.agent.bridge.messaging;
 
 public class JmsProperties {
