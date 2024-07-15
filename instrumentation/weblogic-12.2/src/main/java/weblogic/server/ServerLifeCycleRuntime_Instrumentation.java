2119c3e61469e9de8067542760c1bf36cc9d44b6
Jerry Duffy <jtduffy@users.noreply.github.com>
2024-07-15 15:50:27 -0400

    WeaveAllConstr in WL 12.2 module

--- instrumentation/weblogic-12.2/src/main/java/weblogic/server/ServerLifeCycleRuntime_Instrumentation.java
+++ instrumentation/weblogic-12.2/src/main/java/weblogic/server/ServerLifeCycleRuntime_Instrumentation.java
@@ -10,12 +10,19 @@ package weblogic.server;
 import com.newrelic.agent.bridge.AgentBridge;
 import com.newrelic.api.agent.weaver.Weave;
 
+import com.newrelic.api.agent.weaver.WeaveAllConstructors;
 import weblogic.management.configuration.ServerTemplateMBean;
 import weblogic.version;
 
+import java.util.logging.Level;
+
 @Weave(originalName = "weblogic.server.ServerLifeCycleRuntime")
 public final class ServerLifeCycleRuntime_Instrumentation {
-    ServerLifeCycleRuntime_Instrumentation(final ServerTemplateMBean serverMBean) {
+    @WeaveAllConstructors
+    ServerLifeCycleRuntime_Instrumentation() {
+        AgentBridge.getAgent().getLogger().log(Level.FINEST, "ServerLifeCycleRuntime_Instrumentation - constructor called");
+        AgentBridge.getAgent().getLogger().log(Level.FINEST, "ServerLifeCycleRuntime_Instrumentation - weblogic.Name sysprop value: {0}",
+                System.getProperty("weblogic.Name"));
         AgentBridge.publicApi.setServerInfo("WebLogic", version.getVersions().substring(16,26));
         AgentBridge.publicApi.setInstanceName(System.getProperty("weblogic.Name"));
 
