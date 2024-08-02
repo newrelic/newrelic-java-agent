3fa82210a6bfb917ff7e3a623eacec5fc043a859
edeleon <edeleon@newrelic.com>
2024-08-02 16:09:49 -0700

    update/add copyright header

--- instrumentation/jedis-5.0.0/src/main/java/redis/clients/jedis/JedisPubSubBase_Instrumentation.java
+++ instrumentation/jedis-5.0.0/src/main/java/redis/clients/jedis/JedisPubSubBase_Instrumentation.java
@@ -1,3 +1,10 @@
+/*
+ *
+ *  * Copyright 2024 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
 package redis.clients.jedis;
 
 import com.newrelic.agent.bridge.datastore.DatastoreVendor;
