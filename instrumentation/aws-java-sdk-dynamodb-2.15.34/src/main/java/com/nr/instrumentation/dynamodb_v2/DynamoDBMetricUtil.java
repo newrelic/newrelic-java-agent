e97a4913c13fe8659454d00d6c478d7639b1cd09
Andre Onuki <aonuki@newrelic.com>
2024-09-23 15:07:33 -0400

    DynamoDB

--- instrumentation/aws-java-sdk-dynamodb-2.15.34/src/main/java/com/nr/instrumentation/dynamodb_v2/DynamoDBMetricUtil.java
+++ instrumentation/aws-java-sdk-dynamodb-2.15.34/src/main/java/com/nr/instrumentation/dynamodb_v2/DynamoDBMetricUtil.java
@@ -1,35 +1,122 @@
 package com.nr.instrumentation.dynamodb_v2;
 
+import com.newrelic.agent.bridge.AgentBridge;
 import com.newrelic.agent.bridge.datastore.DatastoreVendor;
+import com.newrelic.api.agent.CloudAccountInfo;
 import com.newrelic.api.agent.DatastoreParameters;
+import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.TracedMethod;
+import software.amazon.awssdk.awscore.client.config.AwsClientOption;
+import software.amazon.awssdk.core.client.config.SdkClientConfiguration;
+import software.amazon.awssdk.core.client.config.SdkClientOption;
+import software.amazon.awssdk.regions.Region;
 
 import java.net.URI;
+import java.util.logging.Level;
 
 public abstract class DynamoDBMetricUtil {
 
     private static final String PRODUCT = DatastoreVendor.DynamoDB.name();
     private static final String INSTANCE_HOST = "amazon";
-    private static final String INSTANCE_ID = "dynamodb";
 
-    public static void metrics(TracedMethod tracedMethod, String operation, String collection, URI endpoint) {
-        String host = endpoint == null ? INSTANCE_HOST : endpoint.getHost();
-        String port = endpoint == null ? INSTANCE_ID : String.valueOf(getPort(endpoint));
+    public static void metrics(TracedMethod tracedMethod, String operation, String tableName, Object sdkClient, SdkClientConfiguration clientConfiguration) {
 
+        String host = INSTANCE_HOST;
+        Integer port = null;
+        String arn = null;
+        if (clientConfiguration != null) {
+            URI endpoint = clientConfiguration.option(SdkClientOption.ENDPOINT);
+            if (endpoint != null) {
+                host = endpoint.getHost();
+                port = getPort(endpoint);
+            }
+            arn = getArn(tableName, sdkClient, clientConfiguration, host);
+        }
         DatastoreParameters params = DatastoreParameters
                 .product(PRODUCT)
-                .collection(collection)
+                .collection(tableName)
                 .operation(operation)
                 .instance(host, port)
                 .noDatabaseName()
+                .cloudResourceId(arn)
                 .build();
 
         tracedMethod.reportAsExternal(params);
     }
 
-    private static int getPort(URI endpoint) {
-        if (endpoint.getPort() > 0) {
-            return endpoint.getPort();
+    // visible for testing
+    static String getArn(String tableName, Object sdkClient, SdkClientConfiguration clientConfiguration, String host) {
+        String accountId = AgentBridge.cloud.getAccountInfo(sdkClient, CloudAccountInfo.AWS_ACCOUNT_ID);
+        if (accountId == null) {
+            NewRelic.getAgent().getLogger().log(Level.FINEST, "Unable to assemble ARN. No account information provided.");
+            return null;
+        }
+
+        if (tableName == null) {
+            NewRelic.getAgent().getLogger().log(Level.FINEST, "Unable to assemble ARN. Table name is null.");
+            return null;
+        }
+
+        String region = findRegion(clientConfiguration, host);
+        if (region == null) {
+            NewRelic.getAgent().getLogger().log(Level.FINEST, "Unable to assemble ARN. Region is null.");
+            return null;
+        }
+
+        // arn:${Partition}:dynamodb:${Region}:${Account}:table/${TableName}
+        return "arn:aws:dynamodb:" + region + ":" + accountId + ":table/" + tableName;
+    }
+
+    // visible for testing
+    static String findRegion(SdkClientConfiguration clientConfig, String host) {
+        Boolean endpointOverridden = clientConfig.option(SdkClientOption.ENDPOINT_OVERRIDDEN);
+        if (endpointOverridden == Boolean.TRUE) { // endpointOverridden could be null
+            return findRegionFromHost(host);
+        }
+
+        Region awsRegion = clientConfig.option(AwsClientOption.AWS_REGION);
+        if (awsRegion != null) {
+            return awsRegion.id();
+        }
+
+        return null;
+    }
+
+    // visible for testing
+    static String findRegionFromHost(String host) {
+        if (host == null) {
+            return null;
+        }
+        if (!host.startsWith("dynamodb")) {
+            return null;
+        }
+
+        final int afterDynamoDb = 8; // "dynamodb".length()
+        if (host.charAt(afterDynamoDb) == '.') {
+            // dynamodb.{region}.amazonaws.com
+            int secondPeriod = host.indexOf('.', afterDynamoDb + 1);
+            if (secondPeriod > 0 && host.startsWith(".amazonaws.com", secondPeriod)) {
+                return host.substring(afterDynamoDb + 1, secondPeriod);
+            } else {
+                return null;
+            }
+        } else if (host.startsWith("-fips.", afterDynamoDb)) {
+            // dynamodb-fips.{region}.amazonaws.com
+            final int firstPeriod = 13; // "dynamodb-fips".length()
+            int secondPeriod = host.indexOf('.', firstPeriod + 1);
+            if (secondPeriod > 0 && host.startsWith(".amazonaws.com", secondPeriod)) {
+                return host.substring(firstPeriod + 1, secondPeriod);
+            } else {
+                return null;
+            }
+        }
+        return null;
+    }
+
+    private static Integer getPort(URI endpoint) {
+        int port = endpoint.getPort();
+        if (port > 0) {
+            return port;
         }
 
         final String scheme = endpoint.getScheme();
@@ -38,8 +125,7 @@ public abstract class DynamoDBMetricUtil {
         } else if ("https".equalsIgnoreCase(scheme)) {
             return 443;
         }
-        return -1;
+        return null;
     }
-
 }
 
---
+++ instrumentation/aws-java-sdk-dynamodb-2.15.34/src/test/java/com/nr/instrumentation/dynamodb_v2/DynamoDBMetricUtilTest.java
@@ -0,0 +1,204 @@
+/*
+ *
+ *  * Copyright 2024 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package com.nr.instrumentation.dynamodb_v2;
+
+import com.newrelic.agent.bridge.AgentBridge;
+import com.newrelic.agent.bridge.CloudApi;
+import com.newrelic.api.agent.CloudAccountInfo;
+import com.newrelic.api.agent.DatastoreParameters;
+import com.newrelic.api.agent.TracedMethod;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import software.amazon.awssdk.awscore.client.config.AwsClientOption;
+import software.amazon.awssdk.core.client.config.SdkClientConfiguration;
+import software.amazon.awssdk.core.client.config.SdkClientOption;
+import software.amazon.awssdk.regions.Region;
+
+import java.net.URI;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class DynamoDBMetricUtilTest {
+
+    private CloudApi previousCloudApi;
+
+    @Before
+    public void setup() {
+        previousCloudApi = AgentBridge.cloud;
+        AgentBridge.cloud = mock(CloudApi.class);
+    }
+
+    @After
+    public void tearDown() {
+        AgentBridge.cloud = previousCloudApi;
+    }
+
+    @Test
+    public void testFindRegionFromHost() {
+        String dynamoDbEndpoint = "dynamodb.us-west-2.amazonaws.com";
+        assertEquals("us-west-2", DynamoDBMetricUtil.findRegionFromHost(dynamoDbEndpoint));
+
+        String fipsEndpoint = "dynamodb-fips.my-region.amazonaws.com";
+        assertEquals("my-region", DynamoDBMetricUtil.findRegionFromHost(fipsEndpoint));
+
+        String localhost = "localhost";
+        assertNull(DynamoDBMetricUtil.findRegionFromHost(localhost));
+
+        // null
+        assertNull(DynamoDBMetricUtil.findRegionFromHost(null));
+        assertNull(DynamoDBMetricUtil.findRegionFromHost("dynamodb.ms-region.azure.com"));
+        assertNull(DynamoDBMetricUtil.findRegionFromHost("dynamodb-fips.gg-region.gcp.com"));
+        assertNull(DynamoDBMetricUtil.findRegionFromHost("cosmosdb.us-east-1.amazonaws.com"));
+    }
+
+    @Test
+    public void testFindRegion_fromRegion() {
+        String host = "dynamodb.us-west-2.amazonaws.com";
+        SdkClientConfiguration clientConfig = SdkClientConfiguration.builder()
+                .option(AwsClientOption.AWS_REGION, Region.US_WEST_2)
+                .option(SdkClientOption.ENDPOINT, URI.create("https://" + host))
+                .build();
+        assertEquals("us-west-2", DynamoDBMetricUtil.findRegion(clientConfig, host));
+    }
+
+    @Test
+    public void testFindRegion_fromEndpoint() {
+        String host = "dynamodb.ap-east-1.amazonaws.com";
+        SdkClientConfiguration clientConfig = SdkClientConfiguration.builder()
+                .option(AwsClientOption.AWS_REGION, Region.US_WEST_2)
+                .option(SdkClientOption.ENDPOINT, URI.create("https://" + host))
+                .option(SdkClientOption.ENDPOINT_OVERRIDDEN, Boolean.TRUE)
+                .build();
+        assertEquals("ap-east-1", DynamoDBMetricUtil.findRegion(clientConfig, host));
+    }
+
+    @Test
+    public void testFindRegion_fail() {
+        SdkClientConfiguration clientConfig = SdkClientConfiguration.builder()
+                .build();
+        String host = null;
+        assertNull(DynamoDBMetricUtil.findRegion(clientConfig, host));
+    }
+
+
+    @Test
+    public void testGetArn() {
+        Object sdkClient = new Object();
+        when(AgentBridge.cloud.getAccountInfo(eq(sdkClient), eq(CloudAccountInfo.AWS_ACCOUNT_ID)))
+                .thenReturn("123456789");
+        SdkClientConfiguration config = SdkClientConfiguration.builder()
+                .option(AwsClientOption.AWS_REGION, Region.US_EAST_2)
+                .build();
+        String table = "test";
+        String host = "dynamodb.us-east-2.amazonaws.com";
+
+        String arn = DynamoDBMetricUtil.getArn(table, sdkClient, config, host);
+        assertEquals("arn:aws:dynamodb:us-east-2:123456789:table/test", arn);
+    }
+
+    @Test
+    public void testGetArn_withoutAccountId() {
+        Object sdkClient = new Object();
+        SdkClientConfiguration config = SdkClientConfiguration.builder()
+                .option(AwsClientOption.AWS_REGION, Region.US_EAST_2)
+                .option(SdkClientOption.ENDPOINT, URI.create("https://dynamodb.us-east-2.amazonaws.com"))
+                .build();
+        String table = "test";
+        String host = "dynamodb.us-east-2.amazonaws.com";
+
+        String arn = DynamoDBMetricUtil.getArn(table, sdkClient, config, host);
+        assertNull(arn);
+    }
+
+    @Test
+    public void testGetArn_withoutTable() {
+        Object sdkClient = new Object();
+        when(AgentBridge.cloud.getAccountInfo(eq(sdkClient), eq(CloudAccountInfo.AWS_ACCOUNT_ID)))
+                .thenReturn("123456789");
+        SdkClientConfiguration config = SdkClientConfiguration.builder()
+                .option(AwsClientOption.AWS_REGION, Region.US_EAST_2)
+                .build();
+        String host = "dynamodb.us-east-2.amazonaws.com";
+
+        String arn = DynamoDBMetricUtil.getArn(null, sdkClient, config, host);
+        assertNull(arn);
+    }
+
+    @Test
+    public void testGetArn_withoutRegion() {
+        Object sdkClient = new Object();
+        when(AgentBridge.cloud.getAccountInfo(eq(sdkClient), eq(CloudAccountInfo.AWS_ACCOUNT_ID)))
+                .thenReturn("123456789");
+        SdkClientConfiguration config = SdkClientConfiguration.builder()
+                .build();
+        String table = "test";
+        String host = "dynamodb.us-east-2.amazonaws.com";
+
+        String arn = DynamoDBMetricUtil.getArn(table, sdkClient, config, host);
+        assertNull(arn);
+    }
+
+    @Test
+    public void testMetrics() {
+        Object sdkClient = new Object();
+        when(AgentBridge.cloud.getAccountInfo(eq(sdkClient), eq(CloudAccountInfo.AWS_ACCOUNT_ID)))
+                .thenReturn("123456789");
+        SdkClientConfiguration config = SdkClientConfiguration.builder()
+                .option(AwsClientOption.AWS_REGION, Region.US_EAST_2)
+                .option(SdkClientOption.ENDPOINT, URI.create("https://dynamodb.us-east-2.amazonaws.com"))
+                .build();
+        String table = "test";
+        TracedMethod tracedMethod = mock(TracedMethod.class);
+        String operation = "getItem";
+
+        DynamoDBMetricUtil.metrics(tracedMethod, operation, table, sdkClient, config);
+
+        ArgumentCaptor<DatastoreParameters> externalParamsCaptor = ArgumentCaptor.forClass(DatastoreParameters.class);
+        verify(tracedMethod).reportAsExternal(externalParamsCaptor.capture());
+        DatastoreParameters params = externalParamsCaptor.getValue();
+        assertEquals("DynamoDB", params.getProduct());
+        assertEquals("test", params.getCollection());
+        assertEquals("getItem", params.getOperation());
+        assertEquals("dynamodb.us-east-2.amazonaws.com", params.getHost());
+        assertEquals(Integer.valueOf(443), params.getPort());
+        assertNull(params.getPathOrId());
+        assertEquals("arn:aws:dynamodb:us-east-2:123456789:table/test", params.getCloudResourceId());
+        assertNull(params.getDatabaseName());
+    }
+
+    @Test
+    public void testMetrics_withoutClientConfig() {
+        Object sdkClient = new Object();
+        when(AgentBridge.cloud.getAccountInfo(eq(sdkClient), eq(CloudAccountInfo.AWS_ACCOUNT_ID)))
+                .thenReturn("123456789");
+        String table = "test";
+        TracedMethod tracedMethod = mock(TracedMethod.class);
+        String operation = "getItem";
+
+        DynamoDBMetricUtil.metrics(tracedMethod, operation, table, sdkClient, null);
+
+        ArgumentCaptor<DatastoreParameters> externalParamsCaptor = ArgumentCaptor.forClass(DatastoreParameters.class);
+        verify(tracedMethod).reportAsExternal(externalParamsCaptor.capture());
+        DatastoreParameters params = externalParamsCaptor.getValue();
+        assertEquals("DynamoDB", params.getProduct());
+        assertEquals("test", params.getCollection());
+        assertEquals("getItem", params.getOperation());
+        assertEquals("amazon", params.getHost());
+        assertNull(params.getPort());
+        assertNull(params.getPathOrId());
+        assertNull(params.getCloudResourceId());
+        assertNull(params.getDatabaseName());
+    }
+}
