dfd831ad5a24cb096eec458648a42fe46d2fdbda
Andre Onuki <aonuki@newrelic.com>
2024-12-16 09:58:44 -0500

    vertx: decrease amount of tokens created

--- instrumentation/vertx-core-4.5.0/src/main/java/io/vertx/core/impl/future/FutureImpl_Instrumentation.java
+++ instrumentation/vertx-core-4.5.0/src/main/java/io/vertx/core/impl/future/FutureImpl_Instrumentation.java
@@ -19,14 +19,29 @@ abstract class FutureImpl_Instrumentation {
 
     public abstract boolean isComplete();
 
+    // The 1st time this method is called, this.listener will be null.
+    // The provided listener will be assigned to this.listener and a token should be created for it.
+    // The 2nd time, the original code will create a ListenerArray and put it in this.listener.
+    // Then it will add both provided listeners to it.
+    // At this point, the first token should be expired and a token should be created for the ListenerArray.
+    // From there on, the ListenerArray will be used to store the listeners. And no token should be created.
     @Trace(async = true, excludeFromTransactionTrace = true)
     public void addListener(Listener listener) {
+        Listener previousHandler = this.listener;
         if (isComplete()) {
             VertxCoreUtil.linkAndExpireToken(listener);
-        } else {
-            VertxCoreUtil.storeToken(listener);
         }
+
         Weaver.callOriginal();
+
+        if (!isComplete()) {
+            if (previousHandler != this.listener) {
+                VertxCoreUtil.storeToken(this.listener);
+                if (previousHandler != null) {
+                    VertxCoreUtil.expireToken(previousHandler);
+                }
+            }
+        }
     }
 
     @Trace(async = true, excludeFromTransactionTrace = true)
