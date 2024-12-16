8baea0d86b8987f7699e2dd72d78cd9793e9d7ea
Andre Onuki <aonuki@newrelic.com>
2024-12-16 09:58:44 -0500

    vertx: decrease amount of tokens created

--- instrumentation/vertx-core-4.5.1/src/main/java/io/vertx/core/impl/future/FutureImpl_Instrumentation.java
+++ instrumentation/vertx-core-4.5.1/src/main/java/io/vertx/core/impl/future/FutureImpl_Instrumentation.java
@@ -8,9 +8,12 @@
 package io.vertx.core.impl.future;
 
 import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 import com.nr.vertx.instrumentation.VertxCoreUtil;
+
+import java.util.ArrayList;
 
 @Weave(originalName = "io.vertx.core.impl.future.FutureImpl")
 public abstract class FutureImpl_Instrumentation {
@@ -21,12 +24,26 @@ public abstract class FutureImpl_Instrumentation {
 
     @Trace(async = true, excludeFromTransactionTrace = true)
     public void addListener(Listener listener) {
+        boolean newListenerArray = false;
         if (isComplete()) {
             VertxCoreUtil.linkAndExpireToken(listener);
         } else {
-            VertxCoreUtil.storeToken(listener);
+            if (this.listener == null) {
+                // storing a token for the first listener, which will be set in callOriginal
+                VertxCoreUtil.storeToken(listener);
+            } else if (!(this.listener instanceof ListenerArray)) {
+                // when a 2nd listener is added, it will convert this.listener to a ListenerArray
+                // so we expire the previous token, and after the ListenerArray is created, we store a token for that
+                VertxCoreUtil.expireToken(this.listener);
+                newListenerArray = true;
+            }
         }
+
         Weaver.callOriginal();
+
+        if (newListenerArray) {
+            VertxCoreUtil.storeToken(this.listener);
+        }
     }
 
     @Trace(async = true, excludeFromTransactionTrace = true)
@@ -39,5 +56,9 @@ public abstract class FutureImpl_Instrumentation {
     public boolean tryFail(Throwable cause) {
         VertxCoreUtil.linkAndExpireToken(this.listener);
         return Weaver.callOriginal();
+    }
+
+    @Weave(type = MatchType.ExactClass, originalName = "io.vertx.core.impl.future.FutureImpl$ListenerArray")
+    private abstract static class ListenerArray<T> extends ArrayList<Listener<T>> implements Listener<T> {
     }
 }
