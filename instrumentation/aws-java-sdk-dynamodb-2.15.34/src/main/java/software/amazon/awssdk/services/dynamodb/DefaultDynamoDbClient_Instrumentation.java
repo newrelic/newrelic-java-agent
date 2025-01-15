13b373749b80ef6d166220e00464d72fed5f7a76
Andre Onuki <aonuki@newrelic.com>
2025-01-15 16:00:03 -0500

    aws-*: do not hide http request

--- instrumentation/aws-java-sdk-dynamodb-2.15.34/src/main/java/software/amazon/awssdk/services/dynamodb/DefaultDynamoDbClient_Instrumentation.java
+++ instrumentation/aws-java-sdk-dynamodb-2.15.34/src/main/java/software/amazon/awssdk/services/dynamodb/DefaultDynamoDbClient_Instrumentation.java
@@ -19,115 +19,115 @@ import software.amazon.awssdk.services.dynamodb.model.*;
 final class DefaultDynamoDbClient_Instrumentation {
     private final SdkClientConfiguration clientConfiguration = Weaver.callOriginal();
 
-    @Trace(leaf = true)
+    @Trace
     public GetItemResponse getItem(GetItemRequest getItemRequest) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "getItem", getItemRequest.tableName(), this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public ListTagsOfResourceResponse listTagsOfResource(ListTagsOfResourceRequest request) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "listTagsOfResource", null, this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public UntagResourceResponse untagResource(UntagResourceRequest request) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "untagResource", null, this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public BatchGetItemResponse batchGetItem(BatchGetItemRequest request) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "batchGetItem", "batch", this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public BatchWriteItemResponse batchWriteItem(BatchWriteItemRequest request) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "batchWriteItem", "batch", this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public PutItemResponse putItem(PutItemRequest putItemRequest) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "putItem", putItemRequest.tableName(), this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public DeleteItemResponse deleteItem(DeleteItemRequest request) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "deleteItem", request.tableName(), this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public ListTablesResponse listTables(ListTablesRequest request) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "listTables", request.exclusiveStartTableName(), this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public CreateTableResponse createTable(CreateTableRequest request) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "createTable", request.tableName(), this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public DeleteTableResponse deleteTable(DeleteTableRequest request) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "deleteTable", request.tableName(), this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public DescribeTableResponse describeTable(DescribeTableRequest request) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "describeTable", request.tableName(), this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public ScanResponse scan(ScanRequest request) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "scan", request.tableName(), this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public QueryResponse query(QueryRequest queryRequest) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "query", queryRequest.tableName(), this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public UpdateItemResponse updateItem(UpdateItemRequest updateItemRequest) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "updateItem", updateItemRequest.tableName(), this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public UpdateTableResponse updateTable(UpdateTableRequest updateTableRequest) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "updateTable", updateTableRequest.tableName(), this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public UpdateTimeToLiveResponse updateTimeToLive(UpdateTimeToLiveRequest request) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "updateTimeToLive", request.tableName(), this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public DescribeLimitsResponse describeLimits(DescribeLimitsRequest describeLimitsRequest) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "describeLimits", null, this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public TagResourceResponse tagResource(TagResourceRequest tagResourceRequest) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "tagResource", tagResourceRequest.resourceArn(), this, clientConfiguration);
         return Weaver.callOriginal();
     }
 
-    @Trace(leaf = true)
+    @Trace
     public DescribeTimeToLiveResponse describeTimeToLive(DescribeTimeToLiveRequest describeTimeToLiveRequest) {
         DynamoDBMetricUtil.metrics(NewRelic.getAgent().getTracedMethod(), "describeTimeToLive", describeTimeToLiveRequest.tableName(), this, clientConfiguration);
         return Weaver.callOriginal();
