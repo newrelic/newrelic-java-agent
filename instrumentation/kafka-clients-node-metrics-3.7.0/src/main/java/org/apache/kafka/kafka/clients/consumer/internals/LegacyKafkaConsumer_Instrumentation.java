e5bc12cf6163b3e3bd9d9fd2aad821d37627a157
André Onuki <aonuki@newrelic.com>
2025-01-22 14:23:23 -0500

    Kafka clients node metrics (#2202)

--- instrumentation/kafka-clients-node-metrics-3.7.0/src/main/java/org/apache/kafka/kafka/clients/consumer/internals/LegacyKafkaConsumer_Instrumentation.java
+++ instrumentation/kafka-clients-node-metrics-3.7.0/src/main/java/org/apache/kafka/kafka/clients/consumer/internals/LegacyKafkaConsumer_Instrumentation.java
@@ -1,50 +1,51 @@
 /*
  *
- *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * Copyright 2025 New Relic Corporation. All rights reserved.
  *  * SPDX-License-Identifier: Apache-2.0
  *
  */
 
-package org.apache.kafka.kafka.clients.consumer;
+package org.apache.kafka.kafka.clients.consumer.internals;
 
 import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.weaver.NewField;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.WeaveAllConstructors;
+import com.newrelic.api.agent.weaver.Weaver;
+import com.nr.instrumentation.kafka.ClientType;
 import com.nr.instrumentation.kafka.NewRelicMetricsReporter;
-
-import java.time.Duration;
-import java.util.Map;
-import java.util.logging.Level;
-
+import org.apache.kafka.clients.consumer.internals.ConsumerMetadata;
 import org.apache.kafka.common.Metric;
 import org.apache.kafka.common.MetricName;
+import org.apache.kafka.common.Node;
 import org.apache.kafka.common.Uuid;
 import org.apache.kafka.common.metrics.Metrics;
 
-@Weave(originalName = "org.apache.kafka.clients.consumer.KafkaConsumer")
-public abstract class KafkaConsumer_Instrumentation<K, V> {
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
 
+@Weave(originalName = "org.apache.kafka.clients.consumer.internals.LegacyKafkaConsumer")
+public abstract class LegacyKafkaConsumer_Instrumentation<K, V> {
+
+    private final Metrics metrics = Weaver.callOriginal();
+    private final String clientId = Weaver.callOriginal();
+    private final ConsumerMetadata metadata = Weaver.callOriginal();
     // It's possible for constructors to be invoked multiple times (e.g. `C() { C("some default") }` ).
     // When this happens we don't want to register the metrics reporter multiple times.
     @NewField
     private boolean metricsReporterInstalled;
 
     @WeaveAllConstructors
-    public KafkaConsumer_Instrumentation() {
-
-        String clientId = clientInstanceId(Duration.ofSeconds(1)).toString();
-        Metrics metrics = (Metrics) metrics();
-
+    public LegacyKafkaConsumer_Instrumentation() {
         if (!metricsReporterInstalled) {
             NewRelic.getAgent().getLogger().log(Level.INFO,
                     "newrelic-kafka-clients-enhancements engaged for consumer {0}", clientId);
-            metrics.addReporter(new NewRelicMetricsReporter());
+            metrics.addReporter(new NewRelicMetricsReporter(ClientType.CONSUMER, metadata.fetch().nodes()));
             metricsReporterInstalled = true;
         }
     }
-
-    public abstract Uuid clientInstanceId(Duration timeout);
-
-    public abstract Map<MetricName, ? extends Metric> metrics();
 }
--- instrumentation/kafka-clients-node-metrics-3.7.0/src/main/java/org/apache/kafka/kafka/clients/producer/KafkaProducer_Instrumentation.java
+++ instrumentation/kafka-clients-node-metrics-3.7.0/src/main/java/org/apache/kafka/kafka/clients/producer/KafkaProducer_Instrumentation.java
@@ -1,3 +1,10 @@
+/*
+ *
+ *  * Copyright 2025 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
 package org.apache.kafka.kafka.clients.producer;
 
 import com.newrelic.api.agent.NewRelic;
@@ -5,8 +12,11 @@ import com.newrelic.api.agent.weaver.NewField;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.WeaveAllConstructors;
 import com.newrelic.api.agent.weaver.Weaver;
+import com.nr.instrumentation.kafka.ClientType;
 import com.nr.instrumentation.kafka.NewRelicMetricsReporter;
 import java.util.logging.Level;
+
+import org.apache.kafka.clients.producer.internals.ProducerMetadata;
 import org.apache.kafka.common.metrics.Metrics;
 
 @Weave(originalName = "org.apache.kafka.clients.producer.KafkaProducer")
@@ -14,6 +24,7 @@ public class KafkaProducer_Instrumentation<K, V> {
 
     private final Metrics metrics = Weaver.callOriginal();
     private final String clientId = Weaver.callOriginal();
+    private final ProducerMetadata metadata = Weaver.callOriginal();
 
     // It's possible for constructors to be invoked multiple times (e.g. `C() { C("some default") }` ).
     // When this happens we don't want to register the metrics reporter multiple times.
@@ -25,7 +36,7 @@ public class KafkaProducer_Instrumentation<K, V> {
         if (!metricsReporterInstalled) {
             NewRelic.getAgent().getLogger().log(Level.INFO,
                 "newrelic-kafka-clients-enhancements engaged for producer {0}", clientId);
-            metrics.addReporter(new NewRelicMetricsReporter());
+            metrics.addReporter(new NewRelicMetricsReporter(ClientType.PRODUCER, metadata.fetch().nodes()));
             metricsReporterInstalled = true;
         }
     }
