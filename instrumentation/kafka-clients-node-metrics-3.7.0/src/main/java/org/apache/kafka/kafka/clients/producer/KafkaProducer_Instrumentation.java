e610b68283d934264c27b557635cf25ecf0c8512
Andre Onuki <aonuki@newrelic.com>
2025-01-22 13:33:34 -0500

    kafks: adding copyright

--- instrumentation/kafka-clients-node-metrics-3.7.0/src/main/java/org/apache/kafka/kafka/clients/producer/KafkaProducer_Instrumentation.java
+++ instrumentation/kafka-clients-node-metrics-3.7.0/src/main/java/org/apache/kafka/kafka/clients/producer/KafkaProducer_Instrumentation.java
@@ -1,3 +1,10 @@
+/*
+ *
+ *  * Copyright 2025 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
 package org.apache.kafka.kafka.clients.producer;
 
 import com.newrelic.api.agent.NewRelic;
