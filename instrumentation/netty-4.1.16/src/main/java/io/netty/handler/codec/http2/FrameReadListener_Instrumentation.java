40b318d63d5e200af8b828b7f8a1f3688f550f0f
Jerry Duffy <jtduffy@users.noreply.github.com>
2025-02-07 15:04:04 -0500

    WIP commit

--- instrumentation/netty-4.1.16/src/main/java/io/netty/handler/codec/http2/FrameReadListener_Instrumentation.java
+++ instrumentation/netty-4.1.16/src/main/java/io/netty/handler/codec/http2/FrameReadListener_Instrumentation.java
@@ -19,16 +19,23 @@ class FrameReadListener_Instrumentation {
     // Process HTTP/2 request headers and start txn
     public void onHeadersRead(ChannelHandlerContext_Instrumentation ctx, int streamId, Http2Headers headers, int streamDependency, short weight,
             boolean exclusive, int padding, boolean endOfStream) {
-        if (ctx.pipeline().token == null) {
-            // NettyDispatcher class is usually initialized in AbstractBootstrap; however,
-            // that code is not always invoked when using recent Netty versions (4.1.54)
-            // so we check here and initialize if we haven't yet.
-            if (!NettyDispatcher.instrumented.get()) {
-                NettyDispatcher.get();
+        if (isValidIncomingRequest(headers)) {
+            if (ctx.pipeline().token == null) {
+                // NettyDispatcher class is usually initialized in AbstractBootstrap; however,
+                // that code is not always invoked when using recent Netty versions (4.1.54)
+                // so we check here and initialize if we haven't yet.
+                if (!NettyDispatcher.instrumented.get()) {
+                    NettyDispatcher.get();
+                }
+                NettyDispatcher.channelRead(ctx, headers);
             }
-            NettyDispatcher.channelRead(ctx, headers);
         }
+
         // Order matters here!!! Weaver.callOriginal() must come after the call to NettyDispatcher.channelRead.
         Weaver.callOriginal();
+    }
+
+    private boolean isValidIncomingRequest(Http2Headers headers) {
+        return (headers.method() != null && headers.authority() != null);
     }
 }
