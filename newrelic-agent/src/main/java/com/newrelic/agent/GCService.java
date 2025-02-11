552ab820f318702aea3f68f0dcca354993134913
Kate Anderson <kateanderson@newrelic.com>
2025-02-11 13:23:29 -0800

    Modify GCService for edge case debug

--- newrelic-agent/src/main/java/com/newrelic/agent/GCService.java
+++ newrelic-agent/src/main/java/com/newrelic/agent/GCService.java
@@ -11,6 +11,7 @@ import com.newrelic.agent.service.AbstractService;
 import com.newrelic.agent.service.ServiceFactory;
 import com.newrelic.agent.stats.ResponseTimeStats;
 import com.newrelic.agent.stats.StatsEngine;
+import com.newrelic.api.agent.NewRelic;
 
 import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
@@ -60,13 +61,16 @@ public class GCService extends AbstractService implements HarvestListener {
 
     private void harvestGC(StatsEngine statsEngine) {
         for (GarbageCollectorMXBean gcBean : ManagementFactory.getGarbageCollectorMXBeans()) {
+            NewRelic.getAgent().getLogger().log(Level.INFO, "GC Debug: Found GC Bean {0} with collection count {1}", gcBean.getName(), gcBean.getCollectionCount());
             GarbageCollector garbageCollector = garbageCollectors.get(gcBean.getName());
             if (garbageCollector == null) {
+                NewRelic.getAgent().getLogger().log(Level.INFO, "GC Debug: registering new garbage collector {0}", gcBean.getName());
                 garbageCollector = new GarbageCollector(gcBean);
                 garbageCollectors.put(gcBean.getName(), garbageCollector);
-            } else {
-                garbageCollector.recordGC(gcBean, statsEngine);
             }
+            // tmp modification: move out of else block
+            // to test possible edge case
+            garbageCollector.recordGC(gcBean, statsEngine);
         }
     }
 
