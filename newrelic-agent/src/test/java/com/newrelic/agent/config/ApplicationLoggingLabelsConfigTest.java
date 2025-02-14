7f24308b8d6ede45fa7b76659aa4e9b1fe5fbeab
edeleon <edeleon@newrelic.com>
2025-02-13 22:36:31 -0800

    added tests for exclude prop

--- newrelic-agent/src/test/java/com/newrelic/agent/config/ApplicationLoggingLabelsConfigTest.java
+++ newrelic-agent/src/test/java/com/newrelic/agent/config/ApplicationLoggingLabelsConfigTest.java
@@ -2,7 +2,9 @@ package com.newrelic.agent.config;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertFalse;
@@ -27,6 +29,21 @@ public class ApplicationLoggingLabelsConfigTest {
         Map<String, Object> props = new HashMap<>();
         ApplicationLoggingLabelsConfig config = new ApplicationLoggingLabelsConfig(props, PARENT_ROOT + LABELS);
         assertFalse(config.getEnabled());
+    }
+
+    @Test
+    public void testLabelsExcludeConfig() {
+        Map<String, Object> props = new HashMap<>();
+        List<String> exclude = new ArrayList<>();
+        exclude.add("test1");
+        exclude.add("test2");
+        props.put("exclude", exclude);
+
+        ApplicationLoggingLabelsConfig config = new ApplicationLoggingLabelsConfig(props, PARENT_ROOT + LABELS);
+
+        assertTrue(config.isExcluded("test1"));
+        assertTrue(config.isExcluded("test2"));
+        assertFalse(config.isExcluded("test3"));
     }
 
 }
