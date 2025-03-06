a14ff1be22f644c1f05a89ab9bd763b01a15c6a9
Jerry Duffy <jtduffy@users.noreply.github.com>
2025-03-06 14:43:40 -0500

    Update browser footer javadoc

--- newrelic-api/src/main/java/com/newrelic/api/agent/NewRelic.java
+++ newrelic-api/src/main/java/com/newrelic/api/agent/NewRelic.java
@@ -352,8 +352,10 @@ public final class NewRelic {
     /**
      * Get the RUM JavaScript footer for the current web transaction.
      *
+     * @deprecated The full browser script is now included when calling {@link PublicApi#getBrowserTimingHeader}
+     * or {@link PublicApi#getBrowserTimingHeader(String)}
+     *
      * @return RUM JavaScript footer for the current web transaction.
-     * @since 2.21.0
      */
     public static String getBrowserTimingFooter() {
         return "";
@@ -361,9 +363,12 @@ public final class NewRelic {
 
     /**
      * Get the RUM JavaScript footer for the current web transaction.
+     *
      * @param nonce a random per-request nonce for sites using Content Security Policy (CSP)
+     * @deprecated The full browser script is not included when calling {@link PublicApi#getBrowserTimingHeader}
+     * or {@link PublicApi#getBrowserTimingHeader(String)}
+     *
      * @return RUM JavaScript footer for the current web transaction.
-     * @since 7.6.0
      */
     public static String getBrowserTimingFooter(String nonce) {
         return "";
