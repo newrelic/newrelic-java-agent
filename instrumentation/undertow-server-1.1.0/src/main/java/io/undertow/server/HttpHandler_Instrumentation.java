2121c107cc63cc8befa1528ec67f23591c95277d
Jerry Duffy <jtduffy@users.noreply.github.com>
2025-03-18 09:19:33 -0400

    Correct Weave annotation

--- instrumentation/undertow-server-1.1.0/src/main/java/io/undertow/server/HttpHandler_Instrumentation.java
+++ instrumentation/undertow-server-1.1.0/src/main/java/io/undertow/server/HttpHandler_Instrumentation.java
@@ -8,9 +8,11 @@ package io.undertow.server;
 
 import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 
-//@Weave(type = MatchType.Interface, originalName = "io.undertow.server.HttpHandler")
+@Weave(type = MatchType.Interface, originalName = "io.undertow.server.HttpHandler")
 public abstract class HttpHandler_Instrumentation {
     @Trace(dispatcher=true)
     public void handleRequest(HttpServerExchange exchange) {
