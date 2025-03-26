be8065e13764b34c19a2235d576043a16c147ae1
Oren Ben-Meir <orenbm11@gmail.com>
2025-03-26 17:20:46 -0400

    add kafka consumer distributed traces

---
+++ instrumentation/kafka-clients-spans-consumer-0.11.0.0/src/main/java/org/apache/kafka/clients/consumer/KafkaConsumer_Instrumentation.java
@@ -0,0 +1,41 @@
+package org.apache.kafka.clients.consumer;
+
+import com.newrelic.agent.bridge.AgentBridge;
+import com.newrelic.api.agent.Headers;
+import com.newrelic.api.agent.NewRelic;
+import com.newrelic.api.agent.TransportType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import com.nr.instrumentation.kafka.HeadersWrapper;
+
+import java.time.Duration;
+import java.util.logging.Level;
+
+@Weave(originalName = "org.apache.kafka.clients.consumer.KafkaConsumer")
+public class KafkaConsumer_Instrumentation<K, V> {
+
+    public ConsumerRecords<K, V> poll(final Duration timeout) {
+        final ConsumerRecords<K, V> records = Weaver.callOriginal();
+        nrAcceptDtHeaders(records);
+        return records;
+    }
+
+    public ConsumerRecords<K, V> poll(final long timeoutMs) {
+        final ConsumerRecords<K, V> records = Weaver.callOriginal();
+        nrAcceptDtHeaders(records);
+        return records;
+    }
+
+    private void nrAcceptDtHeaders(ConsumerRecords<K, V> records) {
+        AgentBridge.getAgent().getLogger().log(Level.INFO, "nrAcceptDtHeaders");
+        if (AgentBridge.getAgent().getTransaction(false) != null) {
+            AgentBridge.getAgent().getLogger().log(Level.INFO, "nrAcceptDtHeaders getTransaction exists");
+            for (ConsumerRecord<?, ?> record : records) {
+                AgentBridge.getAgent().getLogger().log(Level.INFO, "nrAcceptDtHeaders acceptDistributedTraceHeaders");
+                Headers dtHeaders = new HeadersWrapper(record.headers());
+                NewRelic.getAgent().getTransaction().acceptDistributedTraceHeaders(TransportType.Kafka, dtHeaders);
+                break;
+            }
+        }
+    }
+}
