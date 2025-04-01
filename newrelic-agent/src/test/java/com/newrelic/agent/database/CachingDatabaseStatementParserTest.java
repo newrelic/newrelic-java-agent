c96682ac2104aca361ec541f4632b0cb7cfa2e90
Jerry Duffy <jtduffy@users.noreply.github.com>
2025-04-01 13:10:26 -0400

    Prevent cache lock for long DB statement parsing

--- newrelic-agent/src/test/java/com/newrelic/agent/database/CachingDatabaseStatementParserTest.java
+++ newrelic-agent/src/test/java/com/newrelic/agent/database/CachingDatabaseStatementParserTest.java
@@ -6,11 +6,7 @@ import org.junit.Test;
 import java.sql.ResultSetMetaData;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -24,7 +20,14 @@ public class CachingDatabaseStatementParserTest {
         DatabaseStatementParser mockParser = mock(DatabaseStatementParser.class);
         CachingDatabaseStatementParser cache = new CachingDatabaseStatementParser(mockParser);
 
+        when(mockParser.getParsedDatabaseStatement(any(), eq("sql"), any())).thenReturn(new ParsedDatabaseStatement("tablename", "select", true));
+
+        // Initial call will cache the result
         ParsedDatabaseStatement statement = cache.getParsedDatabaseStatement(mock(DatabaseVendor.class), "sql", mock(ResultSetMetaData.class));
+        verify(mockParser, times(1)).getParsedDatabaseStatement(any(), eq("sql"), any());
+
+        // Subsequent calls will retrieve the value from the cache and not call the databaseStatementParser again
+        assertEquals(statement, cache.getParsedDatabaseStatement(mock(DatabaseVendor.class), "sql", mock(ResultSetMetaData.class)));
         verify(mockParser, times(1)).getParsedDatabaseStatement(any(), eq("sql"), any());
     }
 
