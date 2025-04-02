01b3c94acffc08d98978c5d11259081a39222238
gsidhwani_nr <gsidhwani@newrelic.com>
2025-04-02 13:07:55 +0530

    fix: micronaut-http-3.0.0 micronaut-http-netty-2.0.0 micronaut-http-netty-4.1.0micronaut-http-server-netty-3.0.0 micronaut-http-server-netty-4.6.0 micronaut-http-1.0.0 - Fixed verification failures

---
+++ instrumentation/micronaut-http-server-netty-3.0.0/src/main/java/io/micronaut/http/server/netty/binders/MaybeBodyBinder_Instrumentation.java
@@ -0,0 +1,7 @@
+package io.micronaut.http.server.netty.binders;
+
+import com.newrelic.api.agent.weaver.SkipIfPresent;
+
+@SkipIfPresent(originalName = "io.micronaut.http.server.netty.binders.MaybeBodyBinder")
+public class MaybeBodyBinder_Instrumentation {
+}
