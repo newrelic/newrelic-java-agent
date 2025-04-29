7d0fd558df73fafabac96c39ffe0ec893f2e7171
Oren Ben-Meir <orenbm11@gmail.com>
2025-04-29 17:01:01 -0400

    fix up clean.sh

--- jmeter/clean.sh
+++ jmeter/clean.sh
@@ -1,23 +1,12 @@
-# rm -rf deps
-
-TESTS="tests"
-
-if [ ! -d "$TESTS" ]; then
-  echo "Error: Directory '$TESTS' not found."
-  exit 1
-fi
-
-for file in "$TESTS"/*/logs/*; do
+for file in logs/*; do
   if [ -f "$file" ]; then
-    # Your code to process the file goes here
     echo "Removing file: $file"
     rm $file
   fi
 done
 
-for file in "$TESTS"/*/results/*; do
+for file in results/*; do
   if [ -f "$file" ]; then
-    # Your code to process the file goes here
     echo "Removing file: $file"
     rm $file
   fi
