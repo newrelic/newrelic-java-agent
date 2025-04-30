8e0427a7a9ef088133ac60fdcea7287dba0ea9ca
Oren Ben-Meir <orenbm11@gmail.com>
2025-04-30 14:55:50 -0400

    add graph plugin to jmater

--- jmeter/install-jmeter.sh
+++ jmeter/install-jmeter.sh
@@ -1,5 +1,6 @@
 JMETER_VERSION=5.6.3
 JMXMON_VERSION=0.3
+JPGCGGL_VERSION=2.0
  
 mkdir deps
 cd deps
@@ -12,14 +13,21 @@ rm jmeter.tgz
 mv apache-jmeter-${JMETER_VERSION} jmeter
 echo "JMeter installed!"
 
-# Install JMX Plugin
+# Install JMX Plugins
 
 echo "Intalling JMX sampler plugin for JMeter"
 curl --output jpgc-jmxmon-${JMXMON_VERSION}.zip https://jmeter-plugins.org/files/packages/jpgc-jmxmon-${JMXMON_VERSION}.zip
 unzip jpgc-jmxmon-${JMXMON_VERSION}.zip -d jpgc-jmxmon
 mv jpgc-jmxmon/lib/*.jar jmeter/lib
 mv jpgc-jmxmon/lib/ext/*.jar jmeter/lib/ext
-
 rm jpgc-jmxmon-${JMXMON_VERSION}.zip
 rm -rf jpgc-jmxmon
 echo "Intalled JMX sampler plugin for JMeter!"
+
+echo "Intalling Graphs Generator Listener plugin for JMeter"
+curl --output jpgc-ggl-${JPGCGGL_VERSION}.zip https://jmeter-plugins.org/files/packages/jpgc-ggl-${JPGCGGL_VERSION}.zip
+unzip jpgc-ggl-${JPGCGGL_VERSION}.zip -d jpgc-ggl
+mv jpgc-ggl/lib/ext/jmeter-plugins-graphs-ggl-${JPGCGGL_VERSION}.jar jmeter/lib/ext
+rm jpgc-ggl-${JPGCGGL_VERSION}.zip
+rm -rf jpgc-ggl
+echo "Intalled Graphs Generator Listener plugin for JMeter!"
