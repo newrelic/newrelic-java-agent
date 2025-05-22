c72898152aa18370abe30c6f252d39f4040a6d6f
Oren Ben-Meir <orenbm11@gmail.com>
2025-05-22 17:07:17 -0400

    rename environment variable for VM arguments

--- jmeter/tests/spring-petclinic/entrypoint.sh
+++ jmeter/tests/spring-petclinic/entrypoint.sh
@@ -3,7 +3,7 @@ echo "running Spring Pet Clinic with agent build ${AGENT_BUILD}"
 nohup java -javaagent:./deps/agents/"${AGENT_BUILD}"/newrelic.jar \
            -Dmewrelic.config.file=./newrelic.yml \
            -Dnewrelic.logfile=logs/newrelic-"${DATETIME}".log \
-           "${CUSTOM_VM_ARGS}" "${COMMON_VM_ARGS}" \
+           "${CUSTOM_JVM_ARGS}" "${COMMON_VM_ARGS}" \
            -jar build/libs/spring-petclinic-3.4.0.jar >logs/petclinic-"${DATETIME}".log &
 sleep 20
 sh deps/jmeter/bin/jmeter.sh -n -t test.jmx -l results/results.csv -j logs/jmeter-"${DATETIME}".log
