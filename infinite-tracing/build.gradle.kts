c28c8351de4166e8b537c5f22aa2164814099ad6
Jerry Duffy <jtduffy@users.noreply.github.com>
2025-06-26 12:53:01 -0400

    Bump infinite-tracing lib to 3.5

--- infinite-tracing/build.gradle.kts
+++ infinite-tracing/build.gradle.kts
@@ -14,7 +14,7 @@ java {
 }
 
 dependencies {
-    implementation("com.newrelic.agent.java:infinite-tracing-protobuf:3.4")
+    implementation("com.newrelic.agent.java:infinite-tracing-protobuf:3.5")
     implementation("com.google.guava:guava:30.1.1-jre")
     implementation(project(":agent-model"))
     implementation(project(":agent-interfaces"))
