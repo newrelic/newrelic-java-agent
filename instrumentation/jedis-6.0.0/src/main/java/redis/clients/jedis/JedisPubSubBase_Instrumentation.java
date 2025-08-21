0dbfcd834d84a029fef234f83fabbd9a786b9239
Kate Anderson <kate.anderson250@gmail.com>
2025-08-21 15:19:11 -0700

    Update copyright headers

--- instrumentation/jedis-6.0.0/src/main/java/redis/clients/jedis/JedisPubSubBase_Instrumentation.java
+++ instrumentation/jedis-6.0.0/src/main/java/redis/clients/jedis/JedisPubSubBase_Instrumentation.java
@@ -1,6 +1,6 @@
 /*
  *
- *  * Copyright 2024 New Relic Corporation. All rights reserved.
+ *  * Copyright 2025 New Relic Corporation. All rights reserved.
  *  * SPDX-License-Identifier: Apache-2.0
  *
  */
