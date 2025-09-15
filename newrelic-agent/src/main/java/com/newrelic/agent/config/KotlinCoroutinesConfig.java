b08591db40abb2410d66baec81645294517db3bf
Doug Hilpipre <dhilpipre@newrelic.com>
2025-09-15 13:34:52 -0500

    made changes to handle processing configuration correctly

--- newrelic-agent/src/main/java/com/newrelic/agent/config/KotlinCoroutinesConfig.java
+++ newrelic-agent/src/main/java/com/newrelic/agent/config/KotlinCoroutinesConfig.java
@@ -4,9 +4,15 @@ public interface KotlinCoroutinesConfig {
 
     public String[] getIgnoredContinuations();
 
+    public String[] getIgnoredRegExContinuations();
+
     public String[] getIgnoredScopes();
 
+    public String[] getIgnoredRegexScopes();
+
     public String[] getIgnoredDispatched();
+
+    public String[] getIgnoredRegexDispatched();
 
     public boolean isDelayedEnabled();
 
--- newrelic-agent/src/main/java/com/newrelic/agent/config/KotlinCoroutinesConfigImpl.java
+++ newrelic-agent/src/main/java/com/newrelic/agent/config/KotlinCoroutinesConfigImpl.java
@@ -1,6 +1,8 @@
 package com.newrelic.agent.config;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 public class KotlinCoroutinesConfigImpl extends BaseConfig implements KotlinCoroutinesConfig {
@@ -10,12 +12,16 @@ public class KotlinCoroutinesConfigImpl extends BaseConfig implements KotlinCoro
     public static final String SCOPES_ROOT = "scopes";
     public static final String DISPATCHED_ROOT = "dispatched";
     public static final String IGNORE = "ignore";
+    public static final String IGNORE_REGEX = "ignoreRegex";
     public static final String DELAYED_ROOT = "delayed";
     public static final String ENABLED = "enabled";
     private static final boolean DELAY_DEFAULT = true;
     private String[] ignoredContinuations = null;
     private String[] ignoredScopes = null;
     private String[] ignoredDispatched = null;
+    private String[] ignoredRegexContinuations = null;
+    private String[] ignoredRegexScopes = null;
+    private String[] ignoredRegexDispatched = null;
     private boolean delayedEnabled = true;
 
     public KotlinCoroutinesConfigImpl(Map<String, Object> props) {
@@ -24,25 +30,34 @@ public class KotlinCoroutinesConfigImpl extends BaseConfig implements KotlinCoro
 
         if (continuations_root != null) {
             String continuationsToIgnore = continuations_root.get(IGNORE);
-            ignoredContinuations = continuationsToIgnore == null ? new String[0] : continuationsToIgnore.split(",");
+            ignoredContinuations = splitString(continuationsToIgnore); //continuationsToIgnore == null ? new String[0] : continuationsToIgnore.split(",");
+            String continuationsToIgnoreRegex = continuations_root.get(IGNORE_REGEX);
+            ignoredRegexContinuations = splitString(continuationsToIgnoreRegex); //continuationsToIgnoreRegex == null ? new String[0] : continuationsToIgnoreRegex.split(",");
         } else {
             ignoredContinuations = new String[0];
+            ignoredRegexContinuations = new String[0];
         }
 
         Map<String, String> scopes_root = getProperty(SCOPES_ROOT);
         if (scopes_root != null) {
             String scopesToIgnore = scopes_root.get(IGNORE);
-            ignoredScopes = scopesToIgnore == null ? new String[0] : scopesToIgnore.split(",");
+            ignoredScopes = splitString(scopesToIgnore); //scopesToIgnore == null ? new String[0] : scopesToIgnore.split(",");
+            String scopesToIgnoreRegex = scopes_root.get(IGNORE_REGEX);
+            ignoredRegexScopes = splitString(scopesToIgnoreRegex); //scopesToIgnoreRegex == null ? new String[0] : scopesToIgnoreRegex.split(",");
         } else {
             ignoredScopes = new String[0];
+            ignoredRegexScopes = new String[0];
         }
 
         Map<String, String> dispatched_root = getProperty(DISPATCHED_ROOT);
         if (dispatched_root != null) {
             String dispatchedToIgnore = dispatched_root.get(IGNORE);
-            ignoredDispatched = dispatchedToIgnore == null ? new String[0] : dispatchedToIgnore.split(",");
+            ignoredDispatched = splitString(dispatchedToIgnore);  //dispatchedToIgnore == null ? new String[0] : dispatchedToIgnore.split(",");
+            String dispatchedToIgnoreRegex = dispatched_root.get(IGNORE_REGEX);
+            ignoredRegexDispatched = splitString(dispatchedToIgnoreRegex); //dispatchedToIgnoreRegex == null ? new String[0] : dispatchedToIgnoreRegex.split(",");
         } else {
             ignoredDispatched = new String[0];
+            ignoredRegexDispatched = new String[0];
         }
 
         Map<String, Object> delayed_root = getProperty(DELAYED_ROOT);
@@ -73,8 +88,18 @@ public class KotlinCoroutinesConfigImpl extends BaseConfig implements KotlinCoro
     }
 
     @Override
+    public String[] getIgnoredRegExContinuations() {
+        return ignoredRegexContinuations;
+    }
+
+    @Override
     public String[] getIgnoredScopes() {
         return ignoredScopes;
+    }
+
+    @Override
+    public String[] getIgnoredRegexScopes() {
+        return ignoredRegexScopes;
     }
 
     @Override
@@ -83,8 +108,28 @@ public class KotlinCoroutinesConfigImpl extends BaseConfig implements KotlinCoro
     }
 
     @Override
+    public String[] getIgnoredRegexDispatched() {
+        return ignoredRegexDispatched;
+    }
+
+    @Override
     public boolean isDelayedEnabled() {
         return delayedEnabled;
     }
 
+    private String[] splitString(String input) {
+        if (input == null) {
+            return new String[0];
+        }
+        String[] firstSplit = input.split("\"");
+        List<String> result = new ArrayList<>();
+        for (String s : firstSplit) {
+            if(!s.trim().equals(",")) {
+                result.add(s.trim());
+            }
+        }
+        String[] returnValue = new String[result.size()];
+        result.toArray(returnValue);
+        return returnValue;
+    }
 }
