a7475264e81578d378845c0cdf7c11c219b2ca2c
John Bedell <jbedell@newrelic.com>
2025-09-17 08:19:19 -0400

    Log kafka headers

--- instrumentation/kafka-clients-spans-0.11.0.0/src/main/java/org/apache/kafka/clients/producer/KafkaProducer_Instrumentation.java
+++ instrumentation/kafka-clients-spans-0.11.0.0/src/main/java/org/apache/kafka/clients/producer/KafkaProducer_Instrumentation.java
@@ -11,12 +11,14 @@ import com.newrelic.agent.bridge.AgentBridge;
 import com.newrelic.agent.bridge.NoOpDistributedTracePayload;
 import com.newrelic.agent.bridge.Transaction;
 import com.newrelic.api.agent.DistributedTracePayload;
+import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.Future;
+import java.util.logging.Level;
 
 @Weave(originalName = "org.apache.kafka.clients.producer.KafkaProducer")
 public class KafkaProducer_Instrumentation<K, V> {
@@ -28,6 +30,7 @@ public class KafkaProducer_Instrumentation<K, V> {
             DistributedTracePayload payload = transaction.createDistributedTracePayload();
             if (!(payload instanceof NoOpDistributedTracePayload)) {
                 record.headers().add("newrelic", payload.text().getBytes(StandardCharsets.UTF_8));
+                NewRelic.getAgent().getLogger().log(Level.INFO, "JGB: Adding 'newrelic' header value '"+payload.text()+"' to message key: "+record.key());
             }
         }
         return Weaver.callOriginal();
