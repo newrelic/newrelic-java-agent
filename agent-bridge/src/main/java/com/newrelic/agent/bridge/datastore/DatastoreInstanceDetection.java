71ab2d880e0a16619ad64cff4eec0d67efeb6879
John Bedell <jbedell@newrelic.com>
2025-09-29 10:03:40 -0400

    Just return and use the first address we found

--- agent-bridge/src/main/java/com/newrelic/agent/bridge/datastore/DatastoreInstanceDetection.java
+++ agent-bridge/src/main/java/com/newrelic/agent/bridge/datastore/DatastoreInstanceDetection.java
@@ -97,10 +97,9 @@ public final class DatastoreInstanceDetection {
             AgentBridge.getAgent().getLogger().log(Level.FINEST,
                     "JGB: Two different addresses detected: {0} and {1}. Using originally detected address.",
                     previousAddress, addressToStore);
-            stopDetectingConnectionAddress();
-            AgentBridge.getAgent().getLogger().log(Level.FINEST,
-                    "JGB: Keeping previous address");
-            DatastoreInstanceDetection.address.set(previousAddress);
+            //stopDetectingConnectionAddress();
+            //AgentBridge.getAgent().getLogger().log(Level.FINEST, "JGB: Keeping previous address");
+            //DatastoreInstanceDetection.address.set(previousAddress);
             return;
         }
 
