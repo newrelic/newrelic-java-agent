381acacedc694f6432e92cd71f3e08a11f4d333e
John Bedell <jbedell@newrelic.com>
2025-10-02 13:28:41 -0400

    Introduce a preference config for what to do when we detect multiple addresses during datastore detection.  Default is NONE (or legacy), with new options of FIRST or LAST.

--- agent-bridge/src/test/java/com/newrelic/agent/bridge/datastore/DatastoreInstanceDetectionTest.java
+++ agent-bridge/src/test/java/com/newrelic/agent/bridge/datastore/DatastoreInstanceDetectionTest.java
@@ -7,7 +7,10 @@
 
 package com.newrelic.agent.bridge.datastore;
 
+import com.newrelic.api.agent.Agent;
+import com.newrelic.api.agent.NewRelic;
 import org.junit.Test;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
 import java.net.InetSocketAddress;
@@ -47,7 +50,49 @@ public class DatastoreInstanceDetectionTest {
     }
 
     @Test
-    public void testDifferentAddresses() {
+    public void testDifferentAddresses_UseFirst() {
+        Agent mockAgent = Mockito.mock(Agent.class, Mockito.RETURNS_DEEP_STUBS);
+        Mockito.when(mockAgent.getConfig().getValue("datastore_multihost_preference",
+                DatastoreInstanceDetection.MultiHostConfig.NONE)).thenReturn(DatastoreInstanceDetection.MultiHostConfig.FIRST);
+        try (MockedStatic<NewRelic> mockNewRelic = Mockito.mockStatic(NewRelic.class)) {
+            mockNewRelic.when(NewRelic::getAgent).thenReturn(mockAgent);
+            DatastoreInstanceDetection.detectConnectionAddress();
+            DatastoreInstanceDetection.saveAddress(InetSocketAddress.createUnresolved("myhost", 1234));
+            DatastoreInstanceDetection.saveAddress(InetSocketAddress.createUnresolved("someOtherHost", 9128));
+            DatastoreInstanceDetection.saveAddress(InetSocketAddress.createUnresolved("myhost", 1234));
+            Connection connection = Mockito.mock(Connection.class);
+            DatastoreInstanceDetection.associateAddress(connection);
+            DatastoreInstanceDetection.stopDetectingConnectionAddress();
+
+            InetSocketAddress address = DatastoreInstanceDetection.getAddressForConnection(connection);
+            assertEquals("myhost", address.getHostName());
+            assertEquals(1234, address.getPort());
+        }
+    }
+
+    @Test
+    public void testDifferentAddresses_UseLast() {
+        Agent mockAgent = Mockito.mock(Agent.class, Mockito.RETURNS_DEEP_STUBS);
+        Mockito.when(mockAgent.getConfig().getValue("datastore_multihost_preference",
+                DatastoreInstanceDetection.MultiHostConfig.NONE)).thenReturn(DatastoreInstanceDetection.MultiHostConfig.LAST);
+        try (MockedStatic<NewRelic> mockNewRelic = Mockito.mockStatic(NewRelic.class)) {
+            mockNewRelic.when(NewRelic::getAgent).thenReturn(mockAgent);
+            DatastoreInstanceDetection.detectConnectionAddress();
+            DatastoreInstanceDetection.saveAddress(InetSocketAddress.createUnresolved("myhost", 1234));
+            DatastoreInstanceDetection.saveAddress(InetSocketAddress.createUnresolved("someOtherHost", 9128));
+            Connection connection = Mockito.mock(Connection.class);
+            DatastoreInstanceDetection.associateAddress(connection);
+            DatastoreInstanceDetection.stopDetectingConnectionAddress();
+
+            InetSocketAddress address = DatastoreInstanceDetection.getAddressForConnection(connection);
+            assertEquals("someOtherHost", address.getHostName());
+            assertEquals(9128, address.getPort());
+        }
+    }
+
+    @Test
+    public void testDifferentAddresses_UseNone() {
+        // NONE is the default
         DatastoreInstanceDetection.detectConnectionAddress();
         DatastoreInstanceDetection.saveAddress(InetSocketAddress.createUnresolved("myhost", 1234));
         DatastoreInstanceDetection.saveAddress(InetSocketAddress.createUnresolved("someOtherHost", 9128));
@@ -58,6 +103,50 @@ public class DatastoreInstanceDetectionTest {
 
         InetSocketAddress address = DatastoreInstanceDetection.getAddressForConnection(connection);
         assertNull(address);
+    }
+
+    @Test
+    public void testThreading() throws Exception {
+        PausedConnectThread t1 = new PausedConnectThread("first");
+        PausedConnectThread t2 = new PausedConnectThread("second");
+
+        t1.start();
+        Thread.sleep(100);
+        t2.start();
+        Thread.sleep(100);
+
+        t2.goOn();
+        Thread.sleep(100);
+        t1.goOn();
+        Thread.sleep(100);
+    }
+
+    private class PausedConnectThread extends Thread {
+        String host;
+        boolean paused = false;
+        PausedConnectThread(String name) {
+            this.host = name;
+        }
+        public void run() {
+            DatastoreInstanceDetection.detectConnectionAddress();
+            DatastoreInstanceDetection.saveAddress(InetSocketAddress.createUnresolved(host, 1234));
+            paused = true;
+            while (paused) {
+                //System.out.println("Paused, waiting 10ms: "+host);
+                try { Thread.sleep(10); } catch (Exception e) {}
+            }
+            Connection connection = Mockito.mock(Connection.class);
+            DatastoreInstanceDetection.associateAddress(connection);
+            DatastoreInstanceDetection.stopDetectingConnectionAddress();
+
+            InetSocketAddress address = DatastoreInstanceDetection.getAddressForConnection(connection);
+            assertEquals(host, address.getHostName());
+            assertEquals(1234, address.getPort());
+
+        }
+        public void goOn() {
+            this.paused = false;
+        }
     }
 
     @Test
