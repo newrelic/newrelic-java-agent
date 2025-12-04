d518b1b85053f1596138a504315e55d32a290a78
edeleon <edeleon@newrelic.com>
2025-12-04 15:19:27 -0800

    add tests for AwsConfig proxy disable settings

---
+++ newrelic-agent/src/test/java/com/newrelic/agent/config/AwsConfigTest.java
@@ -0,0 +1,38 @@
+package com.newrelic.agent.config;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class AwsConfigTest {
+
+    @Test
+    public void testDisableFargateMetadataEndpointProxy_ValueIsDefaultedFalse() {
+        Map<String, Object> props = new HashMap<>();
+        AwsConfig awsConfig = new AwsConfigImpl(props);
+
+        assertFalse(awsConfig.isFargateMetadataEndpointProxyDisabled());
+    }
+
+    @Test
+    public void testDisableFargateMetadataEndpointProxy_ValueIsSetToFalse() {
+        Map<String, Object> props = new HashMap<>();
+        props.put("fargate_metadata_endpoint_proxy_disable", false);
+        AwsConfig awsConfig = new AwsConfigImpl(props);
+
+        assertFalse(awsConfig.isFargateMetadataEndpointProxyDisabled());
+    }
+
+    @Test
+    public void testDisableFargateMetadataEndpointProxy_ValueIsSetToTrue() {
+        Map<String, Object> props = new HashMap<>();
+        props.put("fargate_metadata_endpoint_proxy_disable", true);
+        AwsConfig awsConfig = new AwsConfigImpl(props);
+
+        assertTrue(awsConfig.isFargateMetadataEndpointProxyDisabled());
+    }
+}
