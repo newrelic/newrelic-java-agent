5dee04d1f7b2ffd28f2d2a59d871ae54d1224450
edeleon <edeleon@newrelic.com>
2026-01-14 19:46:12 -0800

    create CloudConfig interface

---
+++ newrelic-agent/src/main/java/com/newrelic/agent/config/CloudConfig.java
@@ -0,0 +1,16 @@
+/*
+ *
+ *  * Copyright 2025 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package com.newrelic.agent.config;
+
+/**
+ * Interface for cloud configuration options.
+ */
+
+public interface CloudConfig extends Config {
+    AwsConfig getAwsConfig();
+}
