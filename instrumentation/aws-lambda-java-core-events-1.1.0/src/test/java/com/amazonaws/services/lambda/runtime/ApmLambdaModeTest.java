7a793a32001c9dcd344ed1aa7c9c03e9e14f63e9
Oren Ben-Meir <orenbm11@gmail.com>
2026-03-04 18:01:03 -0500

    Add copyright header

--- instrumentation/aws-lambda-java-core-events-1.1.0/src/test/java/com/amazonaws/services/lambda/runtime/ApmLambdaModeTest.java
+++ instrumentation/aws-lambda-java-core-events-1.1.0/src/test/java/com/amazonaws/services/lambda/runtime/ApmLambdaModeTest.java
@@ -1,3 +1,10 @@
+/*
+ *
+ *  * Copyright 2026 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
 package com.amazonaws.services.lambda.runtime;
 
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
