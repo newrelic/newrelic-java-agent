c6cf6f6327a251955397d013844cf0a090c03224
Jason Keller <jasonjkeller@gmail.com>
2026-04-01 10:26:44 -0700

    Add config check for streaming functionality

--- instrumentation/spring-ai-client-chat-1.0.0/src/main/java/org/springframework/ai/chat/client/DefaultChatClient_Instrumentation.java
+++ instrumentation/spring-ai-client-chat-1.0.0/src/main/java/org/springframework/ai/chat/client/DefaultChatClient_Instrumentation.java
@@ -26,6 +26,7 @@ import java.util.Map;
 import java.util.function.Consumer;
 
 import static com.newrelic.agent.bridge.aimonitoring.AiMonitoringUtils.isAiMonitoringEnabled;
+import static com.newrelic.agent.bridge.aimonitoring.AiMonitoringUtils.isAiMonitoringStreamingEnabled;
 import static llm.completions.vendor.Vendor.VENDOR_VERSION;
 
 public class DefaultChatClient_Instrumentation {
@@ -75,65 +76,66 @@ public class DefaultChatClient_Instrumentation {
         private Flux<ChatClientResponse> doGetObservableFluxChatResponse(ChatClientRequest chatClientRequest) {
             long startTime = System.currentTimeMillis();
             Flux<ChatClientResponse> chatClientResponseFlux = Weaver.callOriginal();
-
             if (isAiMonitoringEnabled()) {
-                Transaction txn = AgentBridge.getAgent().getTransaction();
-                ModelInvocation.incrementInstrumentedSupportabilityMetric(VENDOR_VERSION);
+                if (isAiMonitoringStreamingEnabled()) {
+                    Transaction txn = AgentBridge.getAgent().getTransaction();
+                    ModelInvocation.incrementInstrumentedSupportabilityMetric(VENDOR_VERSION);
 
-                if (!(txn instanceof NoOpTransaction)) {
-                    // Segment will be renamed later when the response is available
-                    Segment segment = txn.startSegment("");
-                    // Set llm = true agent attribute, this is required on transaction events
-                    ModelInvocation.setLlmTrueAgentAttribute(txn);
+                    if (!(txn instanceof NoOpTransaction)) {
+                        // Segment will be renamed later when the response is available
+                        Segment segment = txn.startSegment("");
+                        // Set llm = true agent attribute, this is required on transaction events
+                        ModelInvocation.setLlmTrueAgentAttribute(txn);
 
-                    // This should never happen, but protecting against bad implementations
-                    if (chatClientResponseFlux == null) {
-                        segment.end();
-                    } else {
-                        Map<String, Object> userAttributes = txn.getUserAttributes();
-                        Map<String, String> linkingMetadata = NewRelic.getAgent().getLinkingMetadata();
+                        // This should never happen, but protecting against bad implementations
+                        if (chatClientResponseFlux == null) {
+                            segment.end();
+                        } else {
+                            Map<String, Object> userAttributes = txn.getUserAttributes();
+                            Map<String, String> linkingMetadata = NewRelic.getAgent().getLinkingMetadata();
 
-                        try {
-                            // collect all chunks of the stream response into a list without blocking
-                            // must use anonymous classes instead of lambdas or else instrumentation won't apply
-                            chatClientResponseFlux.collectList().subscribe(
-                                    // onNext consumer
-                                    new Consumer<List<ChatClientResponse>>() {
-                                        @Override
-                                        public void accept(List<ChatClientResponse> chatClientResponseList) {
-                                            List<ChatClientResponse> list = chatClientResponseList != null ? chatClientResponseList : new ArrayList<>();
+                            try {
+                                // collect all chunks of the stream response into a list without blocking
+                                // must use anonymous classes instead of lambdas or else instrumentation won't apply
+                                chatClientResponseFlux.collectList().subscribe(
+                                        // onNext consumer
+                                        new Consumer<List<ChatClientResponse>>() {
+                                            @Override
+                                            public void accept(List<ChatClientResponse> chatClientResponseList) {
+                                                List<ChatClientResponse> list = chatClientResponseList != null ? chatClientResponseList : new ArrayList<>();
 
-                                            // Create Spring AI model invocation
-                                            ModelInvocation springAiInvocation = new SpringAiModelInvocation(
-                                                    linkingMetadata, userAttributes, chatClientRequest, list);
-                                            springAiInvocation.setSegmentName(segment, "stream");
-                                            springAiInvocation.recordLlmEventsAsync(startTime, null);
-                                        }
-                                    },
-                                    // onError consumer
-                                    new Consumer<Throwable>() {
-                                        @Override
-                                        public void accept(Throwable error) {
-                                            if (segment != null) {
-                                                segment.endAsync();
+                                                // Create Spring AI model invocation
+                                                ModelInvocation springAiInvocation = new SpringAiModelInvocation(
+                                                        linkingMetadata, userAttributes, chatClientRequest, list);
+                                                springAiInvocation.setSegmentName(segment, "stream");
+                                                springAiInvocation.recordLlmEventsAsync(startTime, null);
+                                            }
+                                        },
+                                        // onError consumer
+                                        new Consumer<Throwable>() {
+                                            @Override
+                                            public void accept(Throwable error) {
+                                                if (segment != null) {
+                                                    segment.endAsync();
+                                                }
+                                            }
+                                        },
+                                        // onComplete consumer
+                                        new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                if (segment != null) {
+                                                    segment.endAsync();
+                                                }
                                             }
                                         }
-                                    },
-                                    // onComplete consumer
-                                    new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            if (segment != null) {
-                                                segment.endAsync();
-                                            }
-                                        }
-                                    }
-                            );
-                        } catch (Throwable t) {
-                            if (segment != null) {
-                                segment.endAsync();
+                                );
+                            } catch (Throwable t) {
+                                if (segment != null) {
+                                    segment.endAsync();
+                                }
+                                AgentBridge.instrumentation.noticeInstrumentationError(t, Weaver.getImplementationTitle());
                             }
-                            AgentBridge.instrumentation.noticeInstrumentationError(t, Weaver.getImplementationTitle());
                         }
                     }
                 }
