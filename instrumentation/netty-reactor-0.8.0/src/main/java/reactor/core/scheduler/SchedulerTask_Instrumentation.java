69f7a8ffd3b049453efc64b3c0f7e6c8252e0090
Doug Hilpipre <dhilpipre@newrelic.com>
2026-04-07 07:14:08 -0500

    added skip for netty reactor

---
+++ instrumentation/netty-reactor-0.8.0/src/main/java/reactor/core/scheduler/SchedulerTask_Instrumentation.java
@@ -0,0 +1,23 @@
+/*
+ *
+ *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package reactor.core.scheduler;
+
+import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+
+@Weave(originalName = "reactor.core.scheduler.SchedulerTask")
+final class SchedulerTask_Instrumentation {
+
+    // We need to be able to link the Token here when executing on a supplied Scheduler via Mono::publishOn
+    // A Token should be available on the thread that this task executes on if tokenLift() was added to Hooks.onEachOperator
+    @Trace(async = true, excludeFromTransactionTrace = true)
+    public Void call() {
+        return Weaver.callOriginal();
+    }
+}
---
+++ instrumentation/netty-reactor-0.8.0/src/main/java/reactor/netty/http/server/HttpServerMetricsHandler.java
@@ -0,0 +1,7 @@
+package reactor.netty.http.server;
+
+import com.newrelic.api.agent.weaver.SkipIfPresent;
+
+@SkipIfPresent
+final class HttpServerMetricsHandler {
+}
