60eb33ad4c0a5f8dcf86e94521fd7d4a1c02acfd
Doug Hilpipre <dhilpipre@newrelic.com>
2026-04-29 15:27:45 -0500

    added support for BlockingHttpClient

--- instrumentation/micronaut-http-client-2.0.0/src/main/java/io/micronaut/http/client/netty/DefaultHttpClient_Instrumentation.java
+++ instrumentation/micronaut-http-client-2.0.0/src/main/java/io/micronaut/http/client/netty/DefaultHttpClient_Instrumentation.java
@@ -28,11 +28,12 @@ import io.reactivex.Flowable;
 @Weave(originalName = "io.micronaut.http.client.netty.DefaultHttpClient", type = MatchType.ExactClass)
 public abstract class DefaultHttpClient_Instrumentation {
 
-    @Trace(dispatcher = true)
+    @Trace
     public <I> Flowable<Event<ByteBuffer<?>>> eventStream(io.micronaut.http.HttpRequest<I> request) {
         return Weaver.callOriginal();
     }
 
+    @Trace
     public <I, O, E> Flowable<io.micronaut.http.HttpResponse<O>> exchange(io.micronaut.http.HttpRequest<I> request, Argument<O> bodyType,
             Argument<E> errorType) {
         MicronautHttpOutbound<I> wrapper = new MicronautHttpOutbound<I>(request);
@@ -49,6 +50,7 @@ public abstract class DefaultHttpClient_Instrumentation {
         return result.doOnSubscribe(listener).doOnCancel(listener).doOnTerminate(listener).doOnNext(new ResponseConsumer(txn));
     }
 
+    @Trace
     public <I> Flowable<io.micronaut.http.HttpResponse<ByteBuffer<?>>> exchangeStream(io.micronaut.http.HttpRequest<I> request) {
         MicronautHttpOutbound<I> wrapper = new MicronautHttpOutbound<I>(request);
         NewRelic.getAgent().getTracedMethod().addOutboundRequestHeaders(wrapper);
@@ -64,12 +66,12 @@ public abstract class DefaultHttpClient_Instrumentation {
         return result.doOnSubscribe(listener).doOnCancel(listener).doOnTerminate(listener).doOnNext(new ResponseConsumer(txn));
     }
 
-    @Trace(dispatcher = true)
+    @Trace
     public <I, O> Flowable<O> jsonStream(io.micronaut.http.HttpRequest<I> request, io.micronaut.core.type.Argument<O> type) {
         return Weaver.callOriginal();
     }
 
-    @Trace(dispatcher = true)
+    @Trace
     public Flowable<MutableHttpResponse<?>> proxy(io.micronaut.http.HttpRequest<?> request) {
         MicronautHeaders headers = new MicronautHeaders(request);
         NewRelic.getAgent().getTransaction().insertDistributedTraceHeaders(headers);
