7cf6eab2f78aa6b091a53c5f13699ea74f818bc1
Doug Hilpipre <dhilpipre@newrelic.com>
2026-04-29 10:53:18 -0500

    updated ReactorUtils to ignore a Reactor Runnable that caused unexpired async tokens

--- instrumentation/reactor-core-3.1.0/src/main/java/com/nr/instrumentation/reactor/ReactorUtils.java
+++ instrumentation/reactor-core-3.1.0/src/main/java/com/nr/instrumentation/reactor/ReactorUtils.java
@@ -3,10 +3,25 @@ package com.nr.instrumentation.reactor;
 import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Token;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ReactorUtils {
 
+	/*
+		Discovered that MonoDelay$MonoDelayRunnable can be dispatched but never run which leads
+		to unexpired tokens.
+		if others are discovered then add them to the list
+	 */
+	private static final Set<String> IGNORED_RUNNABLES = new HashSet<>();
+
+	static {
+		IGNORED_RUNNABLES.add("reactor.core.publisher.MonoDelay$MonoDelayRunnable");
+	}
+
 	public static NRRunnableWrapper getRunnableWrapper(Runnable r) {
-		if(r instanceof NRRunnableWrapper) {return null;}
+		if(r instanceof NRRunnableWrapper) return null;
+		if(IGNORED_RUNNABLES.contains(r.getClass().getName())) return null;
 
 		Token currentToken = NewRelic.getAgent().getTransaction().getToken();
 		if(currentToken != null) {
@@ -21,5 +36,5 @@ public class ReactorUtils {
 
 		return null;
 	}
-	
+
 }
