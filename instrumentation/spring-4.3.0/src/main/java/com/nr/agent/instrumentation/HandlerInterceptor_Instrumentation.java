0dd5c84e1a12e10e1808804f8a4a329abce5f408
Jerry Duffy <jduffy@newrelic.com>
2026-05-07 10:59:34 -0400

    Point cut replacement

---
+++ instrumentation/spring-4.3.0/src/main/java/com/nr/agent/instrumentation/HandlerInterceptor_Instrumentation.java
@@ -0,0 +1,58 @@
+/*
+ *
+ *  * Copyright 2026 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+package com.nr.agent.instrumentation;
+
+import com.newrelic.agent.bridge.AgentBridge;
+import com.newrelic.agent.bridge.Transaction;
+import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Weave(type = MatchType.Interface, originalName = "org.springframework.web.servlet.HandlerInterceptor")
+public class HandlerInterceptor_Instrumentation {
+    @Trace
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        setMetricName("preHandle");
+        return Weaver.callOriginal();
+    }
+
+    @Trace
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) {
+        setMetricName("postHandle");
+        Weaver.callOriginal();
+    }
+
+    @Trace
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+            Exception ex) {
+        setMetricName("afterCompletion");
+        Weaver.callOriginal();
+    }
+
+    private void setMetricName(String methodName) {
+        // This allows us to get a rollup metric name in the form: Spring/HandlerInterceptor
+        // and a segment name like:  Spring/Java/com.foo.MyClass/preHandle
+        // This matches the legacy point cut behavior
+        Transaction transaction = AgentBridge.getAgent().getTransaction(false);
+        if (transaction != null) {
+            com.newrelic.api.agent.TracedMethod tracedMethod = transaction.getTracedMethod();
+            if (tracedMethod != null) {
+                String className = getClass().getName();
+                // Sets both metric name and segment name to the class-specific value
+                tracedMethod.setMetricName("Spring", "Java", className, methodName);
+                // Adds the rollup metric as an additional unscoped metric
+                tracedMethod.addRollupMetricName("Spring", "HandlerInterceptor");
+            }
+        }
+    }
+}
