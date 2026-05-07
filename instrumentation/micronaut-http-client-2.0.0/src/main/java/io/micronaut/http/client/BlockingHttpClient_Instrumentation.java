99757f7763afc3b43df2bd22e53548d8c546fd47
Doug Hilpipre <dhilpipre@newrelic.com>
2026-05-07 16:10:50 -0500

    added missing header, modified 2025 to 2026

--- instrumentation/micronaut-http-client-2.0.0/src/main/java/io/micronaut/http/client/BlockingHttpClient_Instrumentation.java
+++ instrumentation/micronaut-http-client-2.0.0/src/main/java/io/micronaut/http/client/BlockingHttpClient_Instrumentation.java
@@ -1,3 +1,9 @@
+/*
+ *
+ *  * Copyright 2026 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
 package io.micronaut.http.client;
 
 import com.newrelic.api.agent.HttpParameters;
