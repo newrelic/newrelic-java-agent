1be56c54b7ccf751eb09e60c19c4234bc4a38350
Jerry Duffy <jduffy@newrelic.com>
2026-06-02 10:02:46 -0400

    Remove metadata blob NewFields

--- instrumentation/apache-log4j-2.11/src/main/java/org/apache/logging/log4j/core/layout/AbstractJacksonLayout_Instrumentation.java
+++ instrumentation/apache-log4j-2.11/src/main/java/org/apache/logging/log4j/core/layout/AbstractJacksonLayout_Instrumentation.java
@@ -10,7 +10,7 @@ package org.apache.logging.log4j.core.layout;
 import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
-import org.apache.logging.log4j.core.LogEvent_Instrumentation;
+import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.util.StringBuilderWriter;
 
 import java.io.IOException;
@@ -18,7 +18,6 @@ import java.io.Writer;
 
 import static com.newrelic.agent.bridge.logging.AppLoggingUtils.BLOB_PREFIX;
 import static com.newrelic.agent.bridge.logging.AppLoggingUtils.getLinkingMetadataBlob;
-import static com.newrelic.agent.bridge.logging.AppLoggingUtils.getLinkingMetadataBlobFromMap;
 import static com.newrelic.agent.bridge.logging.AppLoggingUtils.isApplicationLoggingEnabled;
 import static com.newrelic.agent.bridge.logging.AppLoggingUtils.isApplicationLoggingLocalDecoratingEnabled;
 import static com.nr.agent.instrumentation.log4j2.AgentUtil.foundIndexToInsertLinkingMetadata;
@@ -27,9 +26,9 @@ import static com.nr.agent.instrumentation.log4j2.AgentUtil.getIndexToModifyJson
 @Weave(originalName = "org.apache.logging.log4j.core.layout.AbstractJacksonLayout", type = MatchType.ExactClass)
 abstract class AbstractJacksonLayout_Instrumentation {
 
-    public abstract void toSerializable(final LogEvent_Instrumentation event, final Writer writer) throws IOException;
+    public abstract void toSerializable(final LogEvent event, final Writer writer) throws IOException;
 
-    public String toSerializable(final LogEvent_Instrumentation event) {
+    public String toSerializable(final LogEvent event) {
         final StringBuilderWriter writer = new StringBuilderWriter();
         try {
             toSerializable(event, writer);
@@ -40,16 +39,17 @@ abstract class AbstractJacksonLayout_Instrumentation {
             if (isApplicationLoggingEnabled() && isApplicationLoggingLocalDecoratingEnabled()) {
                 // It is possible that the log might already have NR-LINKING metadata from JUL instrumentation
                 if (!writerString.contains(BLOB_PREFIX)) {
+                    System.out.println("DUF- here1");
                     int indexToModifyJson = getIndexToModifyJson(writerString);
                     if (foundIndexToInsertLinkingMetadata(indexToModifyJson)) {
                         // Replace the JSON string with modified version that includes NR-LINKING metadata
-                        if (event != null && event.agentLinkingMetadata != null && (!event.agentLinkingMetadata.isEmpty())) {
+                        String linkingMetadata = event.getContextData().getValue(BLOB_PREFIX);
+                        if (linkingMetadata != null) {
                             // Get linking metadata stored on LogEvent if available. This ensures that
                             // the trace.id and span.id will be available when using an async appender.
-                            modified = new StringBuilder(writerString).insert(indexToModifyJson, getLinkingMetadataBlobFromMap(event.agentLinkingMetadata))
-                                    .toString();
+                            modified = new StringBuilder(writerString).insert(indexToModifyJson, linkingMetadata).toString();
                         } else {
-                            // Get linking metadata from current thread if it is not available on LogEvent.
+                            // Get linking metadata from current thread if it is not available on TreadContext.
                             modified = new StringBuilder(writerString).insert(indexToModifyJson, getLinkingMetadataBlob()).toString();
                         }
                     }
---
+++ instrumentation/apache-log4j-2.11/src/main/java/org/apache/logging/log4j/spi/AbstractLogger_Instrumentation.java
@@ -0,0 +1,30 @@
+/*
+ *
+ *  * Copyright 2026 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+package org.apache.logging.log4j.spi;
+
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.ThreadContext;
+import org.apache.logging.log4j.message.Message;
+
+import static com.newrelic.agent.bridge.logging.AppLoggingUtils.*;
+
+@Weave(originalName = "org.apache.logging.log4j.spi.AbstractLogger", type = MatchType.BaseClass)
+public abstract class AbstractLogger_Instrumentation {
+    private void logMessageSafely(final String fqcn, final Level level, final Marker marker,
+            final Message message, final Throwable throwable) {
+        if (isApplicationLoggingEnabled() && isApplicationLoggingLocalDecoratingEnabled()) {
+            System.out.println("DUF- Thread: " + Thread.currentThread());
+            ThreadContext.put(BLOB_PREFIX, getLinkingMetadataBlob());
+        }
+
+        Weaver.callOriginal();
+    }
+}
