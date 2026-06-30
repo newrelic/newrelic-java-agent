f4f23f83bcab36ba077c689de836791c6748e265
Kate Anderson <kate.anderson250@gmail.com>
2026-06-30 15:49:29 -0700

    Add instrumentation for connect(address) and finishConnect() to jdbc-socket

--- instrumentation/jdbc-socket/src/main/java/java/nio/channels/SocketChannel_Instrumentation.java
+++ instrumentation/jdbc-socket/src/main/java/java/nio/channels/SocketChannel_Instrumentation.java
@@ -9,23 +9,47 @@ package java.nio.channels;
 
 import com.newrelic.agent.bridge.datastore.DatastoreInstanceDetection;
 import com.newrelic.agent.bridge.datastore.JdbcHelper;
+import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.NewField;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.util.logging.Level;
 
 @Weave(originalName = "java.nio.channels.SocketChannel", type = MatchType.BaseClass)
 public abstract class SocketChannel_Instrumentation {
 
+    @NewField
+    InetSocketAddress address;
+
     public static SocketChannel_Instrumentation open(SocketAddress remote) {
         SocketChannel_Instrumentation channel = Weaver.callOriginal();
-
         if (channel.isConnected() && DatastoreInstanceDetection.shouldDetectConnectionAddress() && (remote instanceof InetSocketAddress)) {
             DatastoreInstanceDetection.saveAddress((InetSocketAddress) remote);
         }
         return channel;
+    }
+
+    public boolean connect(SocketAddress remote) throws IOException {
+        boolean result = Weaver.callOriginal();
+        if (DatastoreInstanceDetection.shouldDetectConnectionAddress() && (remote instanceof InetSocketAddress)) {
+            NewRelic.getAgent().getLogger().log(Level.INFO, "JDBC Connection Debug: method=connect, storing address {0}", remote);
+            this.address = (InetSocketAddress) remote;
+        }
+        return result;
+    }
+
+    public boolean finishConnect() {
+        boolean result = Weaver.callOriginal();
+        if (isConnected() && DatastoreInstanceDetection.shouldDetectConnectionAddress() && this.address != null) {
+            NewRelic.getAgent().getLogger().log(Level.INFO, "JDBC Connection Debug: method=finishConnect, saving address {0}", this.address);
+            DatastoreInstanceDetection.saveAddress(this.address);
+        }
+        return result;
     }
 
     public abstract boolean isConnected();
