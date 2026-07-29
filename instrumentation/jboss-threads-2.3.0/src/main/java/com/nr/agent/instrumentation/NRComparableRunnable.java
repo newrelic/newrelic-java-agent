e4e2538e3633b428901ea2d2617f41b42fa2e983
Jerry Duffy <jduffy@newrelic.com>
2026-07-29 09:55:15 -0400

    Remove unneeded reinstrument call

--- instrumentation/jboss-threads-2.3.0/src/main/java/com/nr/agent/instrumentation/NRComparableRunnable.java
+++ instrumentation/jboss-threads-2.3.0/src/main/java/com/nr/agent/instrumentation/NRComparableRunnable.java
@@ -6,25 +6,16 @@
  */
 package com.nr.agent.instrumentation;
 
-import com.newrelic.agent.bridge.AgentBridge;
 import com.newrelic.api.agent.Token;
-
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class NRComparableRunnable<T> extends NRRunnable implements Comparable<T> {
     private Comparable<T> comparableDelegate = null;
-    private static final AtomicBoolean isTransformed = new AtomicBoolean(false);
 
     @SuppressWarnings("unchecked")
     public NRComparableRunnable(Runnable runnable, Token token) {
         super(runnable, token);
         if (runnable instanceof Comparable) {
             comparableDelegate = (Comparable<T>)runnable;
-        }
-
-        if (!isTransformed.get()) {
-            AgentBridge.instrumentation.retransformUninstrumentedClass(NRComparableRunnable.class);
-            isTransformed.set(true);
         }
     }
 
