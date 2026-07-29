86b0319aea982b79785e676ee2e44f468a5156fe
Oren Ben-Meir <orenbm11@gmail.com>
2026-07-29 17:40:27 -0400

    Clean older NR Labs redisson modules

--- instrumentation/redisson-3.17/src/main/java/org/redisson/reactive/CommandReactiveService.java
+++
@@ -1,9 +0,0 @@
-package org.redisson.reactive;
-
-import com.newrelic.api.agent.weaver.MatchType;
-import com.newrelic.api.agent.weaver.Weave;
-
-@Weave(type=MatchType.BaseClass)
-public abstract class CommandReactiveService {
-
-}
