c4f4b22c6e07efddedc604716df27f758c1c9943
Jerry Duffy <jduffy@newrelic.com>
2026-08-05 12:25:28 -0400

    Properly handle exceptions thrown from the normalizer flow

--- newrelic-agent/src/test/java/com/newrelic/agent/normalization/NormalizerTest.java
+++ newrelic-agent/src/test/java/com/newrelic/agent/normalization/NormalizerTest.java
@@ -21,6 +21,71 @@ public class NormalizerTest {
 
     private static final String APP_NAME = "Unit Test";
 
+    @Test
+    public void normalize_withInvalidReplacementString_doesNotThrow() {
+        // If the replacement string is a correct format but a group index is out of bounds
+        // This rule is from the actual support case
+        String match =
+                "^MessageBroker/Kafka/Internal/(consumer-metrics|producer-metrics)/client/[^/]+/(.+)$";
+        String replace = "MessageBroker/Kafka/Internal/\\1/client/Kafka-Producers/\\3";  // No group 3 in match String
+        NormalizationRule rule = new NormalizationRule(match, replace, false, 1, true, false, false);
+        List<NormalizationRule> rules = Collections.singletonList(rule);
+
+        Normalizer normalizer = new NormalizerImpl("foo", rules);
+        Assert.assertTrue(normalizer.getRules().contains(rule));
+
+        // This should not throw an exception and the target String should be returned unmodified
+        String result = normalizer.normalize(
+                "MessageBroker/Kafka/Internal/producer-metrics/client/my-producer-id/record-send-rate");
+        Assert.assertEquals(
+                "MessageBroker/Kafka/Internal/producer-metrics/client/my-producer-id/record-send-rate", result);
+
+        // ... and the list should now be empty since the bad rule gets removed
+        Assert.assertTrue(normalizer.getRules().isEmpty());
+    }
+
+    @Test
+    public void normalize_withBadRuleInMiddleOfChain_appliesRemainingRulesAndRemovesOnlyBadRule() {
+        NormalizationRule goodRule1 = new NormalizationRule("^foo/(.*)$", "baz/\\1", false, 1, false, false, false);
+        // Matches "baz/bar/baz" with 2 groups but the replacement references a non-existent group 3
+        NormalizationRule badRule = new NormalizationRule("^baz/(.*)/(.*)$", "baz/\\1/\\2/\\3", false, 2, false, false,
+                false);
+        NormalizationRule goodRule2 = new NormalizationRule("^baz/(.*)$", "final/\\1", false, 3, true, false, false);
+        List<NormalizationRule> rules = Arrays.asList(goodRule1, badRule, goodRule2);
+
+        Normalizer normalizer = new NormalizerImpl("foo", rules);
+
+        // both good rules should apply, in order, despite the bad rule throwing in between them
+        Assert.assertEquals("final/bar/baz", normalizer.normalize("foo/bar/baz"));
+
+        // only the bad rule should have been removed, in place, leaving the good rules intact
+        Assert.assertEquals(Arrays.asList(goodRule1, goodRule2), normalizer.getRules());
+
+        // a second call must not re-throw and should produce the same result using only the surviving rules
+        Assert.assertEquals("final/bar/baz", normalizer.normalize("foo/bar/baz"));
+        Assert.assertEquals(2, normalizer.getRules().size());
+    }
+
+    @Test
+    public void normalize_withBadRuleMarkedTerminateChain_stillAppliesSubsequentRules() {
+        NormalizationRule goodRule1 = new NormalizationRule("^foo/(.*)$", "bar/\\1", false, 1, false, false, false);
+        // terminateChain is true here, but that flag must not take effect because the rule throws instead of matching
+        NormalizationRule badRule = new NormalizationRule("^bar/(.*)/(.*)$", "bar/\\1/\\2/\\3", false, 2, true,
+                false, false);
+        NormalizationRule goodRule2 = new NormalizationRule("^bar/(.*)$", "baz/\\1", false, 3, false, false, false);
+        List<NormalizationRule> rules = Arrays.asList(goodRule1, badRule, goodRule2);
+
+        Normalizer normalizer = new NormalizerImpl("foo", rules);
+
+        // goodRule2 must still run even though the removed rule ahead of it was marked terminateChain=true
+        Assert.assertEquals("baz/boo/fizz", normalizer.normalize("foo/boo/fizz"));
+        Assert.assertEquals(Arrays.asList(goodRule1, goodRule2), normalizer.getRules());
+
+        // a second call must not re-throw and should produce the same result using only the surviving rules
+        Assert.assertEquals("baz/boo/fizz", normalizer.normalize("foo/boo/fizz"));
+        Assert.assertEquals(2, normalizer.getRules().size());
+    }
+
     @SuppressWarnings({ "unchecked", "serial" })
     @Test
     public void emptyUrlRules() {
