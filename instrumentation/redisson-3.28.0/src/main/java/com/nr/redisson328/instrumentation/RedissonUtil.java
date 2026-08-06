257189d73e6ded45c26e4467c011276c82069e85
Oren Ben-Meir <orenbm11@gmail.com>
2026-08-05 20:31:43 -0400

    Extended redisson module verification to 3.28.0

---
+++ instrumentation/redisson-3.28.0/src/main/java/com/nr/redisson328/instrumentation/RedissonUtil.java
@@ -0,0 +1,59 @@
+package com.nr.redisson328.instrumentation;
+
+import com.newrelic.api.agent.DatastoreParameters;
+import com.newrelic.api.agent.NewRelic;
+import com.newrelic.api.agent.Segment;
+import org.redisson.connection.ConnectionManager;
+import org.redisson.connection.MasterSlaveConnectionManager;
+import org.redisson.connection.MasterSlaveEntry;
+import org.redisson.misc.RedisURI;
+
+import java.net.InetSocketAddress;
+
+public class RedissonUtil {
+
+    public static Segment createSegment(String prefix, String operationName) {
+        return NewRelic.getAgent().getTransaction().startSegment(prefix + "-" + operationName);
+    }
+
+    public static DatastoreParameters createDatastoreParameters(String operationName, NrRedisUri nrRedisUri) {
+        DatastoreParameters.InstanceParameter param = DatastoreParameters.product("Redisson")
+                .collection(null)
+                .operation(operationName);
+        if (nrRedisUri != null) {
+            return param.instance(nrRedisUri.getHost(), nrRedisUri.getPort()).build();
+        }
+        return param.build();
+    }
+
+    public static NrRedisUri extractUri(ConnectionManager params) {
+        if (params instanceof MasterSlaveConnectionManager) {
+            MasterSlaveConnectionManager masterSlaveConnectionManager = ((MasterSlaveConnectionManager)params);
+            for (MasterSlaveEntry entry : masterSlaveConnectionManager.getEntrySet()) {
+                InetSocketAddress address = entry.getClient().getAddr();
+                return new NrRedisUri(address.getHostString(), address.getPort());
+            }
+        }
+        RedisURI redisURI = params.getLastClusterNode();
+        if (redisURI != null) {
+            return new NrRedisUri(redisURI.getHost(), redisURI.getPort());
+        }
+        return null;
+    }
+
+    public static class NrRedisUri {
+        private String host;
+        private Integer port;
+        public NrRedisUri(String host, Integer port) {
+            this.host = host;
+            this.port = port;
+        }
+        public String getHost() {
+            return host;
+        }
+        public Integer getPort() {
+            return port;
+        }
+    }
+
+}
