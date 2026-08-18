d317e6b121bbfbe917bd847f76a125c8cb93d04a
Oren Ben-Meir <orenbm11@gmail.com>
2026-08-18 13:01:10 -0400

    Add copyright headers

--- instrumentation/redisson-3.28.0/src/main/java/com/nr/redisson328/instrumentation/NRBiConsumer.java
+++ instrumentation/redisson-3.28.0/src/main/java/com/nr/redisson328/instrumentation/NRBiConsumer.java
@@ -1,3 +1,10 @@
+/*
+ *
+ *  * Copyright 2027 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
 package com.nr.redisson328.instrumentation;
 
 import java.util.function.BiConsumer;
--- instrumentation/redisson-3.28.0/src/main/java/org/redisson/command/CommandBatchService_Instrumentation.java
+++ instrumentation/redisson-3.28.0/src/main/java/org/redisson/command/CommandBatchService_Instrumentation.java
@@ -1,3 +1,10 @@
+/*
+ *
+ *  * Copyright 2027 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
 package org.redisson.command;
 
 import com.newrelic.api.agent.DatastoreParameters;
