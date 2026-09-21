3c7b28cdcf1b9648e3bb359b33975c86b1074b1f
Jerry Duffy <jduffy@newrelic.com>
2026-09-21 13:36:27 -0400

    Disable agent_settings submission

--- newrelic-agent/src/main/java/com/newrelic/agent/RPMService.java
+++ newrelic-agent/src/main/java/com/newrelic/agent/RPMService.java
@@ -47,7 +47,6 @@ import com.newrelic.agent.transport.HostConnectException;
 import com.newrelic.agent.transport.HttpError;
 import com.newrelic.agent.transport.HttpResponseCode;
 import com.newrelic.agent.transport.serverless.DataSenderServerlessConfig;
-import com.newrelic.agent.util.DefaultThreadFactory;
 import com.newrelic.agent.utilization.UtilizationData;
 import org.json.simple.JSONStreamAware;
 
@@ -61,7 +60,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -137,7 +135,9 @@ public class RPMService extends AbstractService implements IRPMService, Environm
         this.agentConnectionEstablishedListeners = new ArrayList<>(agentConnectionEstablishedListeners);
 
         if (this.isMainApp) {
-            scheduler = Executors.newSingleThreadScheduledExecutor(new DefaultThreadFactory("New Relic agent_settings submission thread", true));
+            // TODO This needs to be reverted when agent_settings is updated to send a correct settings payload
+            // scheduler = Executors.newSingleThreadScheduledExecutor(new DefaultThreadFactory("New Relic agent_settings submission thread", true));
+            scheduler = null;
         }
     }
 
